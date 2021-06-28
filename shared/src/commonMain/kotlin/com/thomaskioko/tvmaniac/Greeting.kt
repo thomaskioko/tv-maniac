8a715b5bbbdf2eaf1a4db09eeae9aef1d61326c7
Thomas Kioko <kiokotomas@gmail.com>
2021-06-28 20:10:09 +0200

    Delete unused classes.

--- shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.thomaskioko.tvmaniac
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.thomaskioko.tvmaniac
-
-expect class Platform() {
-    val platform: String
-}
