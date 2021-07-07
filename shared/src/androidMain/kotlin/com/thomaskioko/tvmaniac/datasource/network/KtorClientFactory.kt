b28b33cb7c34e4f43de6d52571a4f91bed131043
Thomas Kioko <kiokotomas@gmail.com>
2021-07-07 19:36:21 +0200

    Update factory configuration, logging, response observer etc

--- shared/src/androidMain/kotlin/com/thomaskioko/tvmaniac/datasource/network/KtorClientFactory.kt
+++ shared/src/androidMain/kotlin/com/thomaskioko/tvmaniac/datasource/network/KtorClientFactory.kt
@@ -1,9 +1,17 @@
 package com.thomaskioko.tvmaniac.datasource.network
 
+import android.util.Log
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
+import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.features.observer.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+
+private const val TIME_OUT = 60_000
 
 actual class KtorClientFactory {
     actual fun build(): HttpClient {
@@ -12,8 +20,35 @@ actual class KtorClientFactory {
                 serializer = KotlinxSerializer(
                     kotlinx.serialization.json.Json {
                         ignoreUnknownKeys = true
+                        prettyPrint = true
+                        isLenient = true
                     }
                 )
+
+                engine {
+                    connectTimeout = TIME_OUT
+                    socketTimeout = TIME_OUT
+                }
+            }
+
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.v("Ktor Logger =>", message)
+                    }
+                }
+
+                level = LogLevel.ALL
+            }
+
+            install(ResponseObserver) {
+                onResponse { response ->
+                    Log.d("HTTP status:", "${response.status.value}")
+                }
+            }
+
+            install(DefaultRequest) {
+                header(HttpHeaders.ContentType, ContentType.Application.Json)
             }
         }
     }
