5fb726f4c3c3d915b61755b61eb249bc3e85636c
Thomas Kioko <kiokotomas@gmail.com>
2021-07-09 14:01:39 +0200

    Provide network objects with hilt

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/di/NetworkModule.kt
@@ -0,0 +1,36 @@
+package com.thomaskioko.tvmaniac.di
+
+import com.thomaskioko.tvmaniac.datasource.TvShowsRepository
+import com.thomaskioko.tvmaniac.datasource.TvShowsRepositoryImpl
+import com.thomaskioko.tvmaniac.datasource.network.KtorClientFactory
+import com.thomaskioko.tvmaniac.datasource.network.TvShowsService
+import com.thomaskioko.tvmaniac.datasource.network.TvShowsServiceImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): HttpClient {
+        return KtorClientFactory().build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowService(
+        httpClient: HttpClient
+    ): TvShowsService = TvShowsServiceImpl(httpClient)
+
+    @Singleton
+    @Provides
+    fun provideTvShowsRepository(
+        tvShowsService: TvShowsService
+    ): TvShowsRepository = TvShowsRepositoryImpl(tvShowsService)
+}
