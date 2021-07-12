73665bbbcce96a934ea08242608bfd27c94c3f64
Thomas Kioko <kiokotomas@gmail.com>
2021-07-12 14:27:41 +0200

    Add coroutine dispatchers

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/injection/InteractorsModule.kt
@@ -0,0 +1,20 @@
+package com.thomaskioko.tvmaniac.injection
+
+import com.thomaskioko.tvmaniac.datasource.TvShowsRepository
+import com.thomaskioko.tvmaniac.interactor.PopularShowsInteractor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object InteractorsModule {
+
+    @Singleton
+    @Provides
+    fun providePopularShowsInteractor(
+        repository: TvShowsRepository
+    ) : PopularShowsInteractor = PopularShowsInteractor(repository)
+}
