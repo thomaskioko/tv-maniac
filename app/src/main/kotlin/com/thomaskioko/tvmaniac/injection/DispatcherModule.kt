87f058cbd23f87f5ea573d1602334aeb33efb465
Thomas Kioko <kiokotomas@gmail.com>
2021-07-12 14:30:38 +0200

    Provide coroutine dispatchers.

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/injection/DispatcherModule.kt
@@ -0,0 +1,28 @@
+package com.thomaskioko.tvmaniac.injection
+
+import com.thomaskioko.tvmaniac.core.annotations.DefaultDispatcher
+import com.thomaskioko.tvmaniac.core.annotations.IoDispatcher
+import com.thomaskioko.tvmaniac.core.annotations.MainDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DispatcherModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
