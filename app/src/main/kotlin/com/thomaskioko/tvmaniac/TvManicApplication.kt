d35ed676e33eb7b4dfd44151ef55d91789b6d38f
Thomas Kioko <kiokotomas@gmail.com>
2021-07-12 14:31:36 +0200

    Initialize Napier

--- app/src/main/kotlin/com/thomaskioko/tvmaniac/TvManicApplication.kt
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/TvManicApplication.kt
@@ -2,11 +2,17 @@ package com.thomaskioko.tvmaniac
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 
 @HiltAndroidApp
 class TvManicApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG){
+            Napier.base(DebugAntilog())
+        }
     }
 }
