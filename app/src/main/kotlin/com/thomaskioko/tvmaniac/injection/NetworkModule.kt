5ce41f425700e5c1ced22aa2ee1aaeea0a39eb03
Thomas Kioko <kiokotomas@gmail.com>
2021-07-13 17:57:41 +0200

    Add cache to param

--- app/src/main/kotlin/com/thomaskioko/tvmaniac/injection/NetworkModule.kt
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/injection/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.thomaskioko.tvmaniac.injection
 
 import com.thomaskioko.tvmaniac.datasource.TvShowsRepository
 import com.thomaskioko.tvmaniac.datasource.TvShowsRepositoryImpl
+import com.thomaskioko.tvmaniac.datasource.cache.db.TvShowCache
 import com.thomaskioko.tvmaniac.datasource.network.KtorClientFactory
 import com.thomaskioko.tvmaniac.datasource.network.TvShowsService
 import com.thomaskioko.tvmaniac.datasource.network.TvShowsServiceImpl
@@ -31,6 +32,7 @@ class NetworkModule {
     @Singleton
     @Provides
     fun provideTvShowsRepository(
-        tvShowsService: TvShowsService
-    ): TvShowsRepository = TvShowsRepositoryImpl(tvShowsService)
+        tvShowsService: TvShowsService,
+        cache: TvShowCache
+    ): TvShowsRepository = TvShowsRepositoryImpl(tvShowsService, cache)
 }
