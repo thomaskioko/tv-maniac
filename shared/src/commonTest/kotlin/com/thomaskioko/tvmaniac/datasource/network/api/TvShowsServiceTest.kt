897c69fe5acd4170642283d1389cbe9c5c37e489
Thomas Kioko <kiokotomas@gmail.com>
2021-07-15 18:04:53 +0200

    Add integration tests

---
+++ shared/src/commonTest/kotlin/com/thomaskioko/tvmaniac/datasource/network/api/TvShowsServiceTest.kt
@@ -0,0 +1,60 @@
+package com.thomaskioko.tvmaniac.datasource.network.api
+
+import com.thomaskioko.tvmaniac.datasource.network.TvShowsServiceMockEngine
+import com.thomaskioko.tvmaniac.datasource.network.mockresponse.getPopularTvShows
+import com.thomaskioko.tvmaniac.datasource.network.mockresponse.getTvSeasonDetailsResponse
+import com.thomaskioko.tvmaniac.util.runBlocking
+
+import kotlin.test.Test
+
+internal class TvShowsServiceTest : TvShowsServiceMockEngine() {
+
+    companion object {
+        const val GET_TV_SHOW_DETAILS = "/tv/1"
+        const val GET_POPULAR_TV_SHOWS = "/tv/popular"
+    }
+
+    @Test
+    fun whenGetTvSeasonDetails_correctUrlIsCalled(): Unit = runBlocking {
+
+        val apiClient = givenAMockTvShowsService(
+            GET_TV_SHOW_DETAILS,
+            httpStatusCode = 200,
+            responseBody = getTvSeasonDetailsResponse() //TODO Read this from json file
+        )
+
+        apiClient.getTvSeasonDetails(1)
+
+        verifyGetRequest()
+        verifyRequestContainsHeader("Accept", "application/json")
+
+    }
+
+    @Test
+    fun whenPopularShows_correctUrlIsCalled(): Unit = runBlocking {
+
+        val apiClient = givenAMockTvShowsService(
+            endPoint = GET_POPULAR_TV_SHOWS,
+            httpStatusCode = 200,
+            responseBody = getPopularTvShows() //TODO Read this from json file
+        )
+
+        apiClient.getPopularShows(1)
+
+        verifyGetRequest()
+        verifyRequestContainsHeader("Accept", "application/json")
+
+    }
+
+    private fun givenAMockTvShowsService(
+        endPoint: String,
+        responseBody: String = "",
+        httpStatusCode: Int = 200
+    ): TvShowsServiceImpl {
+        enqueueMockResponse(endPoint, responseBody, httpStatusCode)
+
+        return TvShowsServiceImpl(httpClient())
+    }
+
+}
+
