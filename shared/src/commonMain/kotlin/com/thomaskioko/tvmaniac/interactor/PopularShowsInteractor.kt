847df8cc2c234219eb1c4345304cf6263c89a887
Thomas Kioko <kiokotomas@gmail.com>
2021-07-16 16:36:04 +0200

    Update imports

--- shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/interactor/PopularShowsInteractor.kt
+++ shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/interactor/PopularShowsInteractor.kt
@@ -1,7 +1,7 @@
 package com.thomaskioko.tvmaniac.interactor
 
-import com.thomaskioko.tvmaniac.datasource.repository.TvShowsRepository
 import com.thomaskioko.tvmaniac.datasource.cache.model.TvShowsEntity
+import com.thomaskioko.tvmaniac.datasource.repository.tvshow.TvShowsRepository
 import com.thomaskioko.tvmaniac.util.DomainResultState
 import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.error
 import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.loading
