352cc44007a79d6b7c7013a97b5ff27bfd920441
Thomas Kioko <kiokotomas@gmail.com>
2021-07-20 16:48:32 +0200

    Setup app theme and typography

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/compose/theme/Theme.kt
@@ -0,0 +1,51 @@
+package com.thomaskioko.tvmaniac.compose.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+private val LightColors = lightColors(
+    primary = colorPrimaryLight,
+    primaryVariant = colorPrimaryVariantLight,
+    secondary = colorSecondaryLight,
+    secondaryVariant = colorSecondaryVariantLight,
+    surface = Color.White,
+    onPrimary = Color.White,
+    background = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    error = colorError
+)
+
+private val DarkColors = darkColors(
+    primary = colorPrimaryDark,
+    primaryVariant = colorPrimaryVariantDark,
+    secondary = colorSecondaryDark,
+    secondaryVariant = colorSecondaryVariantDark,
+    background = Color.Black,
+    surface = Color.Black,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    error = colorError
+)
+
+
+@Composable
+fun TvManiacTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = TvManiacTypography,
+        shapes = shapes,
+        content = content
+    )
+}
---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/compose/theme/Type.kt
@@ -0,0 +1,95 @@
+package com.thomaskioko.tvmaniac.compose.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.thomaskioko.tvmaniac.R
+
+
+private val workSansFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.work_sans_thin,
+            weight = FontWeight.W200,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.work_sans_medium,
+            weight = FontWeight.W400,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.work_sans_semibold,
+            weight = FontWeight.W500,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.work_sans_bold,
+            weight = FontWeight.W600,
+            style = FontStyle.Normal
+        ),
+        Font(
+            resId = R.font.work_sans_extrabold,
+            weight = FontWeight.W700,
+            style = FontStyle.Normal
+        ),
+
+    )
+)
+
+val TvManiacTypography = Typography(
+    h4 = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 30.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 16.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = workSansFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp
+    )
+)
+
