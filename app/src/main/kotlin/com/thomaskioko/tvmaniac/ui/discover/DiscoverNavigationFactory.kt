c81c9e4465c5fb37d41a2139d4f657581f988385
Thomas Kioko <kiokotomas@gmail.com>
2021-07-21 18:56:21 +0200

    Discover screen ui implementation.

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/ui/discover/DiscoverNavigationFactory.kt
@@ -0,0 +1,23 @@
+package com.thomaskioko.tvmaniac.ui.discover
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import com.thomaskioko.tvmaniac.navigation.ComposeNavigationFactory
+import com.thomaskioko.tvmaniac.navigation.NavigationScreen
+import com.thomaskioko.tvmaniac.navigation.viewModelComposable
+import javax.inject.Inject
+
+internal class DiscoverNavigationFactory @Inject constructor() : ComposeNavigationFactory {
+
+    override fun create(builder: NavGraphBuilder, navController: NavHostController) {
+        builder.viewModelComposable<DiscoverViewModel>(
+            route = NavigationScreen.DiscoverNavScreen.route,
+            content = {
+                DiscoverScreen(
+                    viewModel = this,
+                    navController = navController
+                )
+            }
+        )
+    }
+}
