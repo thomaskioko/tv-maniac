dd523e67d6e1b7f83af6a2bc05db89dbf9491477
Thomas Kioko <kiokotomas@gmail.com>
2021-07-21 17:38:07 +0200

    Remove extra forward slash

--- shared/src/androidMain/kotlin/com/thomaskioko/tvmaniac/util/StringUtil.kt
+++ shared/src/androidMain/kotlin/com/thomaskioko/tvmaniac/util/StringUtil.kt
@@ -1,6 +1,6 @@
 package com.thomaskioko.tvmaniac.util
 
-const val POSTER_PATH = "https://image.tmdb.org/t/p/original/%s"
+const val POSTER_PATH = "https://image.tmdb.org/t/p/original%s"
 
 internal actual object StringUtil {
     actual fun formatPosterPath(imageUrl: String): String {
