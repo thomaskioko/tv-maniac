f1b15bd5258c3723f0d5f32e41e2f040cacd6650
Thomas Kioko <kiokotomas@gmail.com>
2021-07-21 12:45:18 +0200

    Create string util class, format poster image.

---
+++ shared/src/androidMain/kotlin/com/thomaskioko/tvmaniac/util/StringUtil.kt
@@ -0,0 +1,9 @@
+package com.thomaskioko.tvmaniac.util
+
+const val POSTER_PATH = "https://image.tmdb.org/t/p/original/%s"
+
+internal actual object StringUtil {
+    actual fun formatPosterPath(imageUrl: String): String {
+        return String.format(POSTER_PATH, imageUrl)
+    }
+}
---
+++ shared/src/iosMain/kotlin/com/thomaskioko/tvmaniac/util/StringUtil.kt
@@ -0,0 +1,7 @@
+package com.thomaskioko.tvmaniac.util
+
+internal actual object StringUtil {
+    actual fun formatPosterPath(imageUrl: String): String {
+        return ""
+    }
+}
