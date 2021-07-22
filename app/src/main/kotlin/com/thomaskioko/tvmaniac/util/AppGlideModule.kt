32da7421834bd4303293eb38b87cff61b15dcc13
Thomas Kioko <kiokotomas@gmail.com>
2021-07-22 20:44:56 +0200

    Create glide module

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/util/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package com.thomaskioko.tvmaniac.util
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class PlasmaAppGlideModule : AppGlideModule()
