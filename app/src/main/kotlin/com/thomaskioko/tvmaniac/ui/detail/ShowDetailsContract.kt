7f118a5ab8a67cb01f81176e1b32c051b0fbcec1
Thomas Kioko <kiokotomas@gmail.com>
2021-07-28 20:51:37 +0200

    Fetch trailer information

--- app/src/main/kotlin/com/thomaskioko/tvmaniac/ui/detail/ShowDetailsContract.kt
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/ui/detail/ShowDetailsContract.kt
@@ -4,6 +4,7 @@ import com.thomaskioko.tvmaniac.interactor.EpisodeQuery
 import com.thomaskioko.tvmaniac.presentation.model.Episode
 import com.thomaskioko.tvmaniac.presentation.model.GenreModel
 import com.thomaskioko.tvmaniac.presentation.model.Season
+import com.thomaskioko.tvmaniac.presentation.model.TrailerModel
 import com.thomaskioko.tvmaniac.presentation.model.TvShow
 
 sealed class ShowDetailAction {
@@ -19,7 +20,8 @@ data class ShowDetailViewState(
     val tvShow: TvShow = TvShow.EMPTY_SHOW,
     val tvSeasons: List<Season> = emptyList(),
     val genreList: List<GenreModel> = emptyList(),
-    val episodesViewState: EpisodesViewState = EpisodesViewState()
+    val episodesViewState: EpisodesViewState = EpisodesViewState(),
+    val trailerViewState: TrailersViewState = TrailersViewState()
 ) {
     companion object {
         val Empty = ShowDetailViewState()
@@ -33,5 +35,15 @@ data class EpisodesViewState(
 ) {
     companion object {
         val Empty = EpisodesViewState()
+    }
+}
+
+data class TrailersViewState(
+    val isLoading: Boolean = false,
+    val errorMessage: String = "",
+    val trailerList: List<TrailerModel> = emptyList(),
+) {
+    companion object {
+        val Empty = TrailersViewState()
     }
 }
