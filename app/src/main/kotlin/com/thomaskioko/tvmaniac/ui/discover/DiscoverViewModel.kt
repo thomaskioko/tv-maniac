968959d7106f8af23e1dda48e4bb4cf2ee973196
Thomas Kioko <kiokotomas@gmail.com>
2021-07-31 18:55:59 +0200

    Update viewModel class implementation. Use store class from shared core module

--- app/src/main/kotlin/com/thomaskioko/tvmaniac/ui/discover/DiscoverViewModel.kt
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/ui/discover/DiscoverViewModel.kt
@@ -1,9 +1,11 @@
 package com.thomaskioko.tvmaniac.ui.discover
 
-import com.thomaskioko.stargazer.core.presentation.ViewAction
-import com.thomaskioko.stargazer.core.presentation.ViewState
-import com.thomaskioko.tvmaniac.core.BaseViewModel
-import com.thomaskioko.tvmaniac.core.annotations.DefaultDispatcher
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.thomaskioko.tvmaniac.core.Store
+import com.thomaskioko.tvmaniac.core.discover.DiscoverShowAction
+import com.thomaskioko.tvmaniac.core.discover.DiscoverShowEffect
+import com.thomaskioko.tvmaniac.core.discover.DiscoverShowState
 import com.thomaskioko.tvmaniac.datasource.enums.TrendingDataRequest
 import com.thomaskioko.tvmaniac.datasource.enums.TrendingDataRequest.FEATURED
 import com.thomaskioko.tvmaniac.datasource.enums.TrendingDataRequest.POPULAR
@@ -13,63 +15,63 @@ import com.thomaskioko.tvmaniac.interactor.GetTrendingShowsInteractor
 import com.thomaskioko.tvmaniac.presentation.model.TvShow
 import com.thomaskioko.tvmaniac.util.DomainResultState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DiscoverViewModel @Inject constructor(
     private val interactor: GetTrendingShowsInteractor,
-    @DefaultDispatcher private val ioDispatcher: CoroutineDispatcher
-) : BaseViewModel<DiscoverShowsAction, DiscoverShowsState>(
-    dispatcher = ioDispatcher,
-    initialViewState = DiscoverShowsState.Loading
-) {
+) : Store<DiscoverShowState, DiscoverShowAction, DiscoverShowEffect>, ViewModel() {
+
+    private val state = MutableStateFlow(DiscoverShowState(false, linkedMapOf()))
+    private val sideEffect = MutableSharedFlow<DiscoverShowEffect>()
 
     init {
-        dispatchAction(
-            DiscoverShowsAction.LoadTvShows(
-                listOf(FEATURED, TODAY, THIS_WEEK, POPULAR)
-            )
+        dispatch(
+            DiscoverShowAction.LoadTvShows(listOf(FEATURED, TODAY, THIS_WEEK, POPULAR))
         )
     }
 
-    override fun handleAction(action: DiscoverShowsAction) {
+    override fun observeState(): StateFlow<DiscoverShowState> = state
+
+    override fun observeSideEffect(): Flow<DiscoverShowEffect> = sideEffect
+
+    override fun dispatch(action: DiscoverShowAction) {
+        val oldState = state.value
+
         when (action) {
-            is DiscoverShowsAction.LoadTvShows -> {
-                interactor.invoke(action.trendingDataRequest)
-                    .onEach { mutableViewState.emit(it.reduce()) }
-                    .stateIn(
-                        ioScope,
-                        SharingStarted.Eagerly,
-                        emptyMap<TrendingDataRequest, List<TvShow>>()
-                    )
+            is DiscoverShowAction.LoadTvShows -> {
+                viewModelScope.launch {
+                    interactor.invoke(action.trendingDataRequest)
+                        .collect {
+                            state.emit(it.stateReducer(oldState))
+                        }
+                }
             }
+            is DiscoverShowAction.Error -> {
+                viewModelScope.launch {
+                    sideEffect.emit(DiscoverShowEffect.Error(action.message))
+                }
+            }
+        }
+    }
+
+    private fun DomainResultState<LinkedHashMap<TrendingDataRequest, List<TvShow>>>.stateReducer(
+        state: DiscoverShowState,
+    ): DiscoverShowState {
+        return when (this) {
+            is DomainResultState.Error -> {
+                dispatch(DiscoverShowAction.Error(message))
+                state.copy(isLoading = false)
+            }
+            is DomainResultState.Loading -> state.copy(isLoading = true)
+            is DomainResultState.Success -> state.copy(isLoading = false, dataMap = data)
         }
     }
 }
 
-private fun DomainResultState<LinkedHashMap<TrendingDataRequest, List<TvShow>>>.reduce(): DiscoverShowsState {
-    return when (this) {
-        is DomainResultState.Error -> DiscoverShowsState.Error(message)
-        is DomainResultState.Loading -> DiscoverShowsState.Loading
-        is DomainResultState.Success -> DiscoverShowsState.Success(data)
-    }
-}
-
-sealed class DiscoverShowsState : ViewState {
-    object Loading : DiscoverShowsState()
-    data class Success(
-        val dataMap: LinkedHashMap<TrendingDataRequest, List<TvShow>>
-    ) : DiscoverShowsState()
-
-    data class Error(val message: String = "") : DiscoverShowsState()
-}
-
-sealed class DiscoverShowsAction : ViewAction {
-    data class LoadTvShows(
-        val trendingDataRequest: List<TrendingDataRequest>
-    ) : DiscoverShowsAction()
-}
