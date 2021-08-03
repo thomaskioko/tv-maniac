e1eac6f2a8ed220b70f37ad5e787b7d3bf2d3f11
kioko <kiokotomas@gmail.com>
2021-08-03 18:37:14 +0200

    Add padding helper util class

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/compose/util/PaddingValues.kt
@@ -0,0 +1,29 @@
+package com.thomaskioko.tvmaniac.compose.util
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PaddingValues.copy(
+    copyStart: Boolean = true,
+    copyTop: Boolean = true,
+    copyEnd: Boolean = true,
+    copyBottom: Boolean = true,
+): PaddingValues {
+    return remember(this) {
+        derivedStateOf {
+            PaddingValues(
+                start = if (copyStart) calculateStartPadding(LayoutDirection.Ltr) else 0.dp,
+                top = if (copyTop) calculateTopPadding() else 0.dp,
+                end = if (copyEnd) calculateEndPadding(LayoutDirection.Ltr) else 0.dp,
+                bottom = if (copyBottom) calculateBottomPadding() else 0.dp,
+            )
+        }
+    }.value
+}
