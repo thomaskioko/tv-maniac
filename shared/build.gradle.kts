bde412a14aaa9b9250b71a4e16d541e4a415cd3e
kioko <kiokotomas@gmail.com>
2021-08-06 20:24:42 +0200

    Remove plugin class

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,12 +5,12 @@ import java.io.FileInputStream
 import java.util.*
 
 plugins {
-    kotlin(Plugins.multiplatform)
-    kotlin(Plugins.cocoapods)
-    kotlin(Plugins.serialization) version ("1.5.10")
-    id(Plugins.androidLibrary)
-    id(Plugins.buildkonfig)
-    id(Plugins.sqlDelight)
+    kotlin("multiplatform")
+    kotlin("native.cocoapods")
+    kotlin("plugin.serialization") version ("1.5.20")
+    id("com.android.library")
+    id("com.codingfeline.buildkonfig")
+    id("com.squareup.sqldelight")
 }
 
 version = "1.0"
