5f94e3f8bf1ef12372ff35c946b5fc7e9d79bfd3
kioko <kiokotomas@gmail.com>
2021-08-09 14:53:37 +0200

    Add empty watchlist view.

--- app-features/watchlist/src/main/kotlin/com/thomaskioko/tvmaniac/watchlist/WatchListScreen.kt
+++ app-features/watchlist/src/main/kotlin/com/thomaskioko/tvmaniac/watchlist/WatchListScreen.kt
@@ -1,11 +1,60 @@
 package com.thomaskioko.tvmaniac.watchlist
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.thomaskioko.tvmaniac.compose.R
+import com.thomaskioko.tvmaniac.compose.components.ColumnSpacer
 
 @Composable
 fun WatchListScreen(
     viewModel: WatchlistViewModel,
     navController: NavHostController,
 ) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_watchlist_empty),
+            colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface.copy(alpha = 0.8F)),
+            modifier = Modifier.size(96.dp),
+            contentDescription = null
+        )
+
+        ColumnSpacer(value = 12)
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = stringResource(id = R.string.msg_empty_watchlist),
+                style = MaterialTheme.typography.body2,
+                lineHeight = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 48.dp),
+            )
+        }
+    }
 }
