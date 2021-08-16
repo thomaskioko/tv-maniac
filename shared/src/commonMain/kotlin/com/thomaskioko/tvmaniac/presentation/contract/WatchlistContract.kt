6b60019d5f237c71c99828c68f251f1b47e17a93
kioko <kiokotomas@gmail.com>
2021-08-16 13:34:54 +0200

    Add watchlist screen

---
+++ shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/presentation/contract/WatchlistContract.kt
@@ -0,0 +1,28 @@
+package com.thomaskioko.tvmaniac.presentation.contract
+
+import com.thomaskioko.tvmaniac.core.Action
+import com.thomaskioko.tvmaniac.core.Effect
+import com.thomaskioko.tvmaniac.core.State
+import com.thomaskioko.tvmaniac.presentation.model.TvShow
+
+
+data class WatchlistState(
+    val isLoading: Boolean,
+    val list: List<TvShow>
+) : State {
+    companion object {
+        val Empty = WatchlistState(
+            isLoading = true,
+            list = emptyList()
+        )
+    }
+}
+
+sealed class WatchlistAction : Action {
+    object LoadWatchlist: WatchlistAction()
+    data class Error(val message: String = "") : WatchlistAction()
+}
+
+sealed class WatchlistEffect : Effect {
+    data class Error(val message: String = "") : WatchlistEffect()
+}
