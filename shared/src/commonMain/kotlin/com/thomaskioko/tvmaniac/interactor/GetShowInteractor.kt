d2165907171dcb91d0e130eb1cb22506f691df99
kioko <kiokotomas@gmail.com>
2021-08-16 13:32:12 +0200

    Simplify interactor

--- shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/interactor/GetShowInteractor.kt
+++ shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/interactor/GetShowInteractor.kt
@@ -1,24 +1,20 @@
 package com.thomaskioko.tvmaniac.interactor
 
-import com.thomaskioko.tvmaniac.presentation.model.TvShow
 import com.thomaskioko.tvmaniac.datasource.repository.tvshow.TvShowsRepository
+import com.thomaskioko.tvmaniac.presentation.model.TvShow
 import com.thomaskioko.tvmaniac.util.DomainResultState
 import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.error
-import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.loading
 import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.success
 import com.thomaskioko.tvmaniac.util.Interactor
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class GetShowInteractor constructor(
     private val repository: TvShowsRepository,
 ) : Interactor<Int, TvShow>() {
 
-    override fun run(params: Int): Flow<DomainResultState<TvShow>> = flow {
-        emit(loading())
-
-        emit(success(repository.getTvShow(params)))
-    }
+    override fun run(params: Int): Flow<DomainResultState<TvShow>> = repository.getTvShow(params)
+        .map { success(it) }
         .catch { emit(error(it)) }
 }
