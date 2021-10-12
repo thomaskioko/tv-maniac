4b08345e6eb32e8ca1658957dfae97b73de6ab38
kioko <kiokotomas@gmail.com>
2021-10-12 16:35:57 +0200

    Add scope annotations module.

---
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/injection/CoroutineScopeModule.kt
@@ -0,0 +1,24 @@
+package com.thomaskioko.tvmaniac.injection
+
+import com.thomaskioko.tvmaniac.core.annotations.IoCoroutineScope
+import com.thomaskioko.tvmaniac.core.annotations.MainCoroutineScope
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutineScopeModule {
+
+    @Provides
+    @MainCoroutineScope
+    fun provideMainCoroutineScope(): CoroutineScope = CoroutineScope(Job() + Dispatchers.Main)
+
+    @Provides
+    @IoCoroutineScope
+    fun provideIOCoroutineScope(): CoroutineScope = CoroutineScope(Job() + Dispatchers.IO)
+}
