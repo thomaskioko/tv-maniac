cbac0088c86ce79d43ec9faba4a18d891acd4834
kioko <kiokotomas@gmail.com>
2021-11-13 22:08:25 +0300

    Update logging implementation.

--- shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/Logger.kt
+++ shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/Logger.kt
@@ -1,3 +1,11 @@
 package com.thomaskioko.tvmaniac.util
 
-internal expect val isDebug: Boolean
+expect class Logger(
+    className: String,
+) {
+    fun log(msg: String)
+}
+
+fun printLogD(className: String?, message: String ) {
+    println("$className: $message")
+}
