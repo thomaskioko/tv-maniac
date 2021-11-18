2ad3438dcb12a10375985b21c7b7cab43ed41713
kioko <kiokotomas@gmail.com>
2021-11-18 13:34:28 +0100

    Use java 8 and add language feature support: enable desugar

--- buildSrc/src/main/kotlin/android-feature-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/android-feature-plugin.gradle.kts
@@ -18,12 +18,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 
     buildFeatures {
@@ -38,7 +40,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.get().toString()
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.get()
     }
 }
 
