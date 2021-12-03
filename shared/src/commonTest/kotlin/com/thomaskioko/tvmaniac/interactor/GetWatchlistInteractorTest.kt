d20f03c254a44723427c977d1db39f7d93edc464
kioko <kiokotomas@gmail.com>
2021-12-03 18:18:51 +0100

    Fix failing tests and update implementation to match interface requirements.

--- shared/src/commonTest/kotlin/com/thomaskioko/tvmaniac/interactor/GetWatchlistInteractorTest.kt
+++ shared/src/commonTest/kotlin/com/thomaskioko/tvmaniac/interactor/GetWatchlistInteractorTest.kt
@@ -2,13 +2,14 @@ package com.thomaskioko.tvmaniac.interactor
 
 import app.cash.turbine.test
 import com.thomaskioko.tvmaniac.MockData.getTvResponse
-import com.thomaskioko.tvmaniac.datasource.mapper.toTvShow
+import com.thomaskioko.tvmaniac.datasource.mapper.toShow
+import com.thomaskioko.tvmaniac.datasource.mapper.toTvShowList
 import com.thomaskioko.tvmaniac.datasource.repository.tvshow.TvShowsRepository
 import com.thomaskioko.tvmaniac.util.DomainResultState
 import com.thomaskioko.tvmaniac.util.invoke
 import com.thomaskioko.tvmaniac.util.runBlocking
 import io.kotest.matchers.shouldBe
-import io.mockk.every
+import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.flow.flowOf
 import kotlin.test.Test
@@ -19,14 +20,14 @@ internal class GetWatchlistInteractorTest {
     private val interactor = GetWatchListInteractor(repository)
 
     @Test
-    fun wheneverPopularShowsInteractorIsInvoked_ExpectedDataIsReturned() = runBlocking {
+    fun wheneverGetWatchlistInteractorIsInvoked_ExpectedDataIsReturned() = runBlocking {
         val result = getTvResponse().results
-            .map { it.toTvShow() }
+            .map { it.toShow() }
 
-        every { runBlocking { repository.getWatchlist() } } returns flowOf(result)
+        coEvery { repository.getWatchlist() } returns flowOf(result)
 
         interactor.invoke().test {
-            awaitItem() shouldBe DomainResultState.Success(result)
+            awaitItem() shouldBe DomainResultState.Success(result.toTvShowList())
             awaitComplete()
         }
     }
--- shared/src/commonTest/kotlin/com/thomaskioko/tvmaniac/interactor/PopularShowsInteractorTest.kt
+++ shared/src/commonTest/kotlin/com/thomaskioko/tvmaniac/interactor/PopularShowsInteractorTest.kt
@@ -3,7 +3,8 @@ package com.thomaskioko.tvmaniac.interactor
 import app.cash.turbine.test
 import com.thomaskioko.tvmaniac.MockData.getTvResponse
 import com.thomaskioko.tvmaniac.datasource.enums.ShowCategory
-import com.thomaskioko.tvmaniac.datasource.mapper.toTvShow
+import com.thomaskioko.tvmaniac.datasource.mapper.toShow
+import com.thomaskioko.tvmaniac.datasource.mapper.toTvShowList
 import com.thomaskioko.tvmaniac.datasource.repository.tvshow.TvShowsRepository
 import com.thomaskioko.tvmaniac.util.DomainResultState
 import com.thomaskioko.tvmaniac.util.invoke
@@ -21,18 +22,13 @@ internal class PopularShowsInteractorTest {
     @Test
     fun wheneverPopularShowsInteractorIsInvoked_ExpectedDataIsReturned() = runBlocking {
         val result = getTvResponse().results
-            .map { it.toTvShow() }
-            .map {
-                it.copy(
-                    showCategory = ShowCategory.POPULAR
-                )
-            }
+            .map { it.toShow() }
 
-        coEvery { repository.getPopularTvShows(1) } returns result
+        coEvery { repository.getShowsByCategoryId(1, ShowCategory.POPULAR.type) } returns result
 
         interactor.invoke().test {
             awaitItem() shouldBe DomainResultState.Loading()
-            awaitItem() shouldBe DomainResultState.Success(result)
+            awaitItem() shouldBe DomainResultState.Success(result.toTvShowList())
             awaitComplete()
         }
     }
@@ -40,7 +36,8 @@ internal class PopularShowsInteractorTest {
     @Test
     fun wheneverPopularShowsInteractorIsInvoked_ErrorIsReturned() = runBlocking {
 
-        coEvery { repository.getPopularTvShows(1) }.throws(Exception("Something went wrong"))
+        coEvery { repository.getShowsByCategoryId(1, ShowCategory.POPULAR.type) }
+            .throws(Exception("Something went wrong"))
 
         interactor.invoke().test {
             awaitItem() shouldBe DomainResultState.Loading()
