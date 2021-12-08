3f67a95436a2fefabe42800519c73cd345dc1c93
kioko <kiokotomas@gmail.com>
2021-12-08 12:36:35 +0100

    Fix status bar icons visibility. Remove xml theme and use systemUIController

--- app/src/main/kotlin/com/thomaskioko/tvmaniac/ui/MainActivity.kt
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/ui/MainActivity.kt
@@ -3,8 +3,16 @@ package com.thomaskioko.tvmaniac.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.thomaskioko.tvmaniac.compose.theme.DarkColors
+import com.thomaskioko.tvmaniac.compose.theme.LightColors
 import com.thomaskioko.tvmaniac.compose.theme.TvManiacTheme
 import com.thomaskioko.tvmaniac.home.HomeScreen
 import com.thomaskioko.tvmaniac.navigation.ComposeNavigationFactory
@@ -28,9 +36,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProvideWindowInsets(consumeWindowInsets = false) {
                 TvManiacTheme(darkTheme = themePreference.shouldUseDarkColors()) {
+                    SetupTheme()
                     HomeScreen(composeNavigationFactories)
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun SetupTheme() {
+        val systemUiController = rememberSystemUiController()
+        val isLightTheme = !themePreference.shouldUseDarkColors()
+
+        val systemBarColor = MaterialTheme.colors.surface.copy(alpha = 0.0f)
+        val transparentColor: (Color) -> Color = { original ->
+            systemBarColor.compositeOver(original)
+        }
+        SideEffect {
+            systemUiController.setSystemBarsColor(
+                color = Color.Transparent,
+                darkIcons = isLightTheme
+            )
+
+            systemUiController.setStatusBarColor(
+                color = Color.Transparent,
+                darkIcons = isLightTheme,
+                transformColorForLightContent = transparentColor
+            )
+
+            systemUiController.setNavigationBarColor(
+                color = if (isLightTheme) LightColors.surface else DarkColors.primary,
+                darkIcons = isLightTheme,
+                navigationBarContrastEnforced = false,
+                transformColorForLightContent = transparentColor
+            )
         }
     }
 }
