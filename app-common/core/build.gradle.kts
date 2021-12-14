b50a23ab7e077d55d41de2b1329a43c92d29f5e1
kioko <kiokotomas@gmail.com>
2021-12-14 16:53:54 +0100

    Remove unused dependencies.

--- app-common/core/build.gradle.kts
+++ app-common/core/build.gradle.kts
@@ -5,7 +5,6 @@ import util.libs
 plugins {
     id("com.android.library")
     kotlin("android")
-    kotlin("kapt")
 }
 
 android {
@@ -20,16 +19,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
 
     implementation(libs.hilt.android)
-
-    implementation(libs.androidx.lifecycle.viewmodel)
-    implementation(libs.androidx.lifecycle.runtime)
-    kapt(libs.androidx.lifecycle.compiler)
 }
