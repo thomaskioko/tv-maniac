99f23646c3949836bdeb8ce2219e09561522c944
kioko <kiokotomas@gmail.com>
2021-12-26 14:56:51 +0100

    Implement flow interactor, migrate away from DomainResult wrapper class.

--- shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/interactor/SeasonsInteractor.kt
+++ shared/src/commonMain/kotlin/com/thomaskioko/tvmaniac/interactor/SeasonsInteractor.kt
@@ -3,28 +3,17 @@ package com.thomaskioko.tvmaniac.interactor
 import com.thomaskioko.tvmaniac.core.usecase.FlowInteractor
 import com.thomaskioko.tvmaniac.datasource.repository.seasons.SeasonsRepository
 import com.thomaskioko.tvmaniac.presentation.model.Season
-import com.thomaskioko.tvmaniac.util.DomainResultState
-import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.error
-import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.loading
-import com.thomaskioko.tvmaniac.util.DomainResultState.Companion.success
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flow
 
 class SeasonsInteractor constructor(
     private val repository: SeasonsRepository,
-) : FlowInteractor<Int, DomainResultState<List<Season>>>() {
+) : FlowInteractor<Int, List<Season>>() {
 
-    override fun run(params: Int): Flow<DomainResultState<List<Season>>> = flow {
-        emit(loading())
+    override fun run(params: Int): Flow<List<Season>> = flow {
 
-        emit(success(repository.getSeasonListByTvShowId(params)))
+        emit(repository.getSeasonListByTvShowId(params))
     }
-        .catch {
-            when (it) {
-                is NullPointerException ->
-                    emit(error(Throwable("No data for season with Show id:: $params")))
-                else -> emit(error(it))
-            }
-        }
+        .distinctUntilChanged()
 }
