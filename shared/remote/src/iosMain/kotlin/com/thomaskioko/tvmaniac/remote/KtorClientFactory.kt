44fe61953d2cb3a5c750e9741aa3fba7a5f74444
kioko <kiokotomas@gmail.com>
2022-01-08 12:36:32 +0100

    Move ktor implementation to remote module

--- shared/remote/src/iosMain/kotlin/com/thomaskioko/tvmaniac/remote/KtorClientFactory.kt
+++ shared/remote/src/iosMain/kotlin/com/thomaskioko/tvmaniac/remote/KtorClientFactory.kt
@@ -1,6 +1,5 @@
-package com.thomaskioko.tvmaniac.datasource.network
+package com.thomaskioko.tvmaniac.remote
 
-import com.thomaskioko.tvmaniac.shared.BuildKonfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.ios.Ios
 import io.ktor.client.features.DefaultRequest
@@ -9,11 +8,14 @@ import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logger
 import io.ktor.client.features.logging.Logging
-import io.ktor.client.features.logging.SIMPLE
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
+import io.ktor.http.URLBuilder
+import co.touchlab.kermit.Logger.Companion as KermitLogger
+
+private const val TMDB_API_KEY = "https://api.themoviedb.org/3/"
 
 actual class KtorClientFactory {
     actual fun build(): HttpClient {
@@ -27,11 +29,20 @@ actual class KtorClientFactory {
             }
 
             install(Logging) {
-                logger = Logger.SIMPLE
                 level = LogLevel.INFO
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        KermitLogger.d { message }
+                    }
+                }
             }
 
             install(DefaultRequest) {
+                val endpointUrlBuilder = URLBuilder(TMDB_API_KEY)
+                url {
+                    protocol = endpointUrlBuilder.protocol
+                    host = endpointUrlBuilder.host
+                }
                 header(HttpHeaders.ContentType, ContentType.Application.Json)
                 parameter("api_key", BuildKonfig.TMDB_API_KEY)
             }
