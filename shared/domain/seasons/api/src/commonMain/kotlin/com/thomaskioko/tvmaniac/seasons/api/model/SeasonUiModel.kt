53712bfacd0e0ee6a334a851b8b2862e88947eab
kioko <kiokotomas@gmail.com>
2022-01-09 20:10:37 +0100

    Remove unused variable

--- shared/domain/seasons/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/seasons/api/model/SeasonUiModel.kt
+++ shared/domain/seasons/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/seasons/api/model/SeasonUiModel.kt
@@ -1,13 +1,10 @@
 package com.thomaskioko.tvmaniac.seasons.api.model
 
-import com.thomaskioko.tvmaniac.seasons.api.model.EpisodeUiModel
-
 data class SeasonUiModel(
     val seasonId: Int,
     val tvShowId: Int,
     val name: String,
     val overview: String,
     val seasonNumber: Int,
-    val episodeCount: Int,
-    val episodeList: List<EpisodeUiModel> = emptyList()
+    val episodeCount: Int
 )
