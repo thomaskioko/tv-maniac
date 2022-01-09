1862d439f062e6cc5587aee0770ce40db566a4eb
kioko <kiokotomas@gmail.com>
2022-01-09 02:50:06 +0100

    Add modules.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,6 +24,9 @@ include(
     ":app-features:settings",
     ":shared",
     ":shared:core",
+    ":shared:core-test",
     ":shared:database",
-    ":shared:remote"
+    ":shared:remote",
+    ":shared:domain:discover:api",
+    ":shared:domain:discover:implementation"
 )
