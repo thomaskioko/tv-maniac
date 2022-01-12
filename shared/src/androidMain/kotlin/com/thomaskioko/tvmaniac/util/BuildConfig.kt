70d0dd1d798cb46d2b6420099388317278f3f1e3
kioko <kiokotomas@gmail.com>
2022-01-12 15:12:27 +0100

    Delete unused classes.

--- shared/src/androidMain/kotlin/com/thomaskioko/tvmaniac/util/BuildConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.thomaskioko.tvmaniac.util
-
-import com.thomaskioko.tvmaniac.shared.BuildConfig
-
-actual class BuildConfig {
-    actual fun isDebug() = BuildConfig.DEBUG
-    actual fun isAndroid() = true
-}
