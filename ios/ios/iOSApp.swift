6b8dbd1615257dedbcaae4f5b1198bd2787bcaa7
kioko <kiokotomas@gmail.com>
2022-01-13 18:14:19 +0100

    Init koin modules

--- ios/ios/iOSApp.swift
+++ ios/ios/iOSApp.swift
@@ -15,7 +15,7 @@ struct iOSApp: App {
 
 class AppDelegate : NSObject, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		KoinKt.doInitKoin()
+		KoiniOSKt.doInitKoinIos()
 		return true
 	}
 }
