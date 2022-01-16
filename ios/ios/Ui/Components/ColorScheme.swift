6c5562937a98ac1b8d4f209d64a79849cb2525d7
kioko <kiokotomas@gmail.com>
2022-01-16 19:20:48 +0100

    Create reusable ui components

---
+++ ios/ios/Ui/Components/ColorScheme.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  tv-maniac
+//
+//  Created by Thomas Kioko on 15.01.22.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+
+import Foundation
+import SwiftUI
+
+extension Color {
+	public static var yellow_300: Color {
+		Color("yellow_300", bundle: nil)
+	}
+	public static var yellow_500: Color {
+		Color("yellow_500", bundle: nil)
+	}
+}
