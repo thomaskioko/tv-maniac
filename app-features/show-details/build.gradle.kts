e837ee463b853bb3c4917e4755ac3f1be912d85a
kioko <kiokotomas@gmail.com>
2022-01-17 11:02:37 +0100

    rename discover module to show

--- app-features/show-details/build.gradle.kts
+++ app-features/show-details/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
     implementation(project(":shared:interactors"))
-    implementation(project(":shared:domain:discover:api"))
+    implementation(project(":shared:domain:show:api"))
     implementation(project(":shared:domain:episodes:api"))
     implementation(project(":shared:domain:genre:api"))
     implementation(project(":shared:domain:seasons:api"))
