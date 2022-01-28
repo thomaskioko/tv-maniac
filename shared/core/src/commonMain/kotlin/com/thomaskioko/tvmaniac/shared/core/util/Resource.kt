f5c6712261705baebc16d2bc789ff4c021659a98
kioko <kiokotomas@gmail.com>
2022-01-28 16:07:00 +0100

    Improve exception handling.

--- shared/core/src/commonMain/kotlin/com/thomaskioko/tvmaniac/shared/core/util/Resource.kt
+++ shared/core/src/commonMain/kotlin/com/thomaskioko/tvmaniac/shared/core/util/Resource.kt
@@ -7,14 +7,14 @@ package com.thomaskioko.tvmaniac.shared.core.util
  * A generic class that holds a value with its loading status.
  * @param <T>
 </T> */
-data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+data class Resource<out T>(val status: Status, val data: T?, val throwable: Throwable?) {
     companion object {
         fun <T> success(data: T?): Resource<T> {
             return Resource(Status.SUCCESS, data, null)
         }
 
-        fun <T> error(msg: String, data: T?): Resource<T> {
-            return Resource(Status.ERROR, data, msg)
+        fun <T> error(throwable: Throwable, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, throwable)
         }
 
         fun <T> loading(data: T?): Resource<T> {
