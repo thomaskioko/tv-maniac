f62e661805589e2b460e0169890666b9e54c7b2c
kioko <kiokotomas@gmail.com>
2022-01-28 14:46:12 +0100

    Remove season adapter.

--- shared/database/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/db/TvManiacDatabaseFactory.kt
+++ shared/database/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/db/TvManiacDatabaseFactory.kt
@@ -1,7 +1,6 @@
 package com.thomaskioko.tvmaniac.core.db
 
 import com.squareup.sqldelight.db.SqlDriver
-import com.thomaskioko.tvmaniac.datasource.cache.Season
 import com.thomaskioko.tvmaniac.datasource.cache.Show
 import com.thomaskioko.tvmaniac.datasource.cache.TvManiacDatabase
 
@@ -14,9 +13,6 @@ class TvManiacDatabaseFactory(
             driver = driverFactory.createDriver(),
             showAdapter = Show.Adapter(
                 genre_idsAdapter = intAdapter,
-            ),
-            seasonAdapter = Season.Adapter(
-                episode_idsAdapter = intAdapter
             )
         )
     }
