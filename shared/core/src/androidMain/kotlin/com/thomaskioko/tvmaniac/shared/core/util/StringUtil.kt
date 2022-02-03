171f26ab859aeff6495fd3a8ab0ebc7f709269ce
kioko <kiokotomas@gmail.com>
2022-02-03 21:29:22 +0100

    Move date formatting to dateUtil class

--- shared/core/src/androidMain/kotlin/com/thomaskioko/tvmaniac/shared/core/util/StringUtil.kt
+++ shared/core/src/androidMain/kotlin/com/thomaskioko/tvmaniac/shared/core/util/StringUtil.kt
@@ -1,9 +1,5 @@
 package com.thomaskioko.tvmaniac.shared.core.util
 
-import co.touchlab.kermit.Logger
-import java.text.SimpleDateFormat
-import java.util.Locale
-
 const val POSTER_PATH = "https://image.tmdb.org/t/p/original%s"
 const val DEFAULT_IMAGE_URL =
     "https://play-lh.googleusercontent.com/IO3niAyss5tFXAQP176P0Jk5rg_A_hfKPNqzC4gb15WjLPjo5I-f7oIZ9Dqxw2wPBAg"
@@ -14,20 +10,5 @@ actual object StringUtil {
             POSTER_PATH,
             imageUrl
         )
-    }
-
-    actual fun formatDate(dateString: String?): String {
-        var result = "TBA"
-        try {
-            if (dateString != null) {
-                val date = SimpleDateFormat("yyyy-mm-dd", Locale.getDefault())
-                    .parse(dateString)
-                result = SimpleDateFormat("EEE, MMM d, yyyy", Locale.getDefault())
-                    .format(date)
-            }
-        } catch (exception: Exception) {
-            Logger.e("formatDate::  $dateString ${exception.message}", exception)
-        }
-        return result
     }
 }
