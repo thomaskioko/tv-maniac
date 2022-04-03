6b750c9da13932cbb334b008014dc1241ce0119b
kioko <kiokotomas@gmail.com>
2022-04-03 19:08:54 +0200

    Fix parallax effect and update UI

--- ios/ios/Feature/Detail/ShowBodyView.swift
+++ ios/ios/Feature/Detail/ShowBodyView.swift
@@ -11,107 +11,163 @@ import TvManiac
 import Kingfisher
 
 struct ShowBodyView: View {
-	
-	var viewState: ShowDetailUiViewState
-	let processor = DownsamplingImageProcessor(size: CGSize(width: PosterStyle.Size.medium.width(), height: PosterStyle.Size.medium.height()))
-	|> RoundCornerImageProcessor(cornerRadius: 5)
-	
-	
-	var body: some View {
-		
-		VStack(alignment: .leading) {
-			
-			Text("Browse Seasons")
-				.titleSemiBoldFont(size: 23)
-				.padding(.top, 8)
-				.foregroundColor(Color.text_color_bg)
-				.padding(.trailing, 16)
-				.padding(.leading, 16)
-			
-			ScrollView(.horizontal, showsIndicators: false) {
-				HStack(alignment: .center){
-					ForEach(viewState.seasonList, id: \.self) { season in
-						
-						Button(action: {}){
-							Text(season.name)
-								.bodyMediumFont(size: 16)
-								.foregroundColor(Color.accent)
-								.padding(10)
-								.background(Color.accent.opacity(0.12))
-								.cornerRadius(5)
-						}
-						
-					}
-				}
-				.padding(.trailing, 16)
-				.padding(.leading, 16)
-			}
-			
-			if !viewState.episodeList.isEmpty{
-				Text("Episodes")
-					.titleSemiBoldFont(size: 23)
-					.padding(.top, 8)
-					.foregroundColor(Color.text_color_bg)
-				
-					.padding(.trailing, 16)
-					.padding(.leading, 16)
-				
-				ScrollView(.horizontal, showsIndicators: false) {
-					
-					HStack(alignment: .center){
-						ForEach(viewState.episodeList, id: \.self) { episode in
-							
-							AirEpisodeCardView(episode: episode)
-						}
-					}
-					.padding(.leading, 8)
-					.padding(.bottom, 16)
-				}
-			}
-			
-			
-			if !viewState.similarShowList.isEmpty{
-				Text("More like this")
-					.titleSemiBoldFont(size: 23)
-					.padding(.top, 8)
-					.foregroundColor(Color.text_color_bg)
-					.padding(.trailing, 16)
-					.padding(.leading, 16)
-				
-				ScrollView(.horizontal, showsIndicators: false) {
-					HStack(alignment: .top){
-						
-						ForEach(viewState.similarShowList, id: \.self) { show in
-							NavigationLink(destination: ShowDetailView(showId: show.id)) {
-								ShowPosterImage(
-									processor: processor,
-									posterSize: .medium,
-									imageUrl: show.posterImageUrl
-								)
-							}
-						}
-					}
-					.padding(.trailing, 16)
-					.padding(.leading, 16)
-				}
-			}
-		}
-		.frame(
-			minWidth: 0,
-			maxWidth: .infinity,
-			minHeight: 0,
-			maxHeight: .infinity,
-			alignment: .topLeading
-		)
-		.padding(.bottom, 94)
-		.background(Color.background)
-		
-	}
-	
+
+    @Environment(\.colorScheme) var scheme
+
+    var viewState: ShowDetailUiViewState
+    let processor = DownsamplingImageProcessor(size: CGSize(width: PosterStyle.Size.medium.width(), height: PosterStyle.Size.medium.height()))
+            |> RoundCornerImageProcessor(cornerRadius: 5)
+
+
+    var body: some View {
+
+
+        VStack {
+
+            Text(viewState.tvShow.title)
+                    .titleFont(size: 30)
+                    .foregroundColor(Color.text_color_bg)
+                    .lineLimit(1)
+                    .padding(.top, 8)
+
+            Text(viewState.tvShow.overview)
+                    .bodyFont(size: 18)
+                    .foregroundColor(Color.text_color_bg)
+                    .lineLimit(3)
+                    .padding(.top, 1)
+
+            ShowInfoRow(show: viewState.tvShow)
+                    .padding(.top, 1)
+
+            GenresRowView(genres: viewState.genreList)
+
+            HStack(alignment: .center, spacing: 8) {
+
+                BorderedButton(
+                        text: "Watch Trailer",
+                        systemImageName: "film.fill",
+                        color: .accent,
+                        borderColor: .grey_200,
+                        isOn: false,
+                        action: {
+
+                        })
+
+                BorderedButton(
+                        text: "Follow Show",
+                        systemImageName: "plus.app.fill",
+                        color: .accent,
+                        borderColor: .grey_200,
+                        isOn: false,
+                        action: {
+
+                        })
+            }
+                    .padding(.bottom, 16)
+                    .padding(.top, 10)
+
+        }
+                .padding(.trailing, 16)
+                .padding(.leading, 16)
+                .padding(.bottom, 16)
+                .background(BackgroundView())
+
+        VStack(alignment: .leading) {
+
+
+            Text("Browse Seasons")
+                    .titleSemiBoldFont(size: 23)
+                    .foregroundColor(Color.text_color_bg)
+                    .padding(.trailing, 16)
+                    .padding(.leading, 16)
+                    .padding(.top, 16)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .center) {
+                    ForEach(viewState.seasonList, id: \.self) { season in
+
+                        Button(action: {}) {
+                            Text(season.name)
+                                    .bodyMediumFont(size: 16)
+                                    .foregroundColor(Color.accent)
+                                    .padding(10)
+                                    .background(Color.accent.opacity(0.12))
+                                    .cornerRadius(5)
+                        }
+
+                    }
+                }
+                        .padding(.trailing, 16)
+                        .padding(.leading, 16)
+            }
+
+            if !viewState.episodeList.isEmpty {
+                Text("Episodes")
+                        .titleSemiBoldFont(size: 23)
+                        .padding(.top, 8)
+                        .foregroundColor(Color.text_color_bg)
+                        .padding(.trailing, 16)
+                        .padding(.leading, 16)
+
+                ScrollView(.horizontal, showsIndicators: false) {
+
+                    HStack(alignment: .center) {
+                        ForEach(viewState.episodeList, id: \.self) { episode in
+                            AirEpisodeCardView(episode: episode)
+                        }
+                    }
+                            .padding(.leading, 8)
+                            .padding(.bottom, 16)
+                }
+            }
+
+
+            if !viewState.similarShowList.isEmpty {
+                Text("More like this")
+                        .titleSemiBoldFont(size: 23)
+                        .padding(.top, 8)
+                        .foregroundColor(Color.text_color_bg)
+                        .padding(.trailing, 16)
+                        .padding(.leading, 16)
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top) {
+
+                        ForEach(viewState.similarShowList, id: \.self) { show in
+                            NavigationLink(destination: ShowDetailView(showId: show.id)) {
+                                ShowPosterImage(
+                                        processor: processor,
+                                        posterSize: .medium,
+                                        imageUrl: show.posterImageUrl
+                                )
+                            }
+                        }
+                    }
+                            .padding(.trailing, 16)
+                            .padding(.leading, 16)
+                }
+            }
+        }
+
+                .background(Color.background)
+
+    }
+
+    @ViewBuilder
+    func BackgroundView() -> some View {
+
+        // Blurred Overlay
+        Rectangle()
+                .fill(.ultraThinMaterial)
+                .blur(radius: 50)
+                .mask(Color.linearGradient)
+
+    }
+
 }
 
 struct ShowBodyView_Previews: PreviewProvider {
-	static var previews: some View {
-		ShowBodyView(viewState: viewState)
-	}
+    static var previews: some View {
+        ShowBodyView(viewState: viewState)
+    }
 }
--- ios/ios/Feature/Detail/ShowDetailView.swift
+++ ios/ios/Feature/Detail/ShowDetailView.swift
@@ -8,47 +8,46 @@
 
 import SwiftUI
 import TvManiac
+import Kingfisher
 
 struct ShowDetailView: View {
-	
-	@ObservedObject var observable = ObservableViewModel<ShowDetailsViewModel, ShowDetailUiViewState>(
-		viewModel: ShowDetailsViewModel()
-	)
-	
-	var showId: Int64
-	
-	init(showId: Int64) {
-		self.showId = showId
-	}
-	
-	var body: some View {
-		ZStack {
-			
-			ScrollView(.vertical, showsIndicators: false) {
-				
-				HeaderView(viewState: observable.state)
-					.frame(width: PosterStyle.Size.max.width(), height: PosterStyle.Size.tv.height())
-				
-				
-				ShowBodyView(viewState: observable.state)
-				
-			}
-			
-		}
-		.onAppear {
-			observable.viewModel.attach()
-			observable.viewModel.dispatch(action: ShowDetailAction.LoadShowDetails(showId: self.showId))
-		}
-		.onDisappear {
-			observable.viewModel.detach()
-		}
-		.background(Color.background)
-		.edgesIgnoringSafeArea(.all)
-	}
+
+    @ObservedObject var observable = ObservableViewModel<ShowDetailsViewModel, ShowDetailUiViewState>(
+            viewModel: ShowDetailsViewModel()
+    )
+
+
+    var showId: Int64
+
+    init(showId: Int64) {
+        self.showId = showId
+    }
+
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+
+            HeaderImageView(viewState: observable.state)
+
+            ShowBodyView(viewState: observable.state)
+                    .offset(y: -180)
+
+            Spacer()
+
+        }
+                .onAppear {
+                    observable.viewModel.attach()
+                    observable.viewModel.dispatch(action: ShowDetailAction.LoadShowDetails(showId: self.showId))
+                }
+                .onDisappear {
+                    observable.viewModel.detach()
+                }
+                .edgesIgnoringSafeArea(.all)
+//                .background(Color.background.edgesIgnoringSafeArea(.all))
+    }
 }
 
 struct ShowDetailView_Previews: PreviewProvider {
-	static var previews: some View {
-		ShowDetailView(showId: 1234)
-	}
+    static var previews: some View {
+        ShowDetailView(showId: 1234)
+    }
 }
