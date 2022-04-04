14f8f6e412ef7cf9796eba3789e30bd8845e8c9c
kioko <kiokotomas@gmail.com>
2022-04-04 09:37:10 +0200

    Launch job in IO dispatcher & fix ANR when changing theme

--- android/features/settings/src/main/kotlin/com/thomaskioko/tvmaniac/settings/SettingsViewModel.kt
+++ android/features/settings/src/main/kotlin/com/thomaskioko/tvmaniac/settings/SettingsViewModel.kt
@@ -2,12 +2,14 @@ package com.thomaskioko.tvmaniac.settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.thomaskioko.tvmaniac.core.annotations.DefaultDispatcher
 import com.thomaskioko.tvmaniac.shared.core.ui.Store
 import com.thomaskioko.tvmaniac.shared.persistance.SettingsActions
 import com.thomaskioko.tvmaniac.shared.persistance.SettingsEffect
 import com.thomaskioko.tvmaniac.shared.persistance.SettingsState
 import com.thomaskioko.tvmaniac.shared.persistance.TvManiacPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,7 +19,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val themePreference: TvManiacPreferences
+    private val themePreference: TvManiacPreferences,
+    @DefaultDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : Store<SettingsState, SettingsActions, SettingsEffect>, ViewModel() {
 
     private val state = MutableStateFlow(SettingsState.DEFAULT)
@@ -34,12 +37,12 @@ class SettingsViewModel @Inject constructor(
     override fun dispatch(action: SettingsActions) {
         when (action) {
             is SettingsActions.ThemeSelected -> {
-                viewModelScope.launch {
+                viewModelScope.launch(context = ioDispatcher) {
                     themePreference.emitTheme(action.theme)
                 }
             }
             SettingsActions.ThemeClicked -> {
-                viewModelScope.launch {
+                viewModelScope.launch(context = ioDispatcher) {
                     val newState = state.value.copy(
                         showPopup = !state.value.showPopup
                     )
