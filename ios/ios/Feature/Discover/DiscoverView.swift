3f3129a99a212c8dafa475925633b99762d2b0b9
kioko <kiokotomas@gmail.com>
2022-04-05 21:11:32 +0200

    Update UI, add background, fix tab counter index

--- ios/ios/Feature/Discover/DiscoverView.swift
+++ ios/ios/Feature/Discover/DiscoverView.swift
@@ -1,9 +1,7 @@
 import SwiftUI
 import TvManiac
-import Kingfisher
 
 struct DiscoverView: View {
-
 
     @ObservedObject var observable = ObservableViewModel<DiscoverShowsViewModel, DiscoverShowsState>(
             viewModel: DiscoverShowsViewModel()
@@ -12,10 +10,8 @@ struct DiscoverView: View {
 
     var body: some View {
         NavigationView {
-            ZStack {
-                Color("Background")
-                        .edgesIgnoringSafeArea(.all)
 
+            VStack {
                 if observable.state is DiscoverShowsState.InProgress {
                     LoadingIndicatorView()
                 } else if observable.state is DiscoverShowsState.Error {
@@ -23,99 +19,138 @@ struct DiscoverView: View {
                     EmptyView()
                 } else if observable.state is DiscoverShowsState.Success {
 
-                    VStack {
-                        ScrollView(showsIndicators: false) {
+                    let result = observable.state as? DiscoverShowsState.Success
 
-                            if let result = observable.state as? DiscoverShowsState.Success {
+                    BodyContentView(showResult: result!.data)
 
-								FeaturedShowsView(shows: result.data.featuredShows.tvShows)
-
-                                ShowRow(
-                                        categoryName: result.data.trendingShows.category.title,
-                                        shows: result.data.trendingShows.tvShows
-                                )
-
-                                ShowRow(
-                                        categoryName: result.data.topRatedShows.category.title,
-                                        shows: result.data.topRatedShows.tvShows
-                                )
-
-                                ShowRow(
-                                        categoryName: result.data.popularShows.category.title,
-                                        shows: result.data.popularShows.tvShows
-                                )
-                            }
-                            Spacer()
-                        }
-                    }
                 }
             }
-                    .onAppear {
-                        observable.viewModel.attach()
-                    }
-                    .onDisappear {
-                        observable.viewModel.detach()
-                    }
+                    .ignoresSafeArea()
                     .navigationBarHidden(true)
         }
-                .accentColor(.white)
+                .onAppear {
+                    observable.viewModel.attach()
+                }
+                .onDisappear {
+                    observable.viewModel.detach()
+                }
+                .accentColor(Color.background)
                 .navigationViewStyle(StackNavigationViewStyle())
     }
 
 }
 
-struct FeaturedShowsView: View {
+struct BodyContentView: View {
+
+    // Environment Values
+    @Namespace var animation
+    @Environment(\.colorScheme) var scheme
 
     @SwiftUI.State var currentIndex: Int = 2
 
-    let shows: [TvShow]
-    let resizingProcessor = ResizingImageProcessor(
-            referenceSize: CGSize(width: PosterStyle.Size.big.width(), height: PosterStyle.Size.big.height())
-    ) |> RoundCornerImageProcessor(cornerRadius: 5)
+    let showResult: DiscoverShowsState.DiscoverShowResult
+
 
     var body: some View {
+        ScrollView(showsIndicators: false) {
+            ZStack {
+                BackgroundView()
 
-        ZStack {
-            if shows.count != 0 {
-				
-                TabView(selection: $currentIndex) {
-                    NavigationLink(destination: ShowDetailView(showId: shows[currentIndex].id)) {
-                        SnapCarousel(
-                                spacing: getRect().height < 750 ? 15 : 20,
-                                trailingSpace: getRect().height < 750 ? 100 : 150,
-                                index: $currentIndex,
-                                items: shows
-                        ) { show in
-                            CardView(show: show)
+                VStack {
+                    SnapCarousel(
+                            spacing: 10,
+                            trailingSpace: 110,
+                            index: $currentIndex,
+                            items: showResult.featuredShows.tvShows
+                    ) { show in
+
+                        GeometryReader { proxy in
+                            let size = proxy.size
+                            ShowPosterImage(
+                                    posterSize: .big,
+                                    imageUrl: show.posterImageUrl
+                            )
+                                    .frame(width: size.width, height: size.height)
+                                    .matchedGeometryEffect(id: show.id, in: animation)
                         }
-                                .offset(y: getRect().height / 5.5)
+
                     }
-				}
-                        .ignoresSafeArea()
-                        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-                        .onAppear {
-                            UIPageControl.appearance().currentPageIndicatorTintColor = .white
-                            UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.2)
-                        }
+                            .frame(height: 450)
+                            .padding(.top, 120)
+
+                    CustomIndicator()
+
+                    //Trending
+                    ShowRow(
+                            categoryName: showResult.trendingShows.category.title,
+                            shows: showResult.trendingShows.tvShows
+                    )
+
+                    //Top Rated
+                    ShowRow(
+                            categoryName: showResult.topRatedShows.category.title,
+                            shows: showResult.topRatedShows.tvShows
+                    )
+                    //Popular
+                    ShowRow(
+                            categoryName: showResult.popularShows.category.title,
+                            shows: showResult.popularShows.tvShows
+                    )
+
+                }
+                        .padding(.bottom, 90)
             }
         }
-                .frame(height: 550)
     }
 
     @ViewBuilder
-    func CardView(show: TvShow) -> some View {
+    func BackgroundView() -> some View {
+        GeometryReader { proxy in
+            let size = proxy.size
 
-        VStack(spacing: 10) {
-            GeometryReader { proxy in
-                ShowPosterImage(
-                        processor: resizingProcessor,
-                        posterSize: .big,
-                        imageUrl: show.posterImageUrl
-                )
-                        .frame(height: getRect().height / 2.5)
+            TabView(selection: $currentIndex) {
+                ForEach(showResult.featuredShows.tvShows.indices, id: \.self) { index in
+                    ShowPosterImage(
+                            posterSize: .big,
+                            imageUrl: showResult.featuredShows.tvShows[index].posterImageUrl
+                    )
+                            .frame(width: size.width, height: size.height)
+                            .clipped()
+                            .tag(index)
+                }
+            }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                    .animation(.easeInOut, value: currentIndex)
+
+            let color: Color = (scheme == .dark ? .black : .white)
+            // Custom Gradient
+            LinearGradient(colors: [
+                .black,
+                .clear,
+                color.opacity(0.15),
+                color.opacity(0.5),
+                color.opacity(0.8),
+                color,
+                color
+            ], startPoint: .top, endPoint: .bottom)
+
+            // Blurred Overlay
+            Rectangle()
+                    .fill(.ultraThinMaterial)
+        }
+                .ignoresSafeArea()
+    }
+
+    @ViewBuilder
+    func CustomIndicator() -> some View {
+        HStack(spacing: 5) {
+            ForEach(showResult.featuredShows.tvShows.indices, id: \.self) { index in
+                Circle()
+                        .fill(currentIndex == index ? Color.accent_color : .gray.opacity(0.5))
+                        .frame(width: currentIndex == index ? 10 : 6, height: currentIndex == index ? 10 : 6)
             }
         }
+                .animation(.easeInOut, value: currentIndex)
     }
 
 }
