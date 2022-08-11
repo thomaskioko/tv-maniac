a7efa17e263f3c89ca0ef6855df57be609d9f5a9
kioko <kiokotomas@gmail.com>
2022-08-11 20:31:11 +0200

    Implement logic to play youtube videos.

---
+++ android/features/video-player/src/main/kotlin/com/thomaskioko/tvmaniac/videoplayer/VideoPlayerNavigationFactory.kt
@@ -0,0 +1,28 @@
+package com.thomaskioko.tvmaniac.videoplayer
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.thomaskioko.tvmaniac.navigation.ComposeNavigationFactory
+import com.thomaskioko.tvmaniac.navigation.NavigationScreen
+import com.thomaskioko.tvmaniac.navigation.viewModelBottomSheetComposable
+import javax.inject.Inject
+
+class VideoPlayerNavigationFactory @Inject constructor() : ComposeNavigationFactory {
+
+    override fun create(builder: NavGraphBuilder, navController: NavHostController) {
+        builder.viewModelBottomSheetComposable<VideoPlayerViewModel>(
+            arguments = listOf(
+                navArgument("showId") { type = NavType.LongType },
+                navArgument("videoKey") { type = NavType.StringType },
+            ),
+            route = "${NavigationScreen.VideoPlayerNavScreen.route}/{showId}/{videoKey}",
+            content = {
+                VideoPlayerScreen(
+                    viewModel = this,
+                )
+            }
+        )
+    }
+}
