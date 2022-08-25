ffb4061cc09e908d7b0f1ec3f55b231769099581
kioko <kiokotomas@gmail.com>
2022-08-26 01:45:54 +0200

    Load keys on debug build & update dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import util.libs
 
 plugins {
@@ -6,6 +7,21 @@ plugins {
 
 android {
     namespace = "com.thomaskioko.tvmaniac"
+
+    buildTypes {
+        getByName("debug") {
+            val properties = gradleLocalProperties(rootDir)
+            val traktClientId: String = properties.getProperty("TRAKT_CLIENT_ID")
+            val traktClientSecret: String = properties.getProperty("TRAKT_CLIENT_SECRET")
+            val traktRedirectUri: String = properties.getProperty("TRAKT_REDIRECT_URI")
+            val tmdbApiKey: String = properties.getProperty("TMDB_API_KEY")
+
+            buildConfigField("String", "TRAKT_CLIENT_ID", traktClientId)
+            buildConfigField("String", "TRAKT_CLIENT_SECRET", traktClientSecret)
+            buildConfigField("String", "TRAKT_REDIRECT_URI", traktRedirectUri)
+            buildConfigField("String", "TMDB_API_KEY", tmdbApiKey)
+        }
+    }
 }
 
 dependencies {
@@ -14,6 +30,7 @@ dependencies {
     implementation(projects.android.core.compose)
     implementation(projects.android.core.navigation)
     implementation(projects.android.core.workmanager)
+    implementation(projects.android.core.traktAuth)
     implementation(projects.android.features.discover)
     implementation(projects.android.features.home)
     implementation(projects.android.features.search)
@@ -24,6 +41,7 @@ dependencies {
     implementation(projects.android.features.seasons)
     implementation(projects.android.features.videoPlayer)
 
+    implementation(libs.appauth)
     implementation(libs.hilt.work)
     implementation(libs.androidx.compose.activity)
     implementation(libs.accompanist.systemuicontroller)
