2f5e452a85a582ce48cd665aae03621e88c635bf
kioko <kiokotomas@gmail.com>
2022-08-26 01:35:40 +0200

    Remove dependency block.

--- android/core/compose/build.gradle.kts
+++ android/core/compose/build.gradle.kts
@@ -1,13 +1,7 @@
-import util.libs
-
 plugins {
     `android-compose-plugin`
 }
 
 android {
     namespace = "com.thomaskioko.tvmaniac.compose"
-}
-
-dependencies {
-    implementation(libs.accompanist.insetsui)
 }
