1862db8d5c21c1addb3dabe7c87e71c95afe01c9
kioko <kiokotomas@gmail.com>
2022-08-28 03:14:16 +0200

    Add flow param.

--- shared/core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/util/Combine.kt
+++ shared/core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/util/Combine.kt
@@ -4,16 +4,25 @@ package com.thomaskioko.tvmaniac.core.util
 
 import kotlinx.coroutines.flow.Flow
 
-fun <T1, T2, T3, T4, T5, T6, R> combine(
+fun <T1, T2, T3, T4, T5, T6, T7, R> combine(
     flow: Flow<T1>,
     flow2: Flow<T2>,
     flow3: Flow<T3>,
     flow4: Flow<T4>,
     flow5: Flow<T5>,
     flow6: Flow<T6>,
-    transform: suspend (T1, T2, T3, T4, T5, T6) -> R
+    flow7: Flow<T7>,
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7) -> R
 ): Flow<R> =
-    kotlinx.coroutines.flow.combine(flow, flow2, flow3, flow4, flow5, flow6) { args: Array<*> ->
+    kotlinx.coroutines.flow.combine(
+        flow,
+        flow2,
+        flow3,
+        flow4,
+        flow5,
+        flow6,
+        flow7
+    ) { args: Array<*> ->
         transform(
             args[0] as T1,
             args[1] as T2,
@@ -21,5 +30,6 @@ fun <T1, T2, T3, T4, T5, T6, R> combine(
             args[3] as T4,
             args[4] as T5,
             args[5] as T6,
+            args[6] as T7,
         )
     }
