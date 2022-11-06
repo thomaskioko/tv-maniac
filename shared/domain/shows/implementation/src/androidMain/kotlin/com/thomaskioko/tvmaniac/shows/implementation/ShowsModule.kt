7eb121d15c2df7c6c12adbef0ca154dc6091ed5e
kioko <kiokotomas@gmail.com>
2022-11-06 19:22:35 +0100

    Some cleanup.

--- shared/domain/shows/implementation/src/androidMain/kotlin/com/thomaskioko/tvmaniac/shows/implementation/ShowsModule.kt
+++ shared/domain/shows/implementation/src/androidMain/kotlin/com/thomaskioko/tvmaniac/shows/implementation/ShowsModule.kt
@@ -1,20 +1,14 @@
 package com.thomaskioko.tvmaniac.shows.implementation
 
 import com.thomaskioko.tvmaniac.core.db.TvManiacDatabase
-import com.thomaskioko.tvmaniac.shared.core.ui.di.DefaultDispatcher
 import com.thomaskioko.tvmaniac.shared.core.ui.di.IoDispatcher
-import com.thomaskioko.tvmaniac.shows.api.UpdateShowsInteractor
-import com.thomaskioko.tvmaniac.shows.api.ObserveShowsInteractor
-import com.thomaskioko.tvmaniac.shows.api.ObserveSyncImages
-import com.thomaskioko.tvmaniac.shows.api.cache.CategoryCache
+import com.thomaskioko.tvmaniac.shows.api.ShowsStateMachine
 import com.thomaskioko.tvmaniac.shows.api.cache.ShowCategoryCache
-import com.thomaskioko.tvmaniac.shows.api.cache.ShowImageCache
 import com.thomaskioko.tvmaniac.shows.api.cache.TvShowCache
 import com.thomaskioko.tvmaniac.shows.api.repository.TmdbRepository
-import com.thomaskioko.tvmaniac.shows.implementation.cache.CategoryCacheImpl
 import com.thomaskioko.tvmaniac.shows.implementation.cache.ShowCategoryCacheImpl
-import com.thomaskioko.tvmaniac.shows.implementation.cache.ShowImageCacheImpl
 import com.thomaskioko.tvmaniac.shows.implementation.cache.TvShowCacheImpl
+import com.thomaskioko.tvmaniac.tmdb.api.ShowImageCache
 import com.thomaskioko.tvmaniac.tmdb.api.TmdbService
 import com.thomaskioko.tvmaniac.trakt.api.TraktRepository
 import dagger.Module
@@ -36,42 +30,17 @@ object ShowsModule {
 
     @Singleton
     @Provides
-    fun provideCategoryCache(
-        database: TvManiacDatabase
-    ): CategoryCache = CategoryCacheImpl(database)
-
-    @Singleton
-    @Provides
     fun provideSeasonWithDiscoverCategoryCache(
         database: TvManiacDatabase
     ): ShowCategoryCache = ShowCategoryCacheImpl(database)
 
-    @Singleton
-    @Provides
-    fun provideShowImageCache(database: TvManiacDatabase): ShowImageCache {
-        return ShowImageCacheImpl(database)
-    }
 
     @Singleton
     @Provides
-    fun provideUpdateShowsInteractor(
+    fun provideShowsStateMachine(
         traktRepository: TraktRepository,
-        tmdbRepository: TmdbRepository
-    ): UpdateShowsInteractor = UpdateShowsInteractor(traktRepository, tmdbRepository)
-
-    @Singleton
-    @Provides
-    fun provideObserveShowsInteractor(
-        repository: TraktRepository,
-        @DefaultDispatcher defaultDispatcher: CoroutineDispatcher,
-    ): ObserveShowsInteractor = ObserveShowsInteractor(repository, defaultDispatcher)
-
-    @Singleton
-    @Provides
-    fun provideObserveSyncImages(
         tmdbRepository: TmdbRepository,
-        @IoDispatcher ioDispatcher: CoroutineDispatcher,
-    ): ObserveSyncImages = ObserveSyncImages(tmdbRepository, ioDispatcher)
+    ): ShowsStateMachine = ShowsStateMachine(traktRepository, tmdbRepository)
 
     @Singleton
     @Provides
