1fef1bc1e8c1c109e5980b83324b7725fbb5957c
kioko <kiokotomas@gmail.com>
2022-11-13 15:19:54 +0100

    Cleanup network resource class

--- shared/core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/util/network/NetworkBoundResource.kt
+++ shared/core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/util/network/NetworkBoundResource.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.onStart
 
 inline fun <ResultType, RequestType> networkBoundResource(
     crossinline query: () -> Flow<ResultType?>,
@@ -22,11 +21,6 @@ inline fun <ResultType, RequestType> networkBoundResource(
     // check for data in database
     val data = query().firstOrNull()
 
-    if (data != null) {
-        // data is not null -> update loading status
-        emit(Resource.loading(data))
-    }
-
     if (shouldFetch(data)) {
         // Need to fetch data -> call backend
         val fetchResult = fetch()
@@ -38,12 +32,11 @@ inline fun <ResultType, RequestType> networkBoundResource(
     val updatedData = query().first()
 
     // emit updated data
-    emit(Resource.success(updatedData))
+    emit(Resource.Success(updatedData))
 }
-    .onStart { emit(Resource.loading(null)) }
     .catch { throwable ->
         onFetchFailed(throwable)
-        emit(Resource.error(throwable, null))
+        emit(Resource.Error(throwable.resolveError(), null))
         throw Throwable(throwable.resolveError())
     }
     .flowOn(coroutineDispatcher)
--- shared/core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/util/network/Resource.kt
+++ shared/core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/util/network/Resource.kt
@@ -4,25 +4,13 @@ package com.thomaskioko.tvmaniac.core.util.network
  * A generic class that describes data with a status
  */
 /**
- * A generic class that holds a value with its loading status.
+ * A generic class that holds a value with after a request is done.
  * @param <T>
 </T> */
-data class Resource<out T>(val status: Status, val data: T?, val throwable: Throwable?) {
-    companion object {
-        fun <T> success(data: T?): Resource<T> {
-            return Resource(Status.SUCCESS, data, null)
-        }
-
-        fun <T> error(throwable: Throwable, data: T?): Resource<T> {
-            return Resource(Status.ERROR, data, throwable)
-        }
-
-        fun <T> loading(data: T?): Resource<T> {
-            return Resource(Status.LOADING, data, null)
-        }
-    }
-}
-
-enum class Status {
-    SUCCESS, ERROR, LOADING,
+sealed class Resource<T>(
+    val data: T? = null,
+    val errorMessage: String = ""
+) {
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Error<T>(errorMessage: String, data: T? = null) : Resource<T>(data, errorMessage)
 }
