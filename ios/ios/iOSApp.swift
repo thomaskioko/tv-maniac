a5ec7cdd30196b10b8b55b5e36ae621aaebf8a2c
kioko <kiokotomas@gmail.com>
2022-12-07 18:16:46 +0100

    Create settings ui

--- ios/ios/iOSApp.swift
+++ ios/ios/iOSApp.swift
@@ -3,12 +3,31 @@ import TvManiac
 
 @main
 struct iOSApp: App {
-	
-   @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-	
+
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	@ObservedObject var viewModel: SettingsViewModel = SettingsViewModel()
+	@Environment(\.colorScheme) var systemColorScheme: ColorScheme
+
 	var body: some Scene {
 		WindowGroup {
 			HomeUIView()
+					.preferredColorScheme(colorScheme)
+					.onAppear {
+						viewModel.startStateMachine()
+					}.environmentObject(viewModel)
+		}
+	}
+
+	var colorScheme: ColorScheme? {
+		withAnimation {
+			switch viewModel.appTheme {
+			case .Dark:
+				return .dark
+			case .Light:
+				return .light
+			default:
+				return .light
+			}
 		}
 	}
 }
