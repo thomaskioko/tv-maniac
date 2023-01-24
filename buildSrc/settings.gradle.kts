4323e9281fc11316609e7ef25cfb64171c89eeb9
kioko <kiokotomas@gmail.com>
2023-01-24 18:57:12 +0100

    Remove enableFeaturePreview

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,8 +1,3 @@
-enableFeaturePreview("VERSION_CATALOGS")
-/**
- * Workaround to make version catalogs accessible from precompiled script plugins
- * https://github.com/gradle/gradle/issues/15383
- */
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     versionCatalogs {
