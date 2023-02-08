41e680768409d48dcae9c2c2b85a182dd5e05a83
kioko <kiokotomas@gmail.com>
2023-02-08 17:34:11 +0100

    Add app flavor extension.

--- tooling/plugins/src/main/kotlin/ApplicationPlugin.kt
+++ tooling/plugins/src/main/kotlin/ApplicationPlugin.kt
@@ -1,11 +1,16 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.thomaskioko.tvmaniac.extensions.TvManiacFlavor
 import com.thomaskioko.tvmaniac.extensions.configureAndroidCompose
+import com.thomaskioko.tvmaniac.extensions.configureFlavors
 import com.thomaskioko.tvmaniac.extensions.configureKotlinAndroid
+import com.thomaskioko.tvmaniac.extensions.FlavorDimension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
+@Suppress("UnstableApiUsage")
 class ApplicationPlugin : Plugin<Project> {
+
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
@@ -14,9 +19,18 @@ class ApplicationPlugin : Plugin<Project> {
             }
 
             extensions.configure<ApplicationExtension> {
+                defaultConfig {
+                    targetSdk = 33
+                    missingDimensionStrategy(FlavorDimension.contentType.name, TvManiacFlavor.demo.name)
+                }
+
+                buildFeatures {
+                    buildConfig = true
+                }
+
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 33
                 configureAndroidCompose(this)
+                configureFlavors(this)
             }
         }
     }
