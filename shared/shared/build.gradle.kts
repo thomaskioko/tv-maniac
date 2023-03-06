e8da1e7349538d71fa9f0121d1be2604632eddc1
Thomas Kioko <kiokotomas@gmail.com>
2023-03-06 19:27:59 +0100

    Add iOs targets and remove KotlinNativeTarget block

--- shared/shared/build.gradle.kts
+++ shared/shared/build.gradle.kts
@@ -16,64 +16,35 @@ kotlin {
     android()
 
     val xcf = XCFramework()
-    ios {
-        binaries.framework {
+    val iosTargets = listOf(iosX64(), iosArm64())
+    iosTargets.forEach {
+        it.binaries.framework {
             baseName = "TvManiac"
-            xcf.add(this)
-        }
-    }
-
-    targets.withType<KotlinNativeTarget> {
-        binaries.withType<Framework> {
             isStatic = false
             linkerOpts.add("-lsqlite3")
-
-            export(project(":shared:core:util"))
-            export(project(":shared:data:database"))
-
-            //TODO:: Do we need to export api and implementation or is domain
-            // good enough?
-            export(project(":shared:data:datastore:api"))
-            export(project(":shared:data:network"))
-            export(project(":shared:data:episodes:api"))
-            export(project(":shared:data:similar:api"))
-            export(project(":shared:data:season-details:api"))
-            export(project(":shared:data:category:api"))
-            export(project(":shared:data:trailers:api"))
-            export(project(":shared:data:tmdb:api"))
-            export(project(":shared:data:trakt:api"))
-            export(project(":shared:domain:following"))
-            export(project(":shared:domain:discover"))
-            export(project(":shared:domain:seasondetails"))
-            export(project(":shared:domain:settings"))
-            export(project(":shared:domain:show-details"))
-            export(project(":shared:domain:trailers"))
-
-            embedBitcode(BitcodeEmbeddingMode.BITCODE)
-
-            transitiveExport = true
+            xcf.add(this)
         }
     }
 
     sourceSets {
         sourceSets["commonMain"].dependencies {
-            api(project(":shared:core:util"))
-            api(project(":shared:data:database"))
-            api(project(":shared:data:datastore:api"))
-            api(project(":shared:data:network"))
-            api(project(":shared:data:episodes:api"))
-            api(project(":shared:data:similar:api"))
-            api(project(":shared:data:season-details:api"))
-            api(project(":shared:data:category:api"))
-            api(project(":shared:data:trailers:api"))
-            api(project(":shared:data:tmdb:api"))
-            api(project(":shared:data:trakt:api"))
-            api(project(":shared:domain:discover"))
-            api(project(":shared:domain:following"))
-            api(project(":shared:domain:seasondetails"))
-            api(project(":shared:domain:settings"))
-            api(project(":shared:domain:show-details"))
-            api(project(":shared:domain:trailers"))
+            implementation(project(":shared:core:util"))
+            implementation(project(":shared:data:database"))
+            implementation(project(":shared:data:datastore:api"))
+            implementation(project(":shared:data:network"))
+            implementation(project(":shared:data:episodes:api"))
+            implementation(project(":shared:data:similar:api"))
+            implementation(project(":shared:data:season-details:api"))
+            implementation(project(":shared:data:category:api"))
+            implementation(project(":shared:data:trailers:api"))
+            implementation(project(":shared:data:tmdb:api"))
+            implementation(project(":shared:data:trakt:api"))
+            implementation(project(":shared:domain:discover"))
+            implementation(project(":shared:domain:following"))
+            implementation(project(":shared:domain:seasondetails"))
+            implementation(project(":shared:domain:settings"))
+            implementation(project(":shared:domain:show-details"))
+            implementation(project(":shared:domain:trailers"))
 
             implementation(project(":shared:data:datastore:implementation"))
             implementation(project(":shared:data:episodes:implementation"))
