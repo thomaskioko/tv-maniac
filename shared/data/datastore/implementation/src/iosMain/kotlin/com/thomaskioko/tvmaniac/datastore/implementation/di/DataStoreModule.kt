9619ad0ecc25fb32c19627bc969a0ae6cc95c29d
Thomas Kioko <kiokotomas@gmail.com>
2023-03-06 19:31:44 +0100

    Specify coroutineContext

--- shared/data/datastore/implementation/src/iosMain/kotlin/com/thomaskioko/tvmaniac/datastore/implementation/di/DataStoreModule.kt
+++ shared/data/datastore/implementation/src/iosMain/kotlin/com/thomaskioko/tvmaniac/datastore/implementation/di/DataStoreModule.kt
@@ -7,7 +7,9 @@ import com.thomaskioko.tvmaniac.datastore.implementation.DatastoreRepositoryImpl
 import com.thomaskioko.tvmaniac.datastore.implementation.createDataStore
 import com.thomaskioko.tvmaniac.datastore.implementation.dataStoreFileName
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
 import org.koin.core.module.Module
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
@@ -15,8 +17,11 @@ import platform.Foundation.NSURL
 import platform.Foundation.NSUserDomainMask
 
 actual fun datastoreModule(): Module = module {
-    single { dataStore(get()) }
-    single<DatastoreRepository> { DatastoreRepositoryImpl(get(), get()) }
+    single { dataStore(get(named("mainScope"))) }
+    single<DatastoreRepository> { DatastoreRepositoryImpl(
+        dataStore = get(),
+        coroutineScope = get(named("mainScope"))
+    ) }
 }
 
 fun dataStore(scope: CoroutineScope): DataStore<Preferences> = createDataStore(
--- shared/data/episodes/implementation/src/iosMain/kotlin/com/thomaskioko/tvmaniac/episodes/implementation/di/EpisodeModule.kt
+++ shared/data/episodes/implementation/src/iosMain/kotlin/com/thomaskioko/tvmaniac/episodes/implementation/di/EpisodeModule.kt
@@ -6,11 +6,23 @@ import com.thomaskioko.tvmaniac.episodes.api.EpisodesCache
 import com.thomaskioko.tvmaniac.episodes.implementation.EpisodeImageCacheImpl
 import com.thomaskioko.tvmaniac.episodes.implementation.EpisodeRepositoryImpl
 import com.thomaskioko.tvmaniac.episodes.implementation.EpisodesCacheImpl
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun episodeDataModule(): Module = module {
-    single<EpisodeRepository> { EpisodeRepositoryImpl(get(), get(), get()) }
-    single<EpisodesCache> { EpisodesCacheImpl(get(), get()) }
-    single<EpisodeImageCache> { EpisodeImageCacheImpl(get()) }
+    single<EpisodeRepository> {
+        EpisodeRepositoryImpl(
+            tmdbService = get(),
+            episodesCache = get(),
+            episodeImageCache = get()
+        )
+    }
+    single<EpisodeImageCache> { EpisodeImageCacheImpl(database = get()) }
+    single<EpisodesCache> {
+        EpisodesCacheImpl(
+            database = get(),
+            coroutineContext = Dispatchers.Default
+        )
+    }
 }
