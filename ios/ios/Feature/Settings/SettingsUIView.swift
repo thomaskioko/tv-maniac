b371e70537b96fd5b1d8be7fb15206436a05e195
Thomas Kioko <kiokotomas@gmail.com>
2023-03-07 18:10:06 +0100

    Minor update to the UI

--- ios/ios/Feature/Settings/SettingsUIView.swift
+++ ios/ios/Feature/Settings/SettingsUIView.swift
@@ -16,65 +16,65 @@ struct SettingsUIView: View {
 	@SwiftUI.State private var showingAlert: Bool = false
 
 	var body: some View {
-		Form {
-			Text("Settings")
-					.font(.largeTitle)
-					.titleFont(size: 22)
-					.frame(width: UIScreen.main.bounds.size.width)
-					.multilineTextAlignment(.leading)
+        NavigationView {
+            Form {
 
-
-			Section(header: Text("App Theme").bodyMediumFont(size: 16)) {
-
-				Picker(
-						selection: $viewModel.appTheme,
-						label: Text("Change Theme")
-								.bodyMediumFont(size: 16),
-						content: {
-							ForEach(AppTheme.allCases, id: \.self) { theme in
-
-								Text(theme.getName())
-										.tag(theme.rawValue)
-
-							}
-						})
-						.onChange(of: viewModel.appTheme) { theme in
-							viewModel.dispatchAction(action: ThemeSelected(theme: theme.toTheme()))
-						}
-
-			}
-
-
-			Section(header: Text("Trakt Account").bodyMediumFont(size: 16)) {
-
-				SettingsItem(
-						image: "person.fill",
-						title: "Connect to Trakt",
-						description: "Trakt is a platform that does many things, but primarily keeps track of TV shows and movies you watch."
-				) {
-					showingAlert = true
-				}
-						.alert(isPresented: $showingAlert) {
-							Alert(
-									title: Text("Trakt Coming Soon"),
-									message: Text("Trakt is a platform that does many things, but primarily keeps track of TV shows and movies you watch."),
-									primaryButton: .default(Text("Cancel")),
-									secondaryButton: .default(Text("Login"))
-							)
-						}
-			}
-
-			Section(header: Text("Info").bodyMediumFont(size: 16)) {
-
-				SettingsItem(
-						image: "info.circle.fill",
-						title: "About TvManiac",
-						description: "Tv-Maniac is a Multiplatform app (Android &amp; iOS) for viewing TV Shows from Trakt."
-				) {
-					openURL(URL(string: "https://github.com/c0de-wizard/tv-maniac")!)
-				}
-			}
-		}
+                Section(header: Text("App Theme").bodyMediumFont(size: 16)) {
+                    
+                    Picker(
+                        selection: $viewModel.appTheme,
+                        label: Text("Change Theme")
+                            .bodyMediumFont(size: 16),
+                        content: {
+                            ForEach(AppTheme.allCases, id: \.self) { theme in
+                                
+                                Text(theme.getName())
+                                    .tag(theme.rawValue)
+                                
+                            }
+                        })
+                    .onChange(of: viewModel.appTheme) { theme in
+                        viewModel.dispatchAction(action: ThemeSelected(theme: theme.toTheme()))
+                    }
+                    
+                }
+                
+                Section(header: Text("Trakt Account").bodyMediumFont(size: 16)) {
+                    
+                    SettingsItem(
+                        image: "person.fill",
+                        title: "Connect to Trakt",
+                        description: "Trakt is a platform that does many things, but primarily keeps track of TV shows and movies you watch."
+                    ) {
+                        showingAlert = true
+                    }
+                    .alert(isPresented: $showingAlert) {
+                        Alert(
+                            title: Text("Trakt Coming Soon"),
+                            message: Text("Trakt is a platform that does many things, but primarily keeps track of TV shows and movies you watch."),
+                            primaryButton: .default(Text("Cancel")),
+                            secondaryButton: .default(Text("Login"))
+                        )
+                    }
+                }
+                
+                Section(header: Text("Info").bodyMediumFont(size: 16)) {
+                    
+                    SettingsItem(
+                        image: "info.circle.fill",
+                        title: "About TvManiac",
+                        description: "Tv-Maniac is a Multiplatform app (Android &amp; iOS) for viewing TV Shows from Trakt."
+                    ) {
+                        openURL(URL(string: "https://github.com/c0de-wizard/tv-maniac")!)
+                    }
+                }
+            }
+            .navigationBarTitle("Settings")
+            .background(Color.background)
+            .accentColor(Color.accent)
+            .onAppear { viewModel.startStateMachine() }
+            .onDisappear { viewModel.cancel() }
+        }
 	}
 }
 
@@ -104,6 +104,7 @@ struct SettingsItem: View {
 						.padding(.top, 1.5)
 			}
 		}
+
 				.onTapGesture(perform: onClick)
 	}
 }
