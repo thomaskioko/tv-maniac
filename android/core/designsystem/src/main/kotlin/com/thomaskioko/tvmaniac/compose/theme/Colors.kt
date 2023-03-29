4d7991529a09bd5c024520eeb22b6a271411611c
Thomas Kioko <kiokotomas@gmail.com>
2023-03-29 19:28:27 +0300

    Update colors

--- android/core/designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/theme/Colors.kt
+++ android/core/designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/theme/Colors.kt
@@ -10,7 +10,7 @@ import kotlin.math.min
 
 val green = Color(0xFF00b300)
 
-val md_theme_light_primary = Color(0xFF1646F7)
+val md_theme_light_primary = Color(0xFF0049c7)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
 val md_theme_light_secondary = Color(0xFF3947EA)
 val md_theme_light_onSecondary = Color(0xFFFFFFFF)
@@ -19,25 +19,27 @@ val md_theme_light_background = Color(0xFFF8FDFF)
 val md_theme_light_onBackground = Color(0xFF001F25)
 val md_theme_light_surface = Color(0xFFF8FDFF)
 val md_theme_light_onSurface = Color(0xFF1F2123)
+val md_theme_light_outline = Color(0xFF1646F7)
 
 
 val md_theme_dark_primary = Color(0xFF1F2123)
 val md_theme_dark_onPrimary = Color(0xFFE0E0FF)
-val md_theme_dark_secondary = Color(0xFFFCBC04)
-val md_theme_dark_onSecondary = Color(0xFFE0E0FF)
+val md_theme_dark_secondary = Color(0xFFFEC804)
+val md_theme_dark_onSecondary = Color(0xFFFFFFFF)
 val md_theme_dark_error = Color(0xFFBA1A1A)
 val md_theme_dark_background = Color(0xFF2c2c30)
 val md_theme_dark_onBackground = Color(0xFFE0E0FF)
 val md_theme_dark_surface = Color(0xFF1F2123)
 val md_theme_dark_onSurface = Color(0xFFF8FDFF)
+val md_theme_dark_outline = Color(0xFF1F2123)
 
 @Composable
 fun backgroundGradient(): List<Color> {
     return listOf(
-        MaterialTheme.colorScheme.surface,
-        MaterialTheme.colorScheme.surface.copy(alpha = 0.9F),
-        MaterialTheme.colorScheme.surface.copy(alpha = 0.8F),
-        MaterialTheme.colorScheme.surface.copy(alpha = 0.7F),
+        MaterialTheme.colorScheme.background,
+        MaterialTheme.colorScheme.background.copy(alpha = 0.9F),
+        MaterialTheme.colorScheme.background.copy(alpha = 0.8F),
+        MaterialTheme.colorScheme.background.copy(alpha = 0.7F),
         Color.Transparent
     )
 }
