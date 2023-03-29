a93fd949c4cea34b8a4ef8a2ea0f5a0e1ed088bb
Thomas Kioko <kiokotomas@gmail.com>
2023-03-29 18:29:48 +0300

    Update mapper and model class

---
+++ android/features/shows-grid/src/main/kotlin/com/thomaskioko/tvmaniac/show_grid/PreviewMockData.kt
@@ -0,0 +1,25 @@
+package com.thomaskioko.tvmaniac.show_grid
+
+import com.thomaskioko.tvmaniac.show_grid.model.TvShow
+
+
+val showList = listOf(
+    TvShow(
+        traktId = 84958,
+        title = "Loki",
+        posterImageUrl = "/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+        backdropImageUrl = "/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+    ),
+    TvShow(
+        traktId = 84958,
+        title = "Loki",
+        posterImageUrl = "/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+        backdropImageUrl = "/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+    ),
+    TvShow(
+        traktId = 84958,
+        title = "Loki",
+        posterImageUrl = "/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+        backdropImageUrl = "/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+    )
+)
