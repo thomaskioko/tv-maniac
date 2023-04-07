a96129fb99e8676684c8baed67c189fe543d18de
Thomas Kioko <kiokotomas@gmail.com>
2023-04-07 21:02:44 +0200

    Delete classes.

--- shared/core/util/src/androidMain/kotlin/com/thomaskioko/tvmaniac/core/util/di/Module.kt
+++
@@ -1,6 +0,0 @@
-package com.thomaskioko.tvmaniac.core.util.di
-
-import org.koin.core.module.Module
-import org.koin.dsl.module
-
-actual fun coreUtilModule(): Module = module { }
--- shared/core/util/src/iosMain/kotlin/com/thomaskioko/tvmaniac/core/util/AppContext.kt
+++
@@ -1,5 +0,0 @@
-package com.thomaskioko.tvmaniac.core.util
-
-import platform.darwin.NSObject
-
-actual typealias AppContext = NSObject
