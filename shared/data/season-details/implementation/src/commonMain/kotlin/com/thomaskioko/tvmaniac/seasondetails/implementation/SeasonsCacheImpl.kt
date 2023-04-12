f590af7fb72558220d867844a493ff67ce9b23db
Thomas Kioko <kiokotomas@gmail.com>
2023-04-12 18:55:11 +0200

    Fix compile error: replace CoroutineDispatcher param with AppCoroutineDispatchers

--- shared/data/season-details/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/seasondetails/implementation/SeasonsCacheImpl.kt
+++ shared/data/season-details/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/seasondetails/implementation/SeasonsCacheImpl.kt
@@ -2,19 +2,19 @@ package com.thomaskioko.tvmaniac.seasondetails.implementation
 
 import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToList
+import com.thomaskioko.tvmaniac.base.model.AppCoroutineDispatchers
 import com.thomaskioko.tvmaniac.core.db.Season
 import com.thomaskioko.tvmaniac.core.db.Season_episodes
 import com.thomaskioko.tvmaniac.core.db.SelectSeasonWithEpisodes
 import com.thomaskioko.tvmaniac.core.db.TvManiacDatabase
 import com.thomaskioko.tvmaniac.seasondetails.api.SeasonsCache
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import me.tatarka.inject.annotations.Inject
 
 @Inject
 class SeasonsCacheImpl(
     private val database: TvManiacDatabase,
-    private val dispatcher: CoroutineDispatcher
+    private val dispatcher: AppCoroutineDispatchers
 ) : SeasonsCache {
 
     private val seasonQueries get() = database.seasonQueries
@@ -39,7 +39,7 @@ class SeasonsCacheImpl(
     override fun observeSeasons(traktId: Long): Flow<List<Season>> {
         return seasonQueries.selectBySeasonId(traktId)
             .asFlow()
-            .mapToList(dispatcher)
+            .mapToList(dispatcher.io)
     }
 
     override fun insert(entity: Season_episodes) {
@@ -55,5 +55,5 @@ class SeasonsCacheImpl(
     override fun observeShowEpisodes(showId: Long): Flow<List<SelectSeasonWithEpisodes>> =
         database.seasonEpisodesQueries.selectSeasonWithEpisodes(showId)
             .asFlow()
-            .mapToList(dispatcher)
+            .mapToList(dispatcher.io)
 }
