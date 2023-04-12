7f75689daf40f797129352d9769f262453b4596c
Thomas Kioko <kiokotomas@gmail.com>
2023-04-12 20:49:13 +0200

    Add `buildKonfig` block, update dependencies and modules.

--- shared/shared/build.gradle.kts
+++ shared/shared/build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
     id("tvmaniac.kmm.library")
-    alias(libs.plugins.serialization)
+    id("com.codingfeline.buildkonfig")
     id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
 }
 
@@ -28,10 +28,10 @@ kotlin {
             isStatic = false
             linkerOpts.add("-lsqlite3")
 
-            export(project(":shared:core:util"))
+            export(project(":shared:core:base"))
+            export(project(":shared:core:networkutil"))
             export(project(":shared:data:database"))
             export(project(":shared:data:datastore:api"))
-            export(project(":shared:data:network"))
             export(project(":shared:data:episodes:api"))
             export(project(":shared:data:similar:api"))
             export(project(":shared:data:season-details:api"))
@@ -56,19 +56,19 @@ kotlin {
     sourceSets {
         sourceSets["commonMain"].dependencies {
 
-            api(project(":shared:core:util"))
+            api(project(":shared:core:base"))
+            api(project(":shared:core:networkutil"))
             api(project(":shared:data:category:api"))
             api(project(":shared:data:database"))
             api(project(":shared:data:datastore:api"))
             api(project(":shared:data:episodes:api"))
-            api(project(":shared:data:network"))
             api(project(":shared:data:similar:api"))
             api(project(":shared:data:season-details:api"))
             api(project(":shared:data:shows:api"))
             api(project(":shared:data:trailers:api"))
             api(project(":shared:data:tmdb:api"))
             api(project(":shared:data:profile:api"))
-            api(project(":shared:data:trakt-service:api"))
+            api(project(":shared:data:trakt-api:api"))
             api(project(":shared:domain:discover"))
             api(project(":shared:domain:following"))
             api(project(":shared:domain:seasondetails"))
@@ -85,19 +85,50 @@ kotlin {
             implementation(project(":shared:data:shows:implementation"))
             implementation(project(":shared:data:tmdb:implementation"))
             implementation(project(":shared:data:trailers:implementation"))
-            implementation(project(":shared:data:trakt-service:implementation"))
+            implementation(project(":shared:data:trakt-api:implementation"))
 
-            implementation(libs.koin)
+            implementation(libs.androidx.datastore.preference)
             implementation(libs.coroutines.core)
+            implementation(libs.kotlinInject.runtime)
         }
 
-        sourceSets["commonTest"].dependencies {}
+        sourceSets["iosMain"].dependencies {
+            implementation(libs.ktor.darwin)
+            implementation(libs.sqldelight.driver.native)
+        }
     }
-    
+
 }
 
 android {
     namespace = "com.thomaskioko.tvmaniac.shared"
+}
+
+buildkonfig {
+    packageName = "com.thomaskioko.tvmaniac.shared"
+
+    defaultConfigs {
+        buildConfigField(
+            com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING,
+            "TMDB_API_KEY",
+            "\"" + propOrDef("TMDB_API_KEY", "") + "\""
+        )
+        buildConfigField(
+            com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING,
+            "TRAKT_CLIENT_ID",
+            "\"" + propOrDef("TRAKT_CLIENT_ID", "") + "\""
+        )
+        buildConfigField(
+            com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING,
+            "TRAKT_CLIENT_SECRET",
+            "\"" + propOrDef("TRAKT_CLIENT_SECRET", "") + "\""
+        )
+        buildConfigField(
+            com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING,
+            "TRAKT_REDIRECT_URI",
+            "\"" + propOrDef("TRAKT_REDIRECT_URI", "") + "\""
+        )
+    }
 }
 
 multiplatformSwiftPackage {
@@ -110,3 +141,10 @@ multiplatformSwiftPackage {
     distributionMode { local() }
     outputDirectory(File("$projectDir/../../../", "tvmaniac-swift-packages"))
 }
+
+fun <T : Any> propOrDef(propertyName: String, defaultValue: T): T {
+    @Suppress("UNCHECKED_CAST")
+    val propertyValue = project.properties[propertyName] as T?
+    return propertyValue ?: defaultValue
+}
+
