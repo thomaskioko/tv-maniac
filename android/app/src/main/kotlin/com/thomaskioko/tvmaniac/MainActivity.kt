2f4ffe9200103e2f97052eec7e32fb4807d4d40d
Thomas Kioko <kiokotomas@gmail.com>
2023-04-13 13:58:01 +0200

    Move theme and connection state logic to the viewModel and add component.

--- android/app/src/main/kotlin/com/thomaskioko/tvmaniac/MainActivity.kt
+++ android/app/src/main/kotlin/com/thomaskioko/tvmaniac/MainActivity.kt
@@ -3,6 +3,7 @@ package com.thomaskioko.tvmaniac
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.expandVertically
@@ -11,33 +12,32 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.thomaskioko.tvmaniac.base.extensions.unsafeLazy
 import com.thomaskioko.tvmaniac.compose.components.ConnectionStatus
 import com.thomaskioko.tvmaniac.compose.theme.TvManiacTheme
 import com.thomaskioko.tvmaniac.core.networkutil.ConnectionState
-import com.thomaskioko.tvmaniac.core.networkutil.NetworkRepository
-import com.thomaskioko.tvmaniac.datastore.api.DatastoreRepository
 import com.thomaskioko.tvmaniac.inject.MainActivityComponent
 import com.thomaskioko.tvmaniac.inject.create
 import com.thomaskioko.tvmaniac.settings.shouldUseDarkColors
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.distinctUntilChanged
 
 @OptIn(ExperimentalAnimationApi::class)
 class MainActivity : ComponentActivity() {
     private lateinit var component: MainActivityComponent
 
-    private val datastoreRepository: DatastoreRepository by unsafeLazy { component.datastoreRepository }
-    private val networkRepository: NetworkRepository by unsafeLazy { component.networkRepository }
+    private val viewModel: MainActivityViewModel by viewModels {
+        viewModelFactory {
+            addInitializer(MainActivityViewModel::class) { component.viewModel() }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,9 +45,13 @@ class MainActivity : ComponentActivity() {
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
+        component.traktAuthManager.registerResult()
+
         setContent {
+            val state = viewModel.state.collectAsState()
+
             val systemUiController = rememberSystemUiController()
-            val darkTheme = datastoreRepository.shouldUseDarkColors()
+            val darkTheme = state.value.theme.shouldUseDarkColors()
 
             DisposableEffect(systemUiController, darkTheme) {
                 systemUiController.systemBarsDarkContentEnabled = !darkTheme
@@ -59,7 +63,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-            ConnectivityStatus(networkRepository)
+            ConnectivityStatus(state.value.connectionState)
         }
     }
 
@@ -67,9 +71,8 @@ class MainActivity : ComponentActivity() {
     @ExperimentalAnimationApi
     @ExperimentalCoroutinesApi
     @Composable
-    fun ConnectivityStatus(observeNetwork: NetworkRepository) {
-        val connection by connectivityState(observeNetwork)
-        val isConnected = connection === ConnectionState.ConnectionAvailable
+    fun ConnectivityStatus(connectionState: ConnectionState) {
+        val isConnected = connectionState === ConnectionState.ConnectionAvailable
 
         var visibility by remember { mutableStateOf(false) }
 
@@ -89,15 +92,5 @@ class MainActivity : ComponentActivity() {
         ) {
             ConnectionStatus(isConnected = isConnected)
         }
-    }
-}
-
-@ExperimentalCoroutinesApi
-@Composable
-fun connectivityState(observeNetwork: NetworkRepository): State<ConnectionState> {
-    return produceState(initialValue = observeNetwork.connectivityState) {
-        observeNetwork.observeConnectionState()
-            .distinctUntilChanged()
-            .collect { value = it }
     }
 }
--- android/app/src/main/kotlin/com/thomaskioko/tvmaniac/inject/MainActivityComponent.kt
+++ android/app/src/main/kotlin/com/thomaskioko/tvmaniac/inject/MainActivityComponent.kt
@@ -1,11 +1,10 @@
 package com.thomaskioko.tvmaniac.inject
 
 import android.app.Activity
+import com.thomaskioko.tvmaniac.MainActivityViewModel
 import com.thomaskioko.tvmaniac.base.scope.ActivityScope
-import com.thomaskioko.tvmaniac.core.networkutil.NetworkRepository
-import com.thomaskioko.tvmaniac.datastore.api.DatastoreRepository
+import com.thomaskioko.tvmaniac.traktauth.TraktAuthManager
 import com.thomaskioko.tvmaniac.traktauth.inject.TraktAuthManagerComponent
-import dagger.hilt.android.components.ActivityComponent
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 
@@ -14,8 +13,7 @@ import me.tatarka.inject.annotations.Provides
 abstract class MainActivityComponent(
     @get:Provides val activity: Activity,
     @Component val applicationComponent: ApplicationComponent = ApplicationComponent.from(activity),
-) : ActivityComponent,
-    TraktAuthManagerComponent {
-    abstract val datastoreRepository: DatastoreRepository
-    abstract val networkRepository: NetworkRepository
+) : TraktAuthManagerComponent {
+    abstract val traktAuthManager: TraktAuthManager
+    abstract val viewModel: () -> MainActivityViewModel
 }
