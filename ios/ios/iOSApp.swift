9b00e95877471d4747a5fcd01352f9610419fb29
Thomas Kioko <kiokotomas@gmail.com>
2023-04-14 18:03:28 +0200

    Get rid of koin.

--- ios/ios/iOSApp.swift
+++ ios/ios/iOSApp.swift
@@ -4,7 +4,6 @@ import TvManiac
 @main
 struct iOSApp: App {
 
-	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 	@ObservedObject var viewModel: SettingsViewModel = SettingsViewModel(settingsState: SettingsContent.companion.EMPTY)
 	@Environment(\.colorScheme) var systemColorScheme: ColorScheme
 
@@ -28,12 +27,5 @@ struct iOSApp: App {
 				return .light
 			}
 		}
-	}
-}
-
-class AppDelegate : NSObject, UIApplicationDelegate {
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		KoinApplication.start()
-		return true
 	}
 }
