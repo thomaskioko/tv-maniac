bfda8b1eb7363a68b3f994ba484f704081daf134
Thomas Kioko <kiokotomas@gmail.com>
2023-04-19 17:12:04 +0200

    Add spotless script.

---
+++ tooling/checks/spotless.gradle.kts
@@ -0,0 +1,34 @@
+val ktlintVersion = "0.48.1"
+
+initscript {
+    val spotlessVersion = "6.17.0"
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:$spotlessVersion")
+    }
+}
+
+rootProject {
+    subprojects {
+        apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+        extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+            kotlin {
+                target("**/*.kt")
+                targetExclude("**/build/**/*.kt")
+                ktlint(ktlintVersion).userData(mapOf("android" to "true"))
+            }
+            format("kts") {
+                target("**/*.kts")
+                targetExclude("**/build/**/*.kts")
+            }
+            format("xml") {
+                target("**/*.xml")
+                targetExclude("**/build/**/*.xml")
+            }
+        }
+    }
+}
