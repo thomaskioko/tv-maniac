94c4c095d127059912d0665358b7e5d3ad02e362
runner <runner@Mac-1682349402136.local>
2023-04-24 15:34:21 +0000

    KMM SPM package release for 0.2.5.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/c0de-wizard/tv-maniac/tv-maniac/shared/base-kmmbridge/0.2.5.1/base-kmmbridge-0.2.5.1.zip"
+let remoteKotlinChecksum = "aa384a0b0bb459b7d6daa71392f5ca5974378bc3aa96d1728fda65f35fd3e831"
 let packageName = "TvManiac"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./shared/base/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
