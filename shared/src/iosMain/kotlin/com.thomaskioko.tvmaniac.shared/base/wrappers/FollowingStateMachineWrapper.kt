0ed3a4436d3c6aab869c02147774893b07a789c5
kioko <kiokotomas@gmail.com>
2023-05-09 01:14:21 +0200

    Update imports

--- shared/src/iosMain/kotlin/com.thomaskioko.tvmaniac.shared/base/wrappers/FollowingStateMachineWrapper.kt
+++ shared/src/iosMain/kotlin/com.thomaskioko.tvmaniac.shared/base/wrappers/FollowingStateMachineWrapper.kt
@@ -1,8 +1,8 @@
 package com.thomaskioko.tvmaniac.shared.base.wrappers
 
-import com.thomaskioko.tvmaniac.domain.following.FollowingAction
-import com.thomaskioko.tvmaniac.domain.following.FollowingState
-import com.thomaskioko.tvmaniac.domain.following.FollowingStateMachine
+import com.thomaskioko.tvmaniac.presentation.following.FollowingAction
+import com.thomaskioko.tvmaniac.presentation.following.FollowingState
+import com.thomaskioko.tvmaniac.presentation.following.FollowingStateMachine
 import com.thomaskioko.tvmaniac.util.model.AppCoroutineScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
