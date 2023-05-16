c4e3ae6658d80829a5833efc602a3c53c1906306
kioko <kiokotomas@gmail.com>
2023-05-16 22:21:33 +0200

    Split seasondetails and season modules.

---
+++ data/seasondetails/implementation/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("tvmaniac.kmm.library")
+    alias(libs.plugins.ksp)
+}
+
+kotlin {
+    android()
+    ios()
+
+    sourceSets {
+        sourceSets["commonMain"].dependencies {
+            implementation(projects.core.database)
+            implementation(projects.core.datastore.api)
+            implementation(projects.core.tmdbApi.api)
+            implementation(projects.core.traktApi.api)
+            implementation(projects.core.util)
+            implementation(projects.data.episodes.api)
+            implementation(projects.data.seasons.api)
+            implementation(projects.data.seasondetails.api)
+
+            implementation(libs.kotlinInject.runtime)
+            implementation(libs.sqldelight.extensions)
+
+        }
+
+        sourceSets["commonTest"].dependencies {
+            implementation(kotlin("test"))
+            implementation(libs.turbine)
+            implementation(libs.kotest.assertions)
+        }
+
+    }
+}
+
+dependencies {
+    add("kspAndroid", libs.kotlinInject.compiler)
+    add("kspIosX64", libs.kotlinInject.compiler)
+    add("kspIosArm64", libs.kotlinInject.compiler)
+}
+
+android {
+    namespace = "com.thomaskioko.tvmaniac.seasons.implementation"
+}
