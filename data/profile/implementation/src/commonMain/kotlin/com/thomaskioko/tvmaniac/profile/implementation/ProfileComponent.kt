7bc8beb94f6ab627616c96f9c46c8287136fde97
kioko <kiokotomas@gmail.com>
2023-05-18 03:41:01 +0200

    Delete unused dao class

--- data/profile/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/profile/implementation/ProfileComponent.kt
+++ data/profile/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/profile/implementation/ProfileComponent.kt
@@ -2,17 +2,12 @@ package com.thomaskioko.tvmaniac.profile.implementation
 
 import com.thomaskioko.tvmaniac.profile.api.ProfileDao
 import com.thomaskioko.tvmaniac.profile.api.ProfileRepository
-import com.thomaskioko.tvmaniac.profile.api.favorite.FavoriteListDao
-import com.thomaskioko.tvmaniac.profile.implementation.favorite.FavoriteListDaoImpl
 import me.tatarka.inject.annotations.Provides
 
 interface ProfileComponent {
 
     @Provides
     fun provideProfileDao(bind: ProfileDaoImpl): ProfileDao = bind
-
-    @Provides
-    fun provideFavoriteListDao(bind: FavoriteListDaoImpl): FavoriteListDao = bind
 
     @Provides
     fun provideProfileRepository(bind: ProfileRepositoryImpl): ProfileRepository = bind
