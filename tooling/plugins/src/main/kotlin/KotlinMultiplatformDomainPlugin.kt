9ce97558d147296e8292deffa8f2f61cf475c3d0
kioko <kiokotomas@gmail.com>
2023-05-22 21:29:14 +0200

    Get rid of jdk 11

--- tooling/plugins/src/main/kotlin/KotlinMultiplatformDomainPlugin.kt
+++ tooling/plugins/src/main/kotlin/KotlinMultiplatformDomainPlugin.kt
@@ -21,8 +21,8 @@ class KotlinMultiplatformDomainPlugin : Plugin<Project> {
                 defaultConfig.targetSdk = 33
                 compileSdk = 33
                 compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_11
-                    targetCompatibility = JavaVersion.VERSION_11
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
                 }
             }
 
