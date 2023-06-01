ef5f0b471350d95021a4ce43887afd24ba06d870
kioko <kiokotomas@gmail.com>
2023-06-01 20:03:00 +0200

    Create helper functions to store auth state.
    - This is just temporary. Since it's not secure, I will probably rewrite this in a refactor ticket.

--- core/datastore/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/datastore/api/DatastoreRepository.kt
+++ core/datastore/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/datastore/api/DatastoreRepository.kt
@@ -5,6 +5,11 @@ import kotlinx.coroutines.flow.Flow
 interface DatastoreRepository {
     fun saveTheme(theme: Theme)
     fun observeTheme(): Flow<Theme>
+
+    fun saveAuthState(authState: AuthState)
+    fun clearAuthState()
+    fun observeAuthState(): Flow<AuthState>
+    suspend fun getAuthState() : AuthState
 }
 
 enum class Theme {
