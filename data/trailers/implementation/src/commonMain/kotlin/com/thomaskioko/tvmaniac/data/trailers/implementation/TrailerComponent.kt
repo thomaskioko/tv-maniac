14dd9fc3a72158d91b56bac40e852d1dc2959074
kioko <kiokotomas@gmail.com>
2023-06-06 10:28:47 +0200

    Add scope annotation.

--- data/trailers/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/data/trailers/implementation/TrailerComponent.kt
+++ data/trailers/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/data/trailers/implementation/TrailerComponent.kt
@@ -1,12 +1,15 @@
 package com.thomaskioko.tvmaniac.data.trailers.implementation
 
+import com.thomaskioko.tvmaniac.util.scope.ApplicationScope
 import me.tatarka.inject.annotations.Provides
 
 interface TrailerComponent {
 
+    @ApplicationScope
     @Provides
     fun provideTrailerDao(bind: TrailerDaoImpl): TrailerDao = bind
 
+    @ApplicationScope
     @Provides
     fun provideTrailerRepository(bind: TrailerRepositoryImpl): TrailerRepository = bind
 }
