b063a2c1a896c9e1d8a6adb083abca4694e136c3
kioko <kiokotomas@gmail.com>
2023-06-06 10:28:47 +0200

    Add scope annotation.

--- data/shows/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/shows/implementation/ShowsComponent.kt
+++ data/shows/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/shows/implementation/ShowsComponent.kt
@@ -2,13 +2,16 @@ package com.thomaskioko.tvmaniac.shows.implementation
 
 import com.thomaskioko.tvmaniac.shows.api.ShowsDao
 import com.thomaskioko.tvmaniac.shows.api.ShowsRepository
+import com.thomaskioko.tvmaniac.util.scope.ApplicationScope
 import me.tatarka.inject.annotations.Provides
 
 interface ShowsComponent {
 
+    @ApplicationScope
     @Provides
     fun provideShowsCache(bind: ShowDaoImpl): ShowsDao = bind
 
+    @ApplicationScope
     @Provides
     fun provideShowsRepository(bind: ShowsRepositoryImpl): ShowsRepository = bind
 }
--- data/watchlist/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/watchlist/implementation/WatchlistComponent.kt
+++ data/watchlist/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/watchlist/implementation/WatchlistComponent.kt
@@ -2,13 +2,16 @@ package com.thomaskioko.tvmaniac.watchlist.implementation
 
 import com.thomaskioko.tvmaniac.shows.api.WatchlistDao
 import com.thomaskioko.tvmaniac.shows.api.WatchlistRepository
+import com.thomaskioko.tvmaniac.util.scope.ApplicationScope
 import me.tatarka.inject.annotations.Provides
 
 interface WatchlistComponent {
 
+    @ApplicationScope
     @Provides
     fun provideWatchlistDao(bind: WatchlistDaoImpl): WatchlistDao = bind
 
+    @ApplicationScope
     @Provides
     fun provideWatchlist(bind: WatchlistRepositoryImpl): WatchlistRepository = bind
 }
