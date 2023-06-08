eabcd2d55b9e368775e336bc95f5064fd8161ad0
kioko <kiokotomas@gmail.com>
2023-06-08 21:11:26 +0200

    Update settings state and fix issue of profile not being loaded.

--- presentation/settings/src/commonTest/kotlin/com/thomaskioko/tvmaniac/presentation/settings/SettingsStateMachineTest.kt
+++ presentation/settings/src/commonTest/kotlin/com/thomaskioko/tvmaniac/presentation/settings/SettingsStateMachineTest.kt
@@ -3,21 +3,23 @@ package com.thomaskioko.tvmaniac.presentation.settings
 import app.cash.turbine.test
 import com.thomaskioko.tvmaniac.datastore.api.Theme
 import com.thomaskioko.tvmaniac.datastore.testing.FakeDatastoreRepository
+import com.thomaskioko.tvmaniac.datastore.testing.authenticatedAuthState
 import com.thomaskioko.tvmaniac.trakt.profile.testing.FakeProfileRepository
+import com.thomaskioko.tvmaniac.trakt.profile.testing.user
 import com.thomaskioko.tvmaniac.traktauth.testing.FakeTraktAuthRepository
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.test.runTest
-import kotlin.test.Ignore
+import org.mobilenativefoundation.store.store5.StoreReadResponse
+import org.mobilenativefoundation.store.store5.StoreReadResponseOrigin
 import kotlin.test.Test
 
-@Ignore
 class SettingsStateMachineTest {
 
-    private val settingsRepository = FakeDatastoreRepository()
+    private val datastoreRepository = FakeDatastoreRepository()
     private val profileRepository = FakeProfileRepository()
     private val traktAuthRepository = FakeTraktAuthRepository()
     private val stateMachine = SettingsStateMachine(
-        datastoreRepository = settingsRepository,
+        datastoreRepository = datastoreRepository,
         profileRepository = profileRepository,
         traktAuthRepository = traktAuthRepository,
     )
@@ -25,31 +27,28 @@ class SettingsStateMachineTest {
     @Test
     fun initial_state_emits_expected_result() = runTest {
         stateMachine.state.test {
-            awaitItem() shouldBe SettingsContent.EMPTY // Initial State
+            awaitItem() shouldBe Default.EMPTY
         }
     }
 
     @Test
     fun when_theme_is_updated_expected_result_is_emitted() = runTest {
         stateMachine.state.test {
-            awaitItem() shouldBe SettingsContent.EMPTY // Initial State
+            awaitItem() shouldBe Default.EMPTY // Initial State
 
             stateMachine.dispatch(ChangeThemeClicked)
-            awaitItem() shouldBe SettingsContent.EMPTY.copy(
-                //  Popup is shown
+            awaitItem() shouldBe Default.EMPTY.copy(
                 showPopup = true,
             )
 
-            settingsRepository.setTheme(Theme.DARK)
+            datastoreRepository.setTheme(Theme.DARK)
             stateMachine.dispatch(ThemeSelected(Theme.DARK))
 
-            awaitItem() shouldBe SettingsContent.EMPTY.copy(
-                // Theme is updated
+            awaitItem() shouldBe Default.EMPTY.copy(
                 showPopup = true,
                 theme = Theme.DARK,
             )
-            awaitItem() shouldBe SettingsContent.EMPTY.copy(
-                // Popup is dismissed.
+            awaitItem() shouldBe Default.EMPTY.copy(
                 showPopup = false,
                 theme = Theme.DARK,
             )
@@ -59,21 +58,145 @@ class SettingsStateMachineTest {
     @Test
     fun when_dialog_is_dismissed_expected_result_is_emitted() = runTest {
         stateMachine.state.test {
-            awaitItem() shouldBe SettingsContent.EMPTY // Initial State
+            awaitItem() shouldBe Default.EMPTY // Initial State
 
             stateMachine.dispatch(ChangeThemeClicked)
 
-            awaitItem() shouldBe SettingsContent.EMPTY.copy(
-                // Popup is shown
+            awaitItem() shouldBe Default.EMPTY.copy(
                 showPopup = true,
             )
 
             stateMachine.dispatch(DimissThemeClicked)
 
-            awaitItem() shouldBe SettingsContent.EMPTY.copy(
-                // Popup is dismissed.
+            awaitItem() shouldBe Default.EMPTY.copy(
                 showPopup = false,
             )
+        }
+    }
+
+    @Test
+    fun when_ShowTraktDialog_is_clicked_expected_result_is_emitted() = runTest {
+        stateMachine.state.test {
+            awaitItem() shouldBe Default.EMPTY // Initial State
+
+            stateMachine.dispatch(ShowTraktDialog)
+
+            awaitItem() shouldBe Default.EMPTY.copy(
+                showTraktDialog = true,
+            )
+
+            stateMachine.dispatch(DismissTraktDialog)
+
+            awaitItem() shouldBe Default.EMPTY.copy(
+                showTraktDialog = false,
+            )
+        }
+    }
+
+    @Test
+    fun given_TraktLoginClicked_andUserIsAuthenticated_expectedResultIsEmitted() = runTest {
+        stateMachine.state.test {
+            awaitItem() shouldBe Default.EMPTY // Initial State
+
+            stateMachine.dispatch(ShowTraktDialog)
+
+            awaitItem() shouldBe Default.EMPTY
+                .copy(
+                    showTraktDialog = true,
+                )
+
+            stateMachine.dispatch(TraktLoginClicked)
+
+            awaitItem() shouldBe Default.EMPTY
+                .copy(
+                    showTraktDialog = false,
+                )
+
+            datastoreRepository.setAuthState(authenticatedAuthState)
+            profileRepository.setUserData(
+                StoreReadResponse.Data(
+                    value = user,
+                    origin = StoreReadResponseOrigin.Cache,
+                ),
+            )
+
+            awaitItem() shouldBe LoggedInContent.DEFAULT_STATE
+            awaitItem() shouldBe LoggedInContent.DEFAULT_STATE
+                .copy(
+                    errorMessage = null,
+                    userInfo = UserInfo(
+                        slug = user.slug,
+                        userName = user.user_name,
+                        fullName = user.full_name,
+                        userPicUrl = user.profile_picture,
+                    ),
+                )
+        }
+    }
+
+    @Test
+    fun given_TraktLoginClicked_andErrorOccurs_expectedResultIsEmitted() = runTest {
+        stateMachine.state.test {
+            val errorMessage = "Something happened"
+
+            awaitItem() shouldBe Default.EMPTY // Initial State
+
+            stateMachine.dispatch(ShowTraktDialog)
+
+            awaitItem() shouldBe Default.EMPTY.copy(
+                showTraktDialog = true,
+            )
+
+            stateMachine.dispatch(TraktLoginClicked)
+
+            awaitItem() shouldBe Default.EMPTY.copy(
+                showTraktDialog = false,
+            )
+
+            datastoreRepository.setAuthState(authenticatedAuthState)
+            profileRepository.setUserData(
+                StoreReadResponse.Error.Exception(
+                    error = Throwable(errorMessage),
+                    origin = StoreReadResponseOrigin.Cache,
+                ),
+            )
+
+            awaitItem() shouldBe LoggedInContent.DEFAULT_STATE
+            awaitItem() shouldBe LoggedInContent.DEFAULT_STATE
+                .copy(
+                    errorMessage = errorMessage,
+                )
+        }
+    }
+
+    @Test
+    fun given_TraktLogoutClicked_expectedResultIsEmitted() = runTest {
+        stateMachine.state.test {
+            awaitItem() shouldBe Default.EMPTY // Initial State
+
+            datastoreRepository.setAuthState(authenticatedAuthState)
+            profileRepository.setUserData(
+                StoreReadResponse.Data(
+                    value = user,
+                    origin = StoreReadResponseOrigin.Cache,
+                ),
+            )
+
+            awaitItem() shouldBe LoggedInContent.DEFAULT_STATE
+            awaitItem() shouldBe LoggedInContent.DEFAULT_STATE
+                .copy(
+                    errorMessage = null,
+                    userInfo = UserInfo(
+                        slug = user.slug,
+                        userName = user.user_name,
+                        fullName = user.full_name,
+                        userPicUrl = user.profile_picture,
+                    ),
+                )
+
+            stateMachine.dispatch(TraktLogoutClicked)
+
+            awaitItem() shouldBe Default.EMPTY
         }
     }
 }
