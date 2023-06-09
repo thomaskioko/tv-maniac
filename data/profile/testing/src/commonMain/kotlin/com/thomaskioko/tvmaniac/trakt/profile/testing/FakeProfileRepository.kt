4ca18d671a65403812e33ecf16266ec6daba8eb6
kioko <kiokotomas@gmail.com>
2023-06-09 20:30:07 +0200

    Add statemachine test cases.

--- data/profile/testing/src/commonMain/kotlin/com/thomaskioko/tvmaniac/trakt/profile/testing/FakeProfileRepository.kt
+++ data/profile/testing/src/commonMain/kotlin/com/thomaskioko/tvmaniac/trakt/profile/testing/FakeProfileRepository.kt
@@ -2,30 +2,31 @@ package com.thomaskioko.tvmaniac.trakt.profile.testing
 
 import com.thomaskioko.tvmaniac.core.db.User
 import com.thomaskioko.tvmaniac.profile.api.ProfileRepository
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.receiveAsFlow
 import org.mobilenativefoundation.store.store5.StoreReadResponse
-import org.mobilenativefoundation.store.store5.StoreReadResponseOrigin
 
 class FakeProfileRepository : ProfileRepository {
 
-    private val user = User(
-        slug = "me",
-        user_name = "silly_eyes",
-        full_name = "Stranger Danger",
-        profile_picture = "",
-        is_me = true,
-    )
+    private val userFlow: Channel<StoreReadResponse<User>> = Channel(Channel.UNLIMITED)
+
+    suspend fun setUserData(response: StoreReadResponse<User>) {
+        userFlow.send(response)
+    }
 
     override fun observeProfile(slug: String): Flow<StoreReadResponse<User>> =
-        flowOf(
-            StoreReadResponse.Data(
-                value = user,
-                origin = StoreReadResponseOrigin.Cache,
-            ),
-        )
+        userFlow.receiveAsFlow()
 
     override suspend fun clearProfile() {
         // no-op
     }
 }
+
+val user = User(
+    slug = "me",
+    user_name = "silly_eyes",
+    full_name = "Stranger Danger",
+    profile_picture = null,
+    is_me = true,
+)
