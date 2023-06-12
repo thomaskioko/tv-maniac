65cd5b5089d5e6a0c3d8650e1928d0dc6203ff9e
kioko <kiokotomas@gmail.com>
2023-06-12 20:43:41 +0200

    Create last request table and create instant adapter.

--- core/database/src/androidMain/kotlin/com/thomaskioko/tvmaniac/db/DatabaseComponent.kt
+++ core/database/src/androidMain/kotlin/com/thomaskioko/tvmaniac/db/DatabaseComponent.kt
@@ -3,6 +3,7 @@ package com.thomaskioko.tvmaniac.db
 import android.app.Application
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import com.thomaskioko.tvmaniac.core.db.Last_requests
 import com.thomaskioko.tvmaniac.core.db.Show
 import com.thomaskioko.tvmaniac.core.db.TvManiacDatabase
 import com.thomaskioko.tvmaniac.util.scope.ApplicationScope
@@ -29,5 +30,8 @@ actual interface DatabaseComponent {
         showAdapter = Show.Adapter(
             genresAdapter = stringColumnAdapter,
         ),
+        last_requestsAdapter = Last_requests.Adapter(
+            timestampAdapter = InstantColumnAdapter
+        )
     )
 }
--- core/database/src/iosMain/kotlin/com/thomaskioko/tvmaniac/db/DatabaseComponent.kt
+++ core/database/src/iosMain/kotlin/com/thomaskioko/tvmaniac/db/DatabaseComponent.kt
@@ -2,6 +2,7 @@ package com.thomaskioko.tvmaniac.db
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import com.thomaskioko.tvmaniac.core.db.Last_requests
 import com.thomaskioko.tvmaniac.core.db.Show
 import com.thomaskioko.tvmaniac.core.db.TvManiacDatabase
 import com.thomaskioko.tvmaniac.util.scope.ApplicationScope
@@ -22,5 +23,8 @@ actual interface DatabaseComponent {
         showAdapter = Show.Adapter(
             genresAdapter = stringColumnAdapter,
         ),
+        last_requestsAdapter = Last_requests.Adapter(
+            timestampAdapter = InstantColumnAdapter
+        )
     )
 }
