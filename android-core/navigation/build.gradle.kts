b852c508e90d8f078143b801a62b0df031e260b9
kioko <kiokotomas@gmail.com>
2023-06-27 16:20:09 +0200

    Minor cleanup.

--- android-core/navigation/build.gradle.kts
+++ android-core/navigation/build.gradle.kts
@@ -12,11 +12,11 @@ dependencies {
     api(libs.androidx.navigation.common)
     api(libs.androidx.navigation.runtime)
 
-    implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.accompanist.navigation.material)
 
+    implementation(libs.kotlinInject.runtime)
     ksp(libs.kotlinInject.compiler)
 
     runtimeOnly(libs.coroutines.android)
