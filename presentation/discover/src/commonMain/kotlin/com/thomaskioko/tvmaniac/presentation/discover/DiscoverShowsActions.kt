25653a55d306deb734c7dfac3d3b6176e50c6404
kioko <kiokotomas@gmail.com>
2023-07-19 11:17:59 +0200

    Show toast when there's an error.

--- presentation/discover/src/commonMain/kotlin/com/thomaskioko/tvmaniac/presentation/discover/DiscoverShowsActions.kt
+++ presentation/discover/src/commonMain/kotlin/com/thomaskioko/tvmaniac/presentation/discover/DiscoverShowsActions.kt
@@ -3,9 +3,6 @@ package com.thomaskioko.tvmaniac.presentation.discover
 sealed interface ShowsAction
 
 object RetryLoading : ShowsAction
-object ReloadFeatured : ShowsAction
-object ReloadPopular : ShowsAction
-object ReloadAnticipated : ShowsAction
-object ReloadTrending : ShowsAction
+object SnackBarDismissed : ShowsAction
 
 data class ReloadCategory(val categoryId: Int) : ShowsAction
