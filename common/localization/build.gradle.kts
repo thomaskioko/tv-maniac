6bb123b88ac7b89564be867d43b4e3a243d3cf49
Thomas Kioko <kiokotomas@gmail.com>
2023-11-16 19:15:34 +0100

    Add moko-resources dependencies.

--- common/localization/build.gradle.kts
+++ common/localization/build.gradle.kts
@@ -1,30 +1,40 @@
 plugins {
     id("plugin.tvmaniac.kotlin.android")
     id("plugin.tvmaniac.multiplatform")
+    alias(libs.plugins.moko.resources)
 }
 
 kotlin {
     sourceSets {
         androidMain {
-            dependencies {
-
-            }
+            dependsOn(commonMain.get())
         }
 
         commonMain {
             dependencies {
+                api(libs.moko.core)
             }
         }
 
         iosMain {
             dependencies {
-
+                dependsOn(commonMain.get())
             }
         }
-
     }
 }
 
 android {
     namespace = "com.thomaskioko.tvmaniac.common.localization"
+}
+
+multiplatformResources {
+    multiplatformResourcesPackage = "com.thomaskioko.tvmaniac.common.localization"
+}
+
+tasks.withType(com.android.build.gradle.tasks.MergeResources::class).configureEach {
+    dependsOn(tasks.getByPath("generateMRandroidMain"))
+}
+tasks.withType(com.android.build.gradle.tasks.MapSourceSetPathsTask::class).configureEach {
+    dependsOn(tasks.getByPath("generateMRandroidMain"))
 }
