07ecfa2ef0fb47f850df13ad40aecc66e66fcca0
Thomas Kioko <kiokotomas@gmail.com>
2023-11-22 13:20:31 +0100

    Add material shapes

---
+++ android-core/designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.thomaskioko.tvmaniac.compose.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val tvManiacShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(16.dp)
+)
