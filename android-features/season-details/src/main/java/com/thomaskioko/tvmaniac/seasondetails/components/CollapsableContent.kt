329630d18e1f004e188b4c79aa22d4d5cadadf8b
Thomas Kioko <kiokotomas@gmail.com>
2023-11-22 17:27:44 +0100

    Minor update on Episode List UI.

--- android-features/season-details/src/main/java/com/thomaskioko/tvmaniac/seasondetails/components/CollapsableContent.kt
+++ android-features/season-details/src/main/java/com/thomaskioko/tvmaniac/seasondetails/components/CollapsableContent.kt
@@ -12,9 +12,7 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.rounded.ExpandLess
@@ -31,21 +29,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color.Companion.LightGray
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
-import com.thomaskioko.tvmaniac.compose.components.AsyncImageComposable
 import com.thomaskioko.tvmaniac.compose.components.ThemePreviews
 import com.thomaskioko.tvmaniac.compose.theme.TvManiacTheme
 import com.thomaskioko.tvmaniac.compose.theme.green
 import com.thomaskioko.tvmaniac.presentation.seasondetails.model.Episode
-import com.thomaskioko.tvmaniac.resources.R
-import com.thomaskioko.tvmaniac.seasondetails.episode
 import com.thomaskioko.tvmaniac.seasondetails.seasonDetails
 
 @Composable
@@ -79,8 +72,13 @@ fun CollapsableContent(
                 Spacer(modifier = Modifier.height(8.dp))
 
                 EpisodeItem(
-                    episode = episode,
-                    onEpisodeClicked = onEpisodeClicked,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .defaultMinSize(minHeight = 84.dp),
+                    imageUrl = episode.imageUrl,
+                    title = episode.episodeNumberTitle,
+                    episodeOverview = episode.overview,
+                    onEpisodeClicked = { onEpisodeClicked(episode.id) },
                 )
 
                 Spacer(modifier = Modifier.height(8.dp))
@@ -96,6 +94,7 @@ private fun SeasonTitleHeader(
     episodesCount: Long,
     watchProgress: Float,
     expanded: Boolean,
+    shape: Shape = MaterialTheme.shapes.small,
     onSeasonHeaderClicked: () -> Unit = {},
 ) {
     val transitionState = remember {
@@ -112,11 +111,10 @@ private fun SeasonTitleHeader(
     )
 
     Card(
-        shape = RectangleShape,
+        shape = shape,
         modifier = Modifier
             .fillMaxWidth()
             .height(64.dp)
-            .padding(horizontal = 16.dp)
             .clickable { onSeasonHeaderClicked() },
     ) {
         ConstraintLayout(
@@ -204,108 +202,6 @@ private fun SeasonTitleHeader(
 
                         width = Dimension.fillToConstraints
                     },
-            )
-        }
-    }
-}
-
-@Composable
-fun EpisodeItem(
-    episode: Episode,
-    modifier: Modifier = Modifier,
-    onEpisodeClicked: (Long) -> Unit = {},
-) {
-    Card(
-        shape = RectangleShape,
-        modifier = modifier
-            .fillMaxWidth()
-            .defaultMinSize(minHeight = 84.dp)
-            .padding(horizontal = 16.dp)
-            .clickable { onEpisodeClicked(episode.id) },
-    ) {
-        ConstraintLayout(
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            val (episodeTitle, image, overview, watchedStatusIcon) = createRefs()
-
-            AsyncImageComposable(
-                model = episode.imageUrl,
-                contentDescription = stringResource(
-                    R.string.cd_show_poster,
-                    episode.episodeNumberTitle,
-                ),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .width(94.dp)
-                    .constrainAs(image) {
-                        start.linkTo(parent.start)
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-
-                        height = Dimension.fillToConstraints
-                    },
-            )
-
-            Text(
-                text = episode.episodeNumberTitle,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier
-                    .constrainAs(episodeTitle) {
-                        start.linkTo(image.end, 8.dp)
-                        end.linkTo(watchedStatusIcon.start)
-                        top.linkTo(parent.top, 8.dp)
-
-                        width = Dimension.fillToConstraints
-                    },
-            )
-
-            Text(
-                text = episode.overview,
-                maxLines = 3,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier
-                    .constrainAs(overview) {
-                        start.linkTo(image.end, 8.dp)
-                        top.linkTo(episodeTitle.bottom, 5.dp)
-                        end.linkTo(watchedStatusIcon.start, 8.dp)
-                        bottom.linkTo(parent.bottom, 8.dp)
-
-                        width = Dimension.fillToConstraints
-                    },
-            )
-
-            IconButton(
-                onClick = {},
-                modifier = Modifier
-                    .constrainAs(watchedStatusIcon) {
-                        centerVerticallyTo(parent)
-                        end.linkTo(parent.end, 8.dp)
-                    },
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.CheckCircle,
-                    contentDescription = stringResource(R.string.cd_navigate_back),
-                    tint = MaterialTheme.colorScheme.onSurface,
-                    modifier = Modifier
-                        .size(32.dp),
-                )
-            }
-        }
-    }
-}
-
-@ThemePreviews
-@Composable
-fun EpisodeItemPreview() {
-    TvManiacTheme {
-        Surface {
-            EpisodeItem(
-                episode = episode,
             )
         }
     }
