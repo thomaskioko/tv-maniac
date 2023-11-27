87dcfb1bc42ee6d021a946892996760561cbc092
Thomas Kioko <kiokotomas@gmail.com>
2023-11-27 18:17:22 +0100

    Move class to core-util module.

--- core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/AppInitializer.kt
+++ core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/AppInitializer.kt
@@ -1,9 +1,5 @@
 package com.thomaskioko.tvmaniac.util
 
-/**
- * This would ideally be in a 'core' module but since we are only using it in the work manager,
- * we can leave it here for now.
- */
 fun interface AppInitializer {
     fun init()
 }
