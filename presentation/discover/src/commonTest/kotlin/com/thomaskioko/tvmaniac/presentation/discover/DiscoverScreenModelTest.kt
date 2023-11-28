6ecaa72122a952bd5f0e7f975afbad8aa7315a9a
Thomas Kioko <kiokotomas@gmail.com>
2023-11-28 21:03:01 +0100

    Add discoverScreenModel test.

--- presentation/discover/src/commonTest/kotlin/com/thomaskioko/tvmaniac/presentation/discover/DiscoverScreenModelTest.kt
+++ presentation/discover/src/commonTest/kotlin/com/thomaskioko/tvmaniac/presentation/discover/DiscoverScreenModelTest.kt
@@ -4,14 +4,35 @@ import app.cash.turbine.test
 import com.thomaskioko.tvmaniac.shows.testing.FakeDiscoverRepository
 import com.thomaskioko.tvmaniac.tmdb.testing.FakeShowImagesRepository
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 
-internal class DiscoverStateMachineTest {
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class DiscoverScreenModelTest {
 
+    private val testDispatcher = StandardTestDispatcher()
     private val discoverRepository = FakeDiscoverRepository()
     private val imagesRepository = FakeShowImagesRepository()
-    private val stateMachine = DiscoverStateMachine(discoverRepository, imagesRepository)
+
+    private lateinit var screenModel: DiscoverScreenModel
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        screenModel = DiscoverScreenModel(discoverRepository, imagesRepository)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
 
     @Test
     fun `given an result is loaded then correct state is emitted`() = runTest {
@@ -20,7 +41,7 @@ internal class DiscoverStateMachineTest {
         discoverRepository.setShowCategory(categoryResult(3))
         discoverRepository.setShowCategory(categoryResult(4))
 
-        stateMachine.state.test {
+        screenModel.state.test {
             awaitItem() shouldBe Loading
             awaitItem() shouldBe discoverContent
         }
