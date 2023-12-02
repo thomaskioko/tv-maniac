767906c6eece20133d1426fdfdebb4b6cc18f0bd
Thomas Kioko <kiokotomas@gmail.com>
2023-12-03 00:10:56 +0100

    Some dependency cleanup: Create SharedComponent

--- app/src/main/kotlin/com/thomaskioko/tvmaniac/inject/ApplicationComponent.kt
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/inject/ApplicationComponent.kt
@@ -2,30 +2,11 @@ package com.thomaskioko.tvmaniac.inject
 
 import android.app.Application
 import android.content.Context
-import com.thomaskioko.trakt.service.implementation.inject.TraktComponent
 import com.thomaskioko.tvmaniac.TvManicApplication
-import com.thomaskioko.tvmaniac.data.category.implementation.CategoryComponent
-import com.thomaskioko.tvmaniac.data.trailers.implementation.TrailerComponent
-import com.thomaskioko.tvmaniac.datastore.implementation.DataStoreComponent
-import com.thomaskioko.tvmaniac.db.DatabaseComponent
-import com.thomaskioko.tvmaniac.episodeimages.implementation.EpisodeImageComponent
-import com.thomaskioko.tvmaniac.episodes.implementation.EpisodeComponent
 import com.thomaskioko.tvmaniac.initializers.AppInitializers
-import com.thomaskioko.tvmaniac.profile.implementation.ProfileComponent
-import com.thomaskioko.tvmaniac.profilestats.implementation.StatsComponent
-import com.thomaskioko.tvmaniac.resourcemanager.implementation.RequestManagerComponent
-import com.thomaskioko.tvmaniac.seasondetails.implementation.SeasonDetailsComponent
-import com.thomaskioko.tvmaniac.seasons.implementation.SeasonsComponent
-import com.thomaskioko.tvmaniac.showimages.implementation.ShowImagesComponent
-import com.thomaskioko.tvmaniac.shows.implementation.DiscoverComponent
-import com.thomaskioko.tvmaniac.similar.implementation.SimilarShowsComponent
-import com.thomaskioko.tvmaniac.tmdb.implementation.TmdbComponent
+import com.thomaskioko.tvmaniac.shared.SharedComponent
 import com.thomaskioko.tvmaniac.traktauth.implementation.TraktAuthComponent
-import com.thomaskioko.tvmaniac.traktauth.implementation.TraktAuthenticationComponent
-import com.thomaskioko.tvmaniac.util.inject.LoggingComponent
-import com.thomaskioko.tvmaniac.util.inject.UtilPlatformComponent
 import com.thomaskioko.tvmaniac.util.scope.ApplicationScope
-import com.thomaskioko.tvmaniac.watchlist.implementation.LibraryComponent
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 
@@ -33,27 +14,7 @@ import me.tatarka.inject.annotations.Provides
 @ApplicationScope
 abstract class ApplicationComponent(
     @get:Provides val application: Application,
-) : CategoryComponent,
-    DataStoreComponent,
-    DatabaseComponent,
-    DiscoverComponent,
-    EpisodeComponent,
-    EpisodeImageComponent,
-    LibraryComponent,
-    LoggingComponent,
-    ProfileComponent,
-    RequestManagerComponent,
-    SeasonDetailsComponent,
-    SeasonsComponent,
-    ShowImagesComponent,
-    SimilarShowsComponent,
-    StatsComponent,
-    TmdbComponent,
-    TrailerComponent,
-    TraktAuthComponent,
-    TraktAuthenticationComponent,
-    TraktComponent,
-    UtilPlatformComponent {
+) : SharedComponent(), TraktAuthComponent {
 
     abstract val initializers: AppInitializers
 
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("plugin.tvmaniac.kotlin.android")
     id("plugin.tvmaniac.multiplatform")
     id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
     alias(libs.plugins.ksp)
@@ -9,39 +10,56 @@ kotlin {
         commonMain {
             dependencies {
 
-                api(projects.core.datastore.api)
-                api(projects.core.traktAuth.api)
                 api(projects.core.util)
-
                 api(projects.navigation)
 
+                api(projects.presentation.discover)
+                api(projects.presentation.library)
+                api(projects.presentation.moreShows)
+                api(projects.presentation.profile)
+                api(projects.presentation.search)
+                api(projects.presentation.seasondetails)
+                api(projects.presentation.settings)
+                api(projects.presentation.showDetails)
+                api(projects.presentation.trailers)
+
                 api(projects.core.database)
+                api(projects.core.datastore.api)
                 api(projects.core.datastore.implementation)
-                api(projects.data.episodeimages.api)
-                api(projects.data.library.api)
-                api(projects.core.util)
-                api(projects.data.showimages.api)
-                api(projects.core.traktApi.api)
-                api(projects.core.traktApi.implementation)
-                api(projects.core.traktAuth.implementation)
                 api(projects.core.tmdbApi.api)
                 api(projects.core.tmdbApi.implementation)
+                api(projects.core.traktApi.api)
+                api(projects.core.traktApi.implementation)
+                api(projects.core.traktAuth.api)
+                api(projects.core.traktAuth.implementation)
 
-
+                api(projects.data.category.api)
                 api(projects.data.category.implementation)
-                api(projects.data.episodes.implementation)
+                api(projects.data.episodeimages.api)
                 api(projects.data.episodeimages.implementation)
+                api(projects.data.episodes.api)
+                api(projects.data.episodes.implementation)
+                api(projects.data.library.api)
                 api(projects.data.library.implementation)
+                api(projects.data.profile.api)
                 api(projects.data.profile.implementation)
+                api(projects.data.profilestats.api)
                 api(projects.data.profilestats.implementation)
-                api(projects.data.similar.implementation)
-                api(projects.data.seasons.implementation)
-                api(projects.data.seasondetails.implementation)
-                api(projects.data.shows.implementation)
-                api(projects.data.showimages.implementation)
-                api(projects.data.trailers.implementation)
+                api(projects.data.requestManager.api)
                 api(projects.data.requestManager.api)
                 api(projects.data.requestManager.implementation)
+                api(projects.data.seasondetails.api)
+                api(projects.data.seasondetails.implementation)
+                api(projects.data.seasons.api)
+                api(projects.data.seasons.implementation)
+                api(projects.data.showimages.api)
+                api(projects.data.showimages.implementation)
+                api(projects.data.shows.api)
+                api(projects.data.shows.implementation)
+                api(projects.data.similar.api)
+                api(projects.data.similar.implementation)
+                api(projects.data.trailers.api)
+                api(projects.data.trailers.implementation)
 
                 implementation(libs.kotlinInject.runtime)
             }
@@ -49,11 +67,16 @@ kotlin {
     }
 }
 
+android {
+    namespace = "com.thomaskioko.tvmaniac.shared"
+}
+
 ksp {
     arg("me.tatarka.inject.generateCompanionExtensions", "true")
 }
 
 dependencies {
+    add("kspAndroid", libs.kotlinInject.compiler)
     add("kspIosX64", libs.kotlinInject.compiler)
     add("kspIosArm64", libs.kotlinInject.compiler)
 }
