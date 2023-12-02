13262f52f38981224f08cf9f7f28dce612cb8d52
Thomas Kioko <kiokotomas@gmail.com>
2023-12-02 20:40:35 +0100

    Add decompose dependencies and remove unused ones.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.dependency.analysis) apply false
-    alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.kmmbridge) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
