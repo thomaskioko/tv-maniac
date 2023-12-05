1c5f7c0d20e5080448bdb5089bb13a073b9084fd
Thomas Kioko <kiokotomas@gmail.com>
2023-12-05 21:17:43 +0100

    Add util dependency

--- presentation/search/build.gradle.kts
+++ presentation/search/build.gradle.kts
@@ -6,6 +6,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
+                implementation(projects.core.util)
 
                 api(libs.decompose.decompose)
                 api(libs.essenty.lifecycle)
