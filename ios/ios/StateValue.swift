41fa56997b8b01db9626bbaed086e2d07c7b7f0e
Thomas Kioko <kiokotomas@gmail.com>
2023-12-06 21:46:50 +0100

    Setup Decompose core navigation on ios 🥳

---
+++ ios/ios/StateValue.swift
@@ -0,0 +1,21 @@
+//
+//  StateValue.swift
+//  tv-maniac
+//
+//  Created by Thomas Kioko on 03.12.23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+import TvManiac
+
+@propertyWrapper struct StateValue<T : AnyObject>: DynamicProperty {
+    @ObservedObject
+    private var obj: ObservableValue<T>
+
+    var wrappedValue: T { obj.value }
+
+    init(_ value: Value<T>) {
+        obj = ObservableValue(value)
+    }
+}
