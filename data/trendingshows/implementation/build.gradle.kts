73af99e62a7159a31a684cfb8700045cb06e6d73
Thomas Kioko <kiokotomas@gmail.com>
2023-12-11 19:20:07 +0100

    Add trending shows data implementation.

---
+++ data/trendingshows/implementation/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("plugin.tvmaniac.multiplatform")
+    alias(libs.plugins.ksp)
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(projects.core.database)
+                implementation(projects.data.trendingshows.api)
+                implementation(projects.core.tmdbApi.api)
+                implementation(projects.core.util)
+                implementation(projects.data.requestManager.api)
+
+                api(libs.coroutines.core)
+
+                implementation(libs.kotlinInject.runtime)
+                implementation(libs.sqldelight.extensions)
+                implementation(libs.kotlinx.atomicfu)
+                implementation(libs.store5)
+            }
+        }
+    }
+}
+
+dependencies {
+    add("kspIosX64", libs.kotlinInject.compiler)
+    add("kspIosArm64", libs.kotlinInject.compiler)
+}
---
+++ data/trendingshows/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/discover/implementation/TrendingShowsStore.kt
@@ -0,0 +1,97 @@
+package com.thomaskioko.tvmaniac.discover.implementation
+
+import com.thomaskioko.tvmaniac.category.api.model.Category
+import com.thomaskioko.tvmaniac.core.db.TrendingShows
+import com.thomaskioko.tvmaniac.core.db.Trending_shows
+import com.thomaskioko.tvmaniac.core.db.Tvshows
+import com.thomaskioko.tvmaniac.db.Id
+import com.thomaskioko.tvmaniac.discover.api.TrendingShowsDao
+import com.thomaskioko.tvmaniac.resourcemanager.api.LastRequest
+import com.thomaskioko.tvmaniac.resourcemanager.api.RequestManagerRepository
+import com.thomaskioko.tvmaniac.shows.api.TvShowsDao
+import com.thomaskioko.tvmaniac.tmdb.api.DEFAULT_API_PAGE
+import com.thomaskioko.tvmaniac.tmdb.api.TmdbShowsNetworkDataSource
+import com.thomaskioko.tvmaniac.tmdb.api.model.TmdbShowResponse
+import com.thomaskioko.tvmaniac.util.FormatterUtil
+import com.thomaskioko.tvmaniac.util.model.ApiResponse
+import com.thomaskioko.tvmaniac.util.model.AppCoroutineScope
+import me.tatarka.inject.annotations.Inject
+import org.mobilenativefoundation.store.store5.Fetcher
+import org.mobilenativefoundation.store.store5.SourceOfTruth
+import org.mobilenativefoundation.store.store5.Store
+import org.mobilenativefoundation.store.store5.StoreBuilder
+
+@Inject
+class TrendingShowsStore(
+    private val tmdbRemoteDataSource: TmdbShowsNetworkDataSource,
+    private val requestManagerRepository: RequestManagerRepository,
+    private val trendingShowsDao: TrendingShowsDao,
+    private val tvShowsDao: TvShowsDao,
+    private val formatterUtil: FormatterUtil,
+    private val scope: AppCoroutineScope,
+) : Store<String, List<TrendingShows>> by StoreBuilder.from<String, List<TmdbShowResponse>, List<TrendingShows>>(
+    fetcher = Fetcher.of { timeWindow ->
+        when (val response = tmdbRemoteDataSource.getTrendingShows(timeWindow)) {
+            is ApiResponse.Success -> {
+                response.body.results
+            }
+
+            is ApiResponse.Error.GenericError -> {
+                throw Throwable("${response.errorMessage}")
+            }
+
+            is ApiResponse.Error.HttpError -> {
+                throw Throwable("${response.code} - ${response.errorMessage}")
+            }
+
+            is ApiResponse.Error.SerializationError -> {
+                throw Throwable("${response.errorMessage}")
+            }
+        }
+    },
+    sourceOfTruth = SourceOfTruth.Companion.of(
+        reader = { _ -> trendingShowsDao.observeTvShow() },
+        writer = { _, trendingShows ->
+            trendingShows.forEach { show ->
+                tvShowsDao.upsert(
+                    Tvshows(
+                        id = Id(show.id.toLong()),
+                        name = show.name,
+                        overview = show.overview,
+                        language = show.originalLanguage,
+                        status = null,
+                        first_air_date = show.firstAirDate,
+                        popularity = show.popularity,
+                        episode_numbers = null,
+                        last_air_date = null,
+                        season_numbers = null,
+                        vote_average = show.voteAverage,
+                        vote_count = show.voteCount.toLong(),
+                        genre_ids = show.genreIds,
+                        poster_path = show.posterPath?.let {
+                            formatterUtil.formatTmdbPosterPath(it)
+                        },
+                        backdrop_path = show.backdropPath?.let {
+                            formatterUtil.formatTmdbPosterPath(it)
+                        },
+                    ),
+                )
+
+                trendingShowsDao.upsert(
+                    Trending_shows(
+                        id = Id(show.id.toLong()),
+                        page = Id(DEFAULT_API_PAGE),
+                    ),
+                )
+            }
+            requestManagerRepository.insert(
+                LastRequest(
+                    id = Category.TRENDING_TODAY.id,
+                    entityId = Category.TRENDING_TODAY.id,
+                    requestType = Category.TRENDING_TODAY.name,
+                ),
+            )
+        },
+    ),
+).scope(scope.io)
+    .build()
