d2c84abab6747b5e1e4a1ca1c07ca0895c1ae025
Thomas Kioko <kiokotomas@gmail.com>
2023-12-13 19:27:04 +0100

    Delete old DB implementation.

--- core/database/src/commonMain/kotlin/com.thomaskioko.tvmaniac.db/DatabaseIdsExt.kt
+++ core/database/src/commonMain/kotlin/com.thomaskioko.tvmaniac.db/DatabaseIdsExt.kt
@@ -3,13 +3,13 @@ package com.thomaskioko.tvmaniac.db
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class CategoryId(val id: Long)
-
-@JvmInline
 value class EpisodeId(val traktId: Long)
 
 @JvmInline
 value class EpisodeImageId(val traktId: Long)
+
+@JvmInline
+value class GenreId(val id: Long)
 
 @JvmInline
 value class NetworkId(val id: Long)
@@ -19,9 +19,6 @@ value class PageId(val id: Long)
 
 @JvmInline
 value class SeasonId(val id: Long)
-
-@JvmInline
-value class ShowId(val traktId: Long)
 
 @JvmInline
 value class SimilarShowId(val id: Long)
