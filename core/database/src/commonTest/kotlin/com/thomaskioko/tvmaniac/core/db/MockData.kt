296025fd38b14c09ea9328a8722a4322483ca827
Thomas Kioko <kiokotomas@gmail.com>
2023-12-14 14:47:10 +0100

    Cleanup mocks.

--- core/database/src/commonTest/kotlin/com/thomaskioko/tvmaniac/core/db/MockData.kt
+++ core/database/src/commonTest/kotlin/com/thomaskioko/tvmaniac/core/db/MockData.kt
@@ -30,66 +30,70 @@ object MockData {
         ),
     )
 
-    fun getShow() = Show(
+    fun getShow() = Tvshows(
         id = Id(84958),
-        title = "Loki",
+        name = "Loki",
         overview = "After stealing the Tesseract during the events of “Avengers: Endgame,” " +
-            "an alternate version of Loki is brought to the mysterious Time Variance " +
-            "Authority, a bureaucratic organization that exists outside of time and " +
-            "space and monitors the timeline. They give Loki a choice: face being " +
-            "erased from existence due to being a “time variant”or help fix " +
-            "the timeline and stop a greater threat.",
-        language = "en",
-        votes = 4958,
-        rating = 8.1,
-        genres = listOf("Horror"),
-        year = "2019",
-        status = "Ended",
-        tmdb_id = 126280,
-        runtime = 0,
-        aired_episodes = 12,
-    )
-
-    fun showList() = listOf(
-        Show(
-            id = Id(84958),
-            title = "Loki",
-            overview = "After stealing the Tesseract during the events of “Avengers: Endgame,” " +
                 "an alternate version of Loki is brought to the mysterious Time Variance " +
                 "Authority, a bureaucratic organization that exists outside of time and " +
                 "space and monitors the timeline. They give Loki a choice: face being " +
                 "erased from existence due to being a “time variant”or help fix " +
                 "the timeline and stop a greater threat.",
-            language = "en",
-            votes = 4958,
-            rating = 8.1,
-            genres = listOf("Horror"),
-            year = "2019",
-            status = "Ended",
-            tmdb_id = 126280,
-            runtime = 0,
-            aired_episodes = 12,
-        ),
-        Show(
-            id = Id(126280),
-            title = "Sex/Life",
-            overview = "A woman's daring sexual past collides with her married-with-kids " +
-                "present when the bad-boy ex she can't stop fantasizing about crashes " +
-                "back into her life.",
-            language = "en",
-            votes = 4958,
-            rating = 8.1,
-            genres = listOf("Horror"),
-            year = "2019",
-            status = "Ended",
-            tmdb_id = 126280,
-            runtime = 0,
-            aired_episodes = 12,
-        ),
+        language = "en",
+        vote_count = 4958,
+        popularity = 8.1,
+        genre_ids = listOf(12, 14, 16, 18, 27, 28),
+        first_air_date = "2019",
+        status = "Ended",
+        poster_path = "/yDWJYRAwMNKbIYT8ZB33qy84uzO.jpg",
+        backdrop_path = "/yDWJYRAwMNKbIYT8ZB33qy84uzO.jpg",
+        episode_numbers = "10",
+        season_numbers = "1",
+        vote_average = 8.1,
+        last_air_date = "2019",
     )
 
-    fun showCategory(traktId: Long, categoryId: Long) = Show_category(
-        id = Id(traktId),
-        category_id = Id(categoryId),
+    fun showList() = listOf(
+        Tvshows(
+            id = Id(84958),
+            name = "Loki",
+            overview = "After stealing the Tesseract during the events of “Avengers: Endgame,” " +
+                    "an alternate version of Loki is brought to the mysterious Time Variance " +
+                    "Authority, a bureaucratic organization that exists outside of time and " +
+                    "space and monitors the timeline. They give Loki a choice: face being " +
+                    "erased from existence due to being a “time variant”or help fix " +
+                    "the timeline and stop a greater threat.",
+            language = "en",
+            vote_count = 4958,
+            popularity = 8.1,
+            genre_ids = listOf(12, 14, 16, 18, 27, 28),
+            first_air_date = "2019",
+            status = "Ended",
+            poster_path = "/yDWJYRAwMNKbIYT8ZB33qy84uzO.jpg",
+            backdrop_path = "/yDWJYRAwMNKbIYT8ZB33qy84uzO.jpg",
+            episode_numbers = "10",
+            season_numbers = "1",
+            vote_average = 8.1,
+            last_air_date = "2019",
+        ),
+        Tvshows(
+            id = Id(126280),
+            name = "Sex/Life",
+            overview = "A woman's daring sexual past collides with her married-with-kids " +
+                    "present when the bad-boy ex she can't stop fantasizing about crashes " +
+                    "back into her life.",
+            language = "en",
+            vote_count = 4958,
+            popularity = 8.1,
+            genre_ids = listOf(12, 14, 16, 18, 27, 28),
+            first_air_date = "2019",
+            status = "Ended",
+            poster_path = "/yDWJYRAwMNKbIYT8ZB33qy84uzO.jpg",
+            backdrop_path = "/yDWJYRAwMNKbIYT8ZB33qy84uzO.jpg",
+            episode_numbers = "10",
+            season_numbers = "1",
+            vote_average = 8.1,
+            last_air_date = "2019",
+        ),
     )
 }
