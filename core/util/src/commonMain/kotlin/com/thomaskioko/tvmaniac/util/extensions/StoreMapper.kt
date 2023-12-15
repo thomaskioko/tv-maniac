2ca957d446cc334b6d3f3b323c5dedd627fc8114
Thomas Kioko <kiokotomas@gmail.com>
2023-12-15 19:29:47 +0100

    Return exception if it exists.

--- core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/extensions/StoreMapper.kt
+++ core/util/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/extensions/StoreMapper.kt
@@ -1,11 +1,11 @@
 package com.thomaskioko.tvmaniac.util.extensions
 
+import com.thomaskioko.tvmaniac.util.model.DefaultError
 import com.thomaskioko.tvmaniac.util.model.Either
 import com.thomaskioko.tvmaniac.util.model.Failure
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import org.mobilenativefoundation.store.store5.StoreReadResponse
@@ -13,11 +13,9 @@ import org.mobilenativefoundation.store.store5.StoreReadResponse
 @OptIn(ExperimentalCoroutinesApi::class)
 fun <T> Flow<StoreReadResponse<T>>.mapResult(): Flow<Either<Failure, T>> =
     distinctUntilChanged()
-        .flatMapLatest {
-            val data = it.dataOrNull()
-            if (data != null) {
-                flowOf(Either.Right(data))
-            } else {
-                emptyFlow()
+        .flatMapLatest { result ->
+            when (val data = result.dataOrNull()) {
+                null -> flowOf(Either.Left(DefaultError(result.errorMessageOrNull())))
+                else -> flowOf(Either.Right(data))
             }
         }
