fc5386b8f51ce06fdcef7c849d2804713ebc7d99
Thomas Kioko <kiokotomas@gmail.com>
2024-02-23 19:42:57 +0100

    Cleanup presenter implementation.

--- presentation/discover/src/commonMain/kotlin/com/thomaskioko/tvmaniac/presentation/discover/DiscoverShowsPresenter.kt
+++ presentation/discover/src/commonMain/kotlin/com/thomaskioko/tvmaniac/presentation/discover/DiscoverShowsPresenter.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -24,7 +25,7 @@ import me.tatarka.inject.annotations.Assisted
 import me.tatarka.inject.annotations.Inject
 
 typealias DiscoverShowsPresenterFactory =
-    (
+  (
     ComponentContext,
     onNavigateToShowDetails: (id: Long) -> Unit,
     onNavigateToMore: (categoryId: Long) -> Unit,
@@ -52,7 +53,7 @@ class DiscoverShowsPresenter(
     _state.asValue(initialValue = _state.value, lifecycle = lifecycle)
 
   init {
-    fetchContent()
+    coroutineScope.launch { observeShowData() }
   }
 
   fun dispatch(action: DiscoverShowAction) {
@@ -65,12 +66,12 @@ class DiscoverShowsPresenter(
       RefreshData ->
         coroutineScope.launch {
           _state.update { (it as? DataLoaded)?.copy(isRefreshing = true) ?: it }
-          fetchShowData(true)
+          observeShowData(true)
         }
       ReloadData ->
         coroutineScope.launch {
           _state.update { Loading }
-          fetchShowData(true)
+          observeShowData(true)
         }
       SnackBarDismissed ->
         coroutineScope.launch {
@@ -84,64 +85,39 @@ class DiscoverShowsPresenter(
     }
   }
 
-  private fun fetchContent() {
-    coroutineScope.launch {
-      fetchShowData()
-      observeShowData()
-    }
+  private fun isEmpty(vararg responses: List<ShowEntity>?): Boolean {
+    return responses.all { it.isNullOrEmpty() }
   }
 
-  private suspend fun fetchShowData(refresh: Boolean = false) {
-    val featuredResponse = featuredShowsRepository.fetchFeaturedShows(forceRefresh = refresh)
-    val topRatedResponse = topRatedShowsRepository.fetchTopRatedShows(forceRefresh = refresh)
-    val popularResponse = popularShowsRepository.fetchPopularShows(forceRefresh = refresh)
-    val upcomingResponse = upcomingShowsRepository.fetchUpcomingShows(forceRefresh = refresh)
-    val trendingShows = trendingShowsRepository.fetchTrendingShows(forceRefresh = refresh)
-
-    if (
-      isEmpty(
-        featuredResponse,
-        topRatedResponse,
-        popularResponse,
-        upcomingResponse,
-        trendingShows,
-      )
-    ) {
-      return _state.update { EmptyState }
-    }
-    _state.update {
-      DataLoaded(
-        topRatedShows = topRatedResponse.toShowList(),
-        popularShows = popularResponse.toShowList(),
-        upcomingShows = upcomingResponse.toShowList(),
-        featuredShows = featuredResponse.toShowList(),
-        trendingToday = trendingShows.toShowList(),
-        isRefreshing = false,
-      )
-    }
-  }
-
-  private fun isEmpty(vararg responses: List<ShowEntity>): Boolean {
-    return responses.all { it.isEmpty() }
-  }
-
-  private suspend fun observeShowData() {
+  private suspend fun observeShowData(refresh: Boolean = false) {
     combine(
-        featuredShowsRepository.observeFeaturedShows(),
-        topRatedShowsRepository.observeTopRatedShows(),
-        popularShowsRepository.observePopularShows(),
-        upcomingShowsRepository.observeUpcomingShows(),
-        trendingShowsRepository.observeTrendingShows(),
+        featuredShowsRepository.observeFeaturedShows(forceRefresh = refresh),
+        topRatedShowsRepository.observeTopRatedShows(forceRefresh = refresh),
+        popularShowsRepository.observePopularShows(forceRefresh = refresh),
+        upcomingShowsRepository.observeUpcomingShows(forceRefresh = refresh),
+        trendingShowsRepository.observeTrendingShows(forceRefresh = refresh),
       ) { featured, topRated, popular, upcomingShows, trendingToday ->
-        _state.update {
-          DataLoaded(
-            featuredShows = featured.getOrNull().toShowList(),
-            topRatedShows = topRated.getOrNull().toShowList(),
-            popularShows = popular.getOrNull().toShowList(),
-            upcomingShows = upcomingShows.getOrNull().toShowList(),
-            trendingToday = trendingToday.getOrNull().toShowList(),
-            errorMessage = getErrorMessage(topRated, popular, upcomingShows, featured),
+        if (
+          isEmpty(
+            featured.getOrNull(),
+            featured.getOrNull(),
+            featured.getOrNull(),
+            featured.getOrNull(),
+            featured.getOrNull(),
           )
+        ) {
+          _state.update { EmptyState }
+        } else {
+          _state.update {
+            DataLoaded(
+              featuredShows = featured.getOrNull().toShowList(),
+              topRatedShows = topRated.getOrNull().toShowList(),
+              popularShows = popular.getOrNull().toShowList(),
+              upcomingShows = upcomingShows.getOrNull().toShowList(),
+              trendingToday = trendingToday.getOrNull().toShowList(),
+              errorMessage = getErrorMessage(topRated, popular, upcomingShows, featured),
+            )
+          }
         }
       }
       .onStart { Loading }
