d3caa878deb8955e4a1d0a11a95c1a92b618af37
Thomas Kioko <kiokotomas@gmail.com>
2024-07-16 17:15:57 +0200

    Update Button views. Add title and refresh button and add elevation effect when clicked.

--- ios/ios/Components/BorderedButton.swift
+++ ios/ios/Components/BorderedButton.swift
@@ -9,68 +9,63 @@
 import SwiftUI
 
 public struct BorderedButton : View {
-	public let text: String
-	public let systemImageName: String
-	public let color: Color
-	public let borderColor: Color
-	public let isOn: Bool
-	public let action: () -> Void
-	
-	public init(text: String, systemImageName: String, color: Color, borderColor: Color, isOn: Bool, action: @escaping () -> Void) {
-		self.text = text
-		self.systemImageName = systemImageName
-		self.color = color
-		self.isOn = isOn
-		self.action = action
-		self.borderColor = borderColor
-	}
-	
-	public var body: some View {
-		Button(action: {
-			self.action()
-		}, label: {
-			HStack(alignment: .center, spacing: 4) {
-				Image(systemName: systemImageName)
-					.resizable()
-					.aspectRatio(contentMode: .fit)
-					.foregroundColor(isOn ? Color.accent : color)
-					.frame(width: 24, height: 24)
-					.padding(.trailing, 16)
-				
-				Text(text)
+    public let text: String
+    public let systemImageName: String
+    public let isOn: Bool
+    public let action: () -> Void
+    public var color: Color = .accent
+    public var borderColor: Color = .accent
+
+    public init(text: String, systemImageName: String, isOn: Bool, action: @escaping () -> Void) {
+        self.text = text
+        self.systemImageName = systemImageName
+        self.isOn = isOn
+        self.action = action
+    }
+
+    public var body: some View {
+        Button(action: {
+            self.action()
+        }, label: {
+            HStack(alignment: .center, spacing: 4) {
+                Image(systemName: systemImageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(isOn ? Color.accent : color)
+                    .frame(width: 24, height: 24)
+                    .padding(.trailing, 16)
+
+                Text(text)
                     .bodyMediumFont(size: 16)
                     .foregroundColor(Color.accent)
-			}
-		})
-			.buttonStyle(BorderlessButtonStyle())
-			.padding(12)
-			.background(
-				RoundedRectangle(cornerRadius: 5)
-					.stroke(borderColor, lineWidth: 2)
-					.background(isOn ? color : .clear)
-					.cornerRadius(2))
-	}
+            }
+        })
+        .buttonStyle(BorderlessButtonStyle())
+        .padding(12)
+        .background(
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(borderColor, lineWidth: 2)
+                .background(isOn ? color : .clear)
+                .cornerRadius(2)
+        )
+    }
 }
 
 struct BorderedButton_Previews : PreviewProvider {
-	static var previews: some View {
-		VStack {
-			BorderedButton(text: "Watch Trailer",
-						   systemImageName: "film",
-						   color: .yellow_300,
-						   borderColor: .grey_900,
-						   isOn: false,
-						   action: {
-				
-			})
-			BorderedButton(text: "Follow",
-						   systemImageName: "film",
-						   color: .yellow_300,
-						   borderColor: .grey_900,
-						   isOn: true,
-						   action: {
-				
-			})
-		}
-	}
+    static var previews: some View {
+        VStack {
+            BorderedButton(
+                text: "Watch Trailer",
+                systemImageName: "film",
+                isOn: false,
+                action: { }
+            )
+            BorderedButton(
+                text: "Follow",
+                systemImageName: "film",
+                isOn: true,
+                action: { }
+            )
+        }
+    }
 }
