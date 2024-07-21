12347acf5ae3444a057cb41f41b6fa3c979a0d5d
Thomas Kioko <kiokotomas@gmail.com>
2024-07-21 14:52:45 +0200

    Fix gesture recognizer conflicts between swipe gesture for navigation and the SnapCarousel drag gesture and cleanup DiscoverView.

--- ios/ios/Discover/DiscoverView.swift
+++ ios/ios/Discover/DiscoverView.swift
@@ -3,7 +3,7 @@ import TvManiac
 import os.log
 
 struct DiscoverView: View {
-    
+
     @Environment(\.colorScheme) var scheme
     @State private var currentIndex: Int = 2
 
@@ -22,19 +22,158 @@ struct DiscoverView: View {
         VStack {
             switch onEnum(of: uiState.value) {
                 case .loading:
-                LoadingIndicatorView()
-                    .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height,  alignment: .center)
-                case .dataLoaded: loadedContent
-                case .emptyState: emptyView
-                case .errorState(let error):  FullScreenView(
-                    systemName: "exclamationmark.arrow.triangle.2.circlepath",
-                    message: error.errorMessage ?? "Something went wrong!!"
-                )
-                case .none: emptyView
+                    LoadingIndicatorView()
+                        .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height,  alignment: .center)
+                case .dataLoaded(let state):
+                    LoadedContent(state: state)
+                case .emptyState:
+                    emptyView
+                case .errorState(let error):
+                    FullScreenView(
+                        systemName: "exclamationmark.arrow.triangle.2.circlepath",
+                        message: error.errorMessage ?? "Something went wrong!!"
+                    )
+                case .none:
+                    emptyView
             }
         }
     }
-    
+
+    @ViewBuilder
+    private func LoadedContent(state: DataLoaded) -> some View {
+        ZStack {
+            BackgroundView(state.featuredShows)
+
+            ScrollableContent(state: state)
+                .refreshable {
+                    presenter.dispatch(action: RefreshData())
+                }
+        }
+    }
+
+    @ViewBuilder
+    func ScrollableContent(state: DataLoaded) -> some View {
+        ScrollView(showsIndicators: false) {
+
+            if(state.errorMessage != nil) {
+                FullScreenView(systemName: "exclamationmark.triangle", message: state.errorMessage!)
+            } else {
+
+                FeaturedContentView(state.featuredShows)
+
+                HorizontalItemContentListView(
+                    items: state.upcomingShows,
+                    title: "Upcoming",
+                    onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
+                    onMoreClicked: { presenter.dispatch(action: UpComingClicked()) }
+                )
+
+                HorizontalItemContentListView(
+                    items: state.trendingToday,
+                    title: "Trending Today",
+                    onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
+                    onMoreClicked: { presenter.dispatch(action: TrendingClicked()) }
+                )
+
+                HorizontalItemContentListView(
+                    items: state.popularShows,
+                    title: "Popular",
+                    onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
+                    onMoreClicked: { presenter.dispatch(action: PopularClicked()) }
+                )
+
+                HorizontalItemContentListView(
+                    items: state.topRatedShows,
+                    title: "Top Rated",
+                    onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
+                    onMoreClicked: { presenter.dispatch(action: TopRatedClicked()) }
+                )
+            }
+        }
+    }
+
+
+    @ViewBuilder
+    func FeaturedContentView(_ shows: [DiscoverShow]?) -> some View {
+        if let shows = shows {
+            if !shows.isEmpty {
+
+                SnapCarousel(
+                    spacing: 10,
+                    trailingSpace: 120,
+                    index: $currentIndex,
+                    items: shows
+                ) { show  in
+
+                    GeometryReader{ proxy in
+
+                        FeaturedContentPosterView(
+                            show: show,
+                            onClick: { id in presenter.dispatch(action: ShowClicked(id: show.tmdbId)) }
+                        )
+                    }
+                }
+                .edgesIgnoringSafeArea(.all)
+                .frame(height: 450)
+                .padding(.top, 70)
+
+                CustomIndicator(shows)
+                    .padding()
+                    .padding(.top, 10)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func BackgroundView(_ tvShows: [DiscoverShow]?) -> some View {
+        if let shows = tvShows {
+            if !shows.isEmpty {
+                GeometryReader { proxy in
+
+                    TabView(selection: $currentIndex) {
+                        ForEach(shows.indices, id: \.self) { index in
+                            TransparentImageBackground(imageUrl: shows[index].posterImageUrl)
+                                .tag(index)
+                        }
+
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                    .animation(.easeInOut, value: currentIndex)
+
+
+                    let color: Color = (scheme == .dark ? .black : .white)
+                    // Custom Gradient
+                    LinearGradient(colors: [
+                        .black,
+                        .clear,
+                        color.opacity(0.15),
+                        color.opacity(0.5),
+                        color.opacity(0.8),
+                        color,
+                        color
+                    ], startPoint: .top, endPoint: .bottom)
+
+                    // Blurred Overlay
+                    Rectangle()
+                        .fill(.ultraThinMaterial)
+                }
+                .ignoresSafeArea()
+            }
+        }
+    }
+
+    @ViewBuilder
+    func CustomIndicator(_ shows: [DiscoverShow]) -> some View {
+        HStack(spacing: 5) {
+            ForEach(shows.indices, id: \.self) { index in
+                Circle()
+                    .fill(currentIndex == index ? Color.accent: .gray.opacity(0.5))
+                    .frame(width: currentIndex == index ? 10 : 6, height: currentIndex == index ? 10 : 6)
+            }
+        }
+        .animation(.easeInOut, value: currentIndex)
+    }
+
     @ViewBuilder
     private var emptyView : some View {
         VStack {
@@ -44,16 +183,16 @@ struct DiscoverView: View {
                 .foregroundColor(Color.accent)
                 .font(Font.title.weight(.thin))
                 .frame(width: 160, height: 180)
-            
+
             Text("Looks like your stash is empty")
                 .titleSemiBoldFont(size: 18)
                 .padding(.top, 8)
-            
+
             Text("Could be that you forgot to add your TMDB API Key. Once you set that up, you can get lost in the vast world of Tmdb's collection.")
                 .captionFont(size: 16)
                 .padding(.top, 1)
                 .padding(.bottom, 16)
-            
+
             Button(action: {
                 presenter.dispatch(action: ReloadData())
             }, label: {
@@ -68,139 +207,10 @@ struct DiscoverView: View {
                     .stroke(Color.accent, lineWidth: 2)
                     .background(.clear)
                     .cornerRadius(2))
-            
+
         }
         .frame(maxWidth: .infinity,maxHeight: .infinity)
         .padding([.trailing, .leading], 16)
     }
-    
-    @ViewBuilder
-    private var loadedContent: some View {
-        ZStack {
-            let contentState = uiState.value as! DataLoaded
 
-            BackgroundView(contentState.featuredShows)
-            
-            ScrollView(showsIndicators: false) {
-                
-                let state = contentState
-                
-                if(state.errorMessage != nil) {
-                    FullScreenView(systemName: "exclamationmark.triangle", message: state.errorMessage!)
-                } else {
-                    
-                    FeaturedContentView(state.featuredShows)
-                    
-                    HorizontalItemContentListView(
-                        items: state.upcomingShows,
-                        title: "Upcoming",
-                        onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
-                        onMoreClicked: { presenter.dispatch(action: UpComingClicked()) }
-                    )
-                    
-                    HorizontalItemContentListView(
-                        items: state.trendingToday,
-                        title: "Trending Today",
-                        onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
-                        onMoreClicked: { presenter.dispatch(action: TrendingClicked()) }
-                    )
-                    
-                    HorizontalItemContentListView(
-                        items: state.popularShows,
-                        title: "Popular",
-                        onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
-                        onMoreClicked: { presenter.dispatch(action: PopularClicked()) }
-                    )
-                    
-                    HorizontalItemContentListView(
-                        items: state.topRatedShows,
-                        title: "Top Rated",
-                        onClick: { id in presenter.dispatch(action: ShowClicked(id: id)) },
-                        onMoreClicked: { presenter.dispatch(action: TopRatedClicked()) }
-                    )
-                }
-            }
-            .refreshable {
-                presenter.dispatch(action: RefreshData())
-            }
-        }
-    }
-    
-    
-    @ViewBuilder
-    func FeaturedContentView(_ shows: [DiscoverShow]?) -> some View {
-        if let shows = shows {
-            if !shows.isEmpty {
-                SnapCarousel(spacing: 10, trailingSpace: 120, index: $currentIndex, items: shows) { show  in
-                    
-                    GeometryReader{ proxy in
-                        
-                        FeaturedContentPosterView(
-                            show: show,
-                            onClick: { id in presenter.dispatch(action: ShowClicked(id: show.tmdbId)) }
-                        )
-                    }
-                }
-                .edgesIgnoringSafeArea(.all)
-                .frame(height: 450)
-                .padding(.top, 70)
-                
-                
-                CustomIndicator(shows)
-                    .padding()
-                    .padding(.top, 10)
-            }
-        }
-    }
-    
-    @ViewBuilder
-    func BackgroundView(_ tvShows: [DiscoverShow]?) -> some View {
-        if let shows = tvShows {
-            if !shows.isEmpty {
-                GeometryReader { proxy in
-                    
-                    TabView(selection: $currentIndex) {
-                        ForEach(shows.indices, id: \.self) { index in
-                            TransparentImageBackground(imageUrl: shows[index].posterImageUrl)
-                                .tag(index)
-                        }
-                        
-                    }
-                    .tabViewStyle(.page(indexDisplayMode: .never))
-                    .animation(.easeInOut, value: currentIndex)
-                    
-                    
-                    let color: Color = (scheme == .dark ? .black : .white)
-                    // Custom Gradient
-                    LinearGradient(colors: [
-                        .black,
-                        .clear,
-                        color.opacity(0.15),
-                        color.opacity(0.5),
-                        color.opacity(0.8),
-                        color,
-                        color
-                    ], startPoint: .top, endPoint: .bottom)
-                    
-                    // Blurred Overlay
-                    Rectangle()
-                        .fill(.ultraThinMaterial)
-                }
-                .ignoresSafeArea()
-            }
-        }
-    }
-    
-    @ViewBuilder
-    func CustomIndicator(_ shows: [DiscoverShow]) -> some View {
-        HStack(spacing: 5) {
-            ForEach(shows.indices, id: \.self) { index in
-                Circle()
-                    .fill(currentIndex == index ? Color.accent: .gray.opacity(0.5))
-                    .frame(width: currentIndex == index ? 10 : 6, height: currentIndex == index ? 10 : 6)
-            }
-        }
-        .animation(.easeInOut, value: currentIndex)
-    }
-    
 }
