1a390b2a8edc108d03303d1942479e849ac1897a
Thomas Kioko <kiokotomas@gmail.com>
2024-09-13 16:41:35 +0200

    Create SnapshotTesting package and add extension classes.

---
+++ ios/Modules/SnapshotTestingLib/Sources/SnapshotTestingLib/SnapshotTesting+Extensions.swift
@@ -0,0 +1,89 @@
+import SnapshotTesting
+import SwiftUI
+
+public extension SwiftUISnapshotLayout {
+    static let defaultDevice = SwiftUISnapshotLayout.device(config: .iPhone13Pro)
+}
+
+public struct SnapshotStyles: OptionSet {
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    public static let light = SnapshotStyles(rawValue: 1 << 0)
+    public static let dark = SnapshotStyles(rawValue: 1 << 1)
+    public static let all: SnapshotStyles = [.light, .dark]
+}
+
+public extension Snapshotting where Value == UIViewController, Format == UIImage {
+    static func image(
+        precision: Float = 1,
+        perceptualPrecision: Float = 1,
+        layout: SwiftUISnapshotLayout = .sizeThatFits,
+        traits: UITraitCollection = .init()
+    ) -> Snapshotting<UIViewController, UIImage> {
+        let config = switch layout {
+        case let .device(config: deviceConfig): deviceConfig
+        case .sizeThatFits: ViewImageConfig(traits: traits)
+        case let .fixed(width: width, height: height):
+            ViewImageConfig(
+                size: CGSize(width: width, height: height),
+                traits: traits
+            )
+        }
+
+        return .image(
+            on: config,
+            precision: precision,
+            perceptualPrecision: perceptualPrecision,
+            traits: traits
+        )
+    }
+}
+
+public extension View {
+    func assertSnapshot(
+        record recording: Bool = false,
+        layout: SwiftUISnapshotLayout = .sizeThatFits,
+        styles: SnapshotStyles = .all,
+        file: StaticString = #file,
+        testName: String
+    ) {
+        var themes: [String: Snapshotting<UIViewController, UIImage>] = [:]
+
+        if styles.contains(.light) {
+            themes["light"] = .image(
+                layout: layout,
+                traits: UITraitCollection(userInterfaceStyle: .light)
+            )
+        }
+
+        if styles.contains(.dark) {
+            themes["dark"] = .image(
+                layout: layout,
+                traits: UITraitCollection(userInterfaceStyle: .dark)
+            )
+        }
+
+        assertSnapshots(
+            of: viewController,
+            as: themes,
+            record: recording,
+            file: file,
+            testName: testName
+        )
+    }
+
+    private var viewController: UIViewController {
+        let viewController = UIHostingController(rootView: self)
+
+        let view = viewController.view!
+        view.bounds = CGRect(origin: .zero, size: view.intrinsicContentSize)
+        view.frame = UIScreen.main.bounds
+        view.backgroundColor = .clear
+
+        return viewController
+    }
+}
