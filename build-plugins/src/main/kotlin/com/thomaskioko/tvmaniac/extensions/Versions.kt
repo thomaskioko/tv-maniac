19934cd6f51dd57c958299a034351ea0d05189f8
Thomas Kioko <kiokotomas@gmail.com>
2024-11-06 16:01:32 +0100

    Bump up compileSdk to ver 35

--- build-plugins/src/main/kotlin/com/thomaskioko/tvmaniac/extensions/Versions.kt
+++ build-plugins/src/main/kotlin/com/thomaskioko/tvmaniac/extensions/Versions.kt
@@ -1,7 +1,7 @@
 package com.thomaskioko.tvmaniac.extensions
 
 object Versions {
-  const val COMPILE_SDK = 34
+  const val COMPILE_SDK = 35
   const val MIN_SDK = 24
-  const val TARGET_SDK = 34
+  const val TARGET_SDK = 35
 }
