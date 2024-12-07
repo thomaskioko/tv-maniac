b71400a1ad4384f13d779a5377b0a871d5ade84d
Thomas Kioko <kiokotomas@gmail.com>
2024-12-07 14:21:20 +0100

    Added proper lifecycle management in iOSApp using SwiftUI's scenePhase.

--- ios/Modules/TvManiacKit/Sources/TvManiacKit/Decompose/AppDelegate.swift
+++ ios/Modules/TvManiacKit/Sources/TvManiacKit/Decompose/AppDelegate.swift
@@ -10,15 +10,21 @@ import UIKit
 import TvManiac
 
 public class AppDelegate: NSObject, UIApplicationDelegate {
+  public let lifecycle = LifecycleRegistryKt.LifecycleRegistry()
+
   private lazy var appComponent = IosApplicationComponent.companion.create()
 
-  lazy var presenterComponent: IosViewPresenterComponent = appComponent.componentFactory.createComponent(
-    componentContext: DefaultComponentContext(
-      lifecycle: ApplicationLifecycle(),
-      stateKeeper: nil,
-      instanceKeeper: nil,
-      backHandler: nil
-    )
+  public lazy var presenterComponent: IosViewPresenterComponent = appComponent.componentFactory.createComponent(
+    componentContext: DefaultComponentContext(lifecycle: lifecycle)
   )
+
+  public override init() {
+    super.init()
+    LifecycleRegistryExtKt.create(lifecycle)
+  }
+
+  deinit {
+    LifecycleRegistryExtKt.destroy(lifecycle)
+  }
 }
 
--- ios/ios/App/iOSApp.swift
+++ ios/ios/App/iOSApp.swift
@@ -11,9 +11,24 @@ struct iOSApp: App {
   @UIApplicationDelegateAdaptor(AppDelegate.self)
   var appDelegate: AppDelegate
 
+  @Environment(\.scenePhase)
+  var scenePhase: ScenePhase
+
   var body: some Scene {
     WindowGroup {
       RootView(rootPresenter: appDelegate.presenterComponent.rootPresenter)
+        .onChange(of: scenePhase) { newPhase in
+          switch newPhase {
+          case .background:
+            appDelegate.lifecycle.stop()
+          case .inactive:
+            appDelegate.lifecycle.pause()
+          case .active:
+            appDelegate.lifecycle.resume()
+          @unknown default:
+            break
+          }
+        }
     }
   }
 }
