15ea18fdcfde29c57a1b3e799ea18bef407a0ee4
Thomas Kioko <kiokotomas@gmail.com>
2024-12-15 17:58:46 +0100

    Create ShowPosterImage struct and make GridView more dynamic

--- ios/Modules/TvManiacUI/Sources/TvManiacUI/GridView.swift
+++ ios/Modules/TvManiacUI/Sources/TvManiacUI/GridView.swift
@@ -6,11 +6,11 @@ public struct GridView: View {
     private let posterHeight: CGFloat
     private let spacing: CGFloat
     private let columns: [GridItem]
-    private let items: [SwiftShow]
+    private let items: [ShowPosterImage]
     private var onAction: (Int64) -> Void
 
     public init(
-        items: [SwiftShow],
+        items: [ShowPosterImage],
         posterWidth: CGFloat = 130,
         posterHeight: CGFloat = 200,
         spacing: CGFloat = 4,
@@ -51,44 +51,32 @@ public struct GridView: View {
             .init(
                 tmdbId: 1234,
                 title: "Arcane",
-                posterUrl: "https://image.tmdb.org/t/p/w780/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg",
-                backdropUrl: nil,
-                inLibrary: false
+                posterUrl: "https://image.tmdb.org/t/p/w780/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg"
             ),
             .init(
                 tmdbId: 123,
                 title: "The Lord of the Rings: The Rings of Power",
-                posterUrl: "https://image.tmdb.org/t/p/w780/NNC08YmJFFlLi1prBkK8quk3dp.jpg",
-                backdropUrl: nil,
-                inLibrary: false
+                posterUrl: "https://image.tmdb.org/t/p/w780/NNC08YmJFFlLi1prBkK8quk3dp.jpg"
             ),
             .init(
                 tmdbId: 12346,
                 title: "Kaos",
-                posterUrl: "https://image.tmdb.org/t/p/w780/9Piw6Zju39bn3enIDLZzPfjMTBR.jpg",
-                backdropUrl: nil,
-                inLibrary: false
+                posterUrl: "https://image.tmdb.org/t/p/w780/9Piw6Zju39bn3enIDLZzPfjMTBR.jpg"
             ),
             .init(
                 tmdbId: 124,
                 title: "Terminator",
-                posterUrl: "https://image.tmdb.org/t/p/w780/woH18JkZMYhMSWqtHkPA4F6Gd1z.jpg",
-                backdropUrl: nil,
-                inLibrary: false
+                posterUrl: "https://image.tmdb.org/t/p/w780/woH18JkZMYhMSWqtHkPA4F6Gd1z.jpg"
             ),
             .init(
                 tmdbId: 123346,
                 title: "The Perfect Couple",
-                posterUrl: "https://image.tmdb.org/t/p/w780//3buRSGVnutw8x4Lww0t70k5dG6R.jpg",
-                backdropUrl: nil,
-                inLibrary: false
+                posterUrl: "https://image.tmdb.org/t/p/w780//3buRSGVnutw8x4Lww0t70k5dG6R.jpg"
             ),
             .init(
                 tmdbId: 2346,
                 title: "One Piece",
-                posterUrl: "https://image.tmdb.org/t/p/w780/2rmK7mnchw9Xr3XdiTFSxTTLXqv.jpg",
-                backdropUrl: nil,
-                inLibrary: false
+                posterUrl: "https://image.tmdb.org/t/p/w780/2rmK7mnchw9Xr3XdiTFSxTTLXqv.jpg"
             ),
         ],
         onAction: { _ in }
---
+++ ios/Modules/TvManiacUI/Sources/TvManiacUI/Models/ShowPosterImage.swift
@@ -0,0 +1,56 @@
+import Foundation
+import TvManiac
+
+public struct ShowPosterImage: Identifiable {
+  public let id: UUID = UUID()
+  public let tmdbId: Int64
+  public let title: String
+  public let posterUrl: String?
+  public let inLibrary: Bool
+
+  public init(
+    tmdbId: Int64,
+    title: String,
+    posterUrl: String?,
+    inLibrary: Bool = false
+  ) {
+    self.tmdbId = tmdbId
+    self.title = title
+    self.posterUrl = posterUrl
+    self.inLibrary = inLibrary
+  }
+}
+
+
+public extension TvManiac.LibraryItem {
+  func toSwift() -> ShowPosterImage {
+    .init(
+      tmdbId: tmdbId,
+      title: title,
+      posterUrl: posterImageUrl,
+      inLibrary: true
+    )
+  }
+}
+
+public extension SeasonImagesModel {
+  func toSwift() -> ShowPosterImage {
+    .init(
+      tmdbId: id,
+      title: "",
+      posterUrl: imageUrl,
+      inLibrary: false
+    )
+  }
+}
+
+public extension TvManiac.TvShow {
+  func toSwift() -> ShowPosterImage {
+    .init(
+      tmdbId: tmdbId,
+      title: title,
+      posterUrl: posterImageUrl,
+      inLibrary: inLibrary
+    )
+  }
+}
