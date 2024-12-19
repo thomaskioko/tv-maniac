d6868b71b17a9c91ba8d88cb7386183068855167
Thomas Kioko <kiokotomas@gmail.com>
2024-12-19 16:41:18 +0100

    Add mapper extension.

--- ios/Modules/TvManiacUI/Sources/TvManiacUI/Models/SwiftShow.swift
+++ ios/Modules/TvManiacUI/Sources/TvManiacUI/Models/SwiftShow.swift
@@ -1,4 +1,5 @@
 import Foundation
+import TvManiac
 
 public struct SwiftShow: Identifiable {
     public let id: UUID = .init()
@@ -24,4 +25,17 @@ public struct SwiftShow: Identifiable {
         self.inLibrary = inLibrary
         self.overview = overview
     }
+}
+
+public extension DiscoverShow {
+  func toSwift() -> SwiftShow {
+    .init(
+      tmdbId: tmdbId,
+      title: title,
+      posterUrl: posterImageUrl,
+      backdropUrl: nil,
+      inLibrary: inLibrary,
+      overview: overView
+    )
+  }
 }
