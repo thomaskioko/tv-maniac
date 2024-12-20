0367dd46d0248dbb6466a70e5df4e2d910d041f4
Thomas Kioko <kiokotomas@gmail.com>
2024-12-20 18:43:10 +0100

    Improve state handling and prevent the app from showing a blank screen for a few seconds.

--- ios/ios/UI/ShowDetails/ShowDetailsView.swift
+++ ios/ios/UI/ShowDetails/ShowDetailsView.swift
@@ -15,92 +15,97 @@ struct ShowDetailsView: View {
   }
 
   var body: some View {
-    if !uiState.isUpdating && uiState.showDetails == nil {
-      FullScreenView(
-        buttonText: "Retry",
-        action: { presenter.dispatch(action: ReloadShowDetails()) }
-      )
-    } else if let showDetails = uiState.showDetails {
-      ParallaxView(
-        imageHeight: DimensionConstants.imageHeight,
-        collapsedImageHeight: DimensionConstants.collapsedImageHeight,
-        header: { proxy in
-          HeaderView(
-            title: showDetails.title,
-            overview: showDetails.overview,
-            backdropImageUrl: showDetails.backdropImageUrl,
-            status: showDetails.status,
-            year: showDetails.year,
-            language: showDetails.language,
-            rating: showDetails.rating,
-            progress: proxy.getTitleOpacity(
-              geometry: proxy,
-              imageHeight: DimensionConstants.imageHeight,
-              collapsedImageHeight: DimensionConstants.collapsedImageHeight
-            ),
-            headerHeight: proxy.getHeightForHeaderImage(proxy)
-          )
-        },
-        content: {
-          showInfoDetails(show: showDetails)
-        },
-        onScroll: { offset in
-          let opacity = -offset - 170
-          let normalizedOpacity = opacity / 220
-          showGlass = max(0, min(1, normalizedOpacity))
-        }
-      )
-      .background(Color.background)
-      .navigationBarTitleDisplayMode(.inline)
-      .navigationBarColor(backgroundColor: .clear)
-      .overlay(
-        GlassToolbar(title: uiState.showDetails?.title ?? "", opacity: showGlass),
-        alignment: .top
-      )
-      .animation(.easeInOut(duration: 0.25), value: showGlass)
-      .coordinateSpace(name: CoordinateSpaces.scrollView)
-      .edgesIgnoringSafeArea(.top)
-    }
+    ParallaxView(
+      imageHeight: DimensionConstants.imageHeight,
+      collapsedImageHeight: DimensionConstants.collapsedImageHeight,
+      header: { proxy in
+        HeaderView(
+          title: uiState.showDetails?.title ?? "",
+          overview: uiState.showDetails?.overview ?? "",
+          backdropImageUrl: uiState.showDetails?.backdropImageUrl ?? "",
+          status: uiState.showDetails?.status ?? "",
+          year: uiState.showDetails?.year ?? "",
+          language: uiState.showDetails?.language ?? "",
+          rating: uiState.showDetails?.rating ?? 0,
+          progress: proxy.getTitleOpacity(
+            geometry: proxy,
+            imageHeight: DimensionConstants.imageHeight,
+            collapsedImageHeight: DimensionConstants.collapsedImageHeight
+          ),
+          headerHeight: proxy.getHeightForHeaderImage(proxy)
+        )
+      },
+      content: {
+        showInfoDetails
+      },
+      onScroll: { offset in
+        let opacity = -offset - 170
+        let normalizedOpacity = opacity / 220
+        showGlass = max(0, min(1, normalizedOpacity))
+      }
+    )
+    .background(Color.background)
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarColor(backgroundColor: .clear)
+    .overlay(
+      GlassToolbar(
+        title: uiState.showDetails?.title ?? "",
+        opacity: showGlass,
+        isLoading: uiState.isUpdating
+      ),
+      alignment: .top
+    )
+    .animation(.easeInOut(duration: 0.25), value: showGlass)
+    .coordinateSpace(name: CoordinateSpaces.scrollView)
+    .edgesIgnoringSafeArea(.top)
   }
 
   @ViewBuilder
-  func showInfoDetails(show: ShowDetails) -> some View {
+  private var showInfoDetails: some View {
     switch onEnum(of: uiState.showInfo) {
     case .loading, .empty:
-      LoadingIndicatorView(animate: true)
+      ProgressView()
+        .progressViewStyle(CircularProgressViewStyle())
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.background)
     case .error:
-      FullScreenView(buttonText: "Retry", action: { presenter.dispatch(action: ReloadShowDetails()) })
-    case let .loaded(state):
-      ShowInfoView(
-        isFollowed: show.isFollowed,
-        openTrailersInYoutube: state.openTrailersInYoutube,
-        genreList: show.genres.map { $0.toSwift() },
-        seasonList: state.seasonsList.map { $0.toSwift() },
-        providerList: state.providers.map { $0.toSwift() },
-        trailerList: state.trailersList.map { $0.toSwift() },
-        castsList: state.castsList.map { $0.toSwift() },
-        recommendedShowList: state.recommendedShowList.map { $0.toSwift() },
-        similarShows: state.similarShows.map { $0.toSwift() },
-        onWatchTrailer: {
-          presenter.dispatch(action: WatchTrailerClicked(id: show.tmdbId))
-        },
-        onAddToLibrary: {
-          presenter.dispatch(action: FollowShowClicked(addToLibrary: show.isFollowed))
-        },
-        onSeasonClicked: { index, season in
-          let params = ShowSeasonDetailsParam(
-            showId: season.tvShowId,
-            seasonId: season.seasonId,
-            seasonNumber: season.seasonNumber,
-            selectedSeasonIndex: Int32(index)
-          )
+       FullScreenView(
+                buttonText: "Retry",
+                action: { presenter.dispatch(action: ReloadShowDetails()) }
+            )
+    case  .loaded(let state):
+      if let show = uiState.showDetails {
+        ShowInfoView(
+          isFollowed: show.isFollowed,
+          openTrailersInYoutube: state.openTrailersInYoutube,
+          genreList: show.genres.map { $0.toSwift() },
+          seasonList: state.seasonsList.map { $0.toSwift() },
+          providerList: state.providers.map { $0.toSwift() },
+          trailerList: state.trailersList.map { $0.toSwift() },
+          castsList: state.castsList.map { $0.toSwift() },
+          recommendedShowList: state.recommendedShowList.map { $0.toSwift() },
+          similarShows: state.similarShows.map { $0.toSwift() },
+          onWatchTrailer: {
+            presenter.dispatch(action: WatchTrailerClicked(id: show.tmdbId))
+          },
+          onAddToLibrary: {
+            presenter.dispatch(action: FollowShowClicked(addToLibrary: show.isFollowed))
+          },
+          onSeasonClicked: { index, season in
+            let params = ShowSeasonDetailsParam(
+              showId: season.tvShowId,
+              seasonId: season.seasonId,
+              seasonNumber: season.seasonNumber,
+              selectedSeasonIndex: Int32(index)
+            )
 
-          presenter.dispatch(action: SeasonClicked(params: params))
-        },
-        onShowClicked: { id in
-          presenter.dispatch(action: DetailShowClicked(id: id))
-        }
-      )
+            presenter.dispatch(action: SeasonClicked(params: params))
+          },
+          onShowClicked: { id in
+            presenter.dispatch(action: DetailShowClicked(id: id))
+          }
+        )
+      }
     }
   }
 
