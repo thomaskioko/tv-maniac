1ca952282c01e334b2f85b6215ed03f46ceda6b9
Thomas Kioko <kiokotomas@gmail.com>
2025-01-27 00:09:18 +0100

    Rename Button and cleanup styling.

--- android/designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/components/Buttons.kt
+++ android/designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/components/Buttons.kt
@@ -4,16 +4,19 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LibraryAddCheck
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -24,19 +27,24 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.thomaskioko.tvmaniac.compose.extensions.iconButtonBackgroundScrim
 import com.thomaskioko.tvmaniac.compose.theme.TvManiacTheme
 
 @Composable
-fun TvManiacTextButton(
+fun FilledTextButton(
   onClick: () -> Unit,
   modifier: Modifier = Modifier,
   enabled: Boolean = true,
@@ -55,66 +63,164 @@ fun TvManiacTextButton(
 }
 
 @Composable
-fun TvManiacOutlinedButton(
+fun FilledVerticalIconButton(
+  text: String,
+  onClick: () -> Unit,
+  imageVector: ImageVector,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  shape: Shape = RectangleShape,
+  style: TextStyle = MaterialTheme.typography.bodyMedium,
+  containerColor: Color = MaterialTheme.colorScheme.secondary,
+) {
+
+  TextButtonContent(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    containerColor = containerColor,
+    shape = shape,
+    content = {
+      Column(
+        modifier = Modifier
+          .sizeIn(minWidth = 120.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        Icon(
+          imageVector = imageVector,
+          contentDescription = null,
+          tint = when {
+            enabled -> MaterialTheme.colorScheme.onSecondary
+            else -> MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+          },
+        )
+
+        Text(
+          modifier = Modifier.padding(top = 2.dp),
+          text = text,
+          style = style,
+          color = when {
+            enabled -> MaterialTheme.colorScheme.onSecondary
+            else -> MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+          },
+        )
+      }
+    },
+  )
+}
+
+@Composable
+fun FilledHorizontalIconButton(
+  text: String,
+  onClick: () -> Unit,
+  imageVector: ImageVector,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  shape: Shape = RectangleShape,
+  style: TextStyle = MaterialTheme.typography.bodyMedium,
+  containerColor: Color = MaterialTheme.colorScheme.secondary,
+) {
+  TextButtonContent(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    containerColor = containerColor,
+    shape = shape,
+    content = {
+      Row(
+        modifier = Modifier
+          .sizeIn(minHeight = 32.dp, minWidth = 140.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        Icon(
+          imageVector = imageVector,
+          contentDescription = null,
+          tint = when {
+            enabled -> MaterialTheme.colorScheme.onSecondary
+            else -> MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+          },
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+          text = text,
+          style = style,
+          color = when {
+            enabled -> MaterialTheme.colorScheme.onSecondary
+            else -> MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+          },
+        )
+      }
+    },
+  )
+}
+
+@Composable
+private fun TextButtonContent(
+  onClick: () -> Unit,
+  enabled: Boolean,
+  containerColor: Color,
+  shape: Shape,
+  content: @Composable () -> Unit,
+  modifier: Modifier = Modifier,
+) {
+  TextButton(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    colors = ButtonDefaults.buttonColors(
+      contentColor = MaterialTheme.colorScheme.onBackground,
+      containerColor = containerColor,
+    ),
+    shape = shape,
+  ) {
+    content()
+  }
+}
+
+@Composable
+fun HorizontalOutlinedButton(
   text: String,
   onClick: () -> Unit,
   modifier: Modifier = Modifier,
   enabled: Boolean = true,
   textPadding: Dp = 0.dp,
+  shape: Shape = MaterialTheme.shapes.small,
   borderColor: Color = MaterialTheme.colorScheme.secondary,
   leadingIcon: @Composable (() -> Unit)? = null,
-) {
-  TvManiacOutlinedButton(
-    onClick = onClick,
-    modifier = modifier,
-    enabled = enabled,
-    borderColor = borderColor,
-    contentPadding =
-      if (leadingIcon != null) {
-        ButtonDefaults.ButtonWithIconContentPadding
-      } else {
-        ButtonDefaults.ContentPadding
-      },
-  ) {
-    TvManiacButtonContent(
-      text = {
-        Text(
-          text = text,
-          style = MaterialTheme.typography.bodyMedium,
-          color =
-            when {
-              enabled -> MaterialTheme.colorScheme.secondary
-              else -> MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-            },
-          modifier = Modifier.padding(textPadding),
-        )
-      },
-      leadingIcon = leadingIcon,
-    )
-  }
-}
-
-@Composable
-fun TvManiacOutlinedButton(
-  onClick: () -> Unit,
-  borderColor: Color,
-  modifier: Modifier = Modifier,
-  enabled: Boolean = true,
-  shape: Shape = MaterialTheme.shapes.small,
-  contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
-  content: @Composable RowScope.() -> Unit,
 ) {
   OutlinedButton(
     onClick = onClick,
     modifier = modifier.padding(2.dp),
     enabled = enabled,
     shape = shape,
-    contentPadding = contentPadding,
-    content = content,
-    colors =
-      ButtonDefaults.outlinedButtonColors(
-        contentColor = MaterialTheme.colorScheme.onSecondary,
-      ),
+    content = {
+      if (leadingIcon != null) {
+        Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) { leadingIcon() }
+      }
+      Box(
+        Modifier.padding(
+          start =
+            when {
+              leadingIcon != null -> ButtonDefaults.IconSpacing
+              else -> 0.dp
+            },
+        ),
+      ) {
+        Text(
+          text = text,
+          style = MaterialTheme.typography.bodyMedium,
+          color = if (enabled) MaterialTheme.colorScheme.secondary
+          else MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+          modifier = Modifier.padding(textPadding),
+        )
+      }
+    },
+    colors = ButtonDefaults.outlinedButtonColors(
+      contentColor = MaterialTheme.colorScheme.onSecondary,
+    ),
     border =
       BorderStroke(
         width = 1.dp,
@@ -128,24 +234,52 @@ fun TvManiacOutlinedButton(
 }
 
 @Composable
-private fun TvManiacButtonContent(
-  text: @Composable () -> Unit,
-  leadingIcon: @Composable (() -> Unit)? = null,
+fun OutlinedVerticalIconButton(
+  text: String,
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  shape: Shape = MaterialTheme.shapes.small,
+  borderColor: Color = MaterialTheme.colorScheme.secondary,
+  leadingIcon: @Composable (() -> Unit) = {},
 ) {
-  if (leadingIcon != null) {
-    Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) { leadingIcon() }
-  }
-  Box(
-    Modifier.padding(
-      start =
+  OutlinedButton(
+    onClick = onClick,
+    modifier = modifier.widthIn(min = 140.dp),
+    enabled = enabled,
+    shape = shape,
+    content = {
+      Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+      ) {
+
+        leadingIcon()
+
+        Text(
+          text = text,
+          style = MaterialTheme.typography.bodyMedium,
+          color =
+            when {
+              enabled -> MaterialTheme.colorScheme.secondary
+              else -> MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+            },
+          modifier = Modifier.padding(top = 2.dp),
+        )
+      }
+    },
+    colors = ButtonDefaults.outlinedButtonColors(
+      contentColor = MaterialTheme.colorScheme.onSecondary,
+    ),
+    border = BorderStroke(
+      width = 1.dp,
+      color =
         when {
-          leadingIcon != null -> ButtonDefaults.IconSpacing
-          else -> 0.dp
+          enabled -> borderColor
+          else -> MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
         },
     ),
-  ) {
-    text()
-  }
+  )
 }
 
 @Composable
@@ -153,10 +287,11 @@ fun ScrimButton(
   onClick: () -> Unit,
   modifier: Modifier = Modifier,
   show: Boolean = false,
+  color: Color = MaterialTheme.colorScheme.surface,
   alpha: Float = 0.4f,
   content: @Composable () -> Unit,
 ) {
-  val isLight = MaterialTheme.colorScheme.surface.luminance() > 0.5
+  val isLight = color.luminance() > 0.5
   TvManiacTheme(darkTheme = if (!show) isLight else !isLight) {
     IconButton(
       onClick = onClick,
@@ -176,7 +311,7 @@ fun RefreshButton(
   Crossfade(isRefreshing, label = "ActionButtonCrossfade") { targetRefreshing ->
     if (targetRefreshing) {
       AutoSizedCircularProgressIndicator(
-        modifier = modifier
+        modifier = modifier,
       )
     } else {
       content()
@@ -186,10 +321,10 @@ fun RefreshButton(
 
 @ThemePreviews
 @Composable
-private fun TvManiacTextButtonPreview() {
+private fun FilledTextButtonPreview() {
   TvManiacTheme {
     Surface {
-      TvManiacTextButton(
+      FilledTextButton(
         onClick = {},
         enabled = false,
         buttonColors =
@@ -198,7 +333,8 @@ private fun TvManiacTextButtonPreview() {
             containerColor = MaterialTheme.colorScheme.secondary,
           ),
         modifier =
-          Modifier.fillMaxWidth()
+          Modifier
+            .fillMaxWidth()
             .padding(2.dp)
             .background(color = MaterialTheme.colorScheme.secondary),
       ) {
@@ -214,10 +350,40 @@ private fun TvManiacTextButtonPreview() {
 
 @ThemePreviews
 @Composable
+private fun FilledIconButtonPreview(@PreviewParameter(ButtonPreviewParamProvider::class) isEnable: Boolean) {
+  TvManiacTheme {
+    Surface {
+      FilledVerticalIconButton(
+        onClick = {},
+        enabled = isEnable,
+        text = "Track",
+        imageVector = Icons.Default.LibraryAddCheck,
+      )
+    }
+  }
+}
+
+@ThemePreviews
+@Composable
+private fun FilledHorizontalIconButtonPreview(@PreviewParameter(ButtonPreviewParamProvider::class) isEnable: Boolean) {
+  TvManiacTheme {
+    Surface {
+      FilledHorizontalIconButton(
+        onClick = {},
+        enabled = isEnable,
+        text = "Add To Library",
+        imageVector = Icons.Default.LibraryAddCheck,
+      )
+    }
+  }
+}
+
+@ThemePreviews
+@Composable
 private fun TvManiacAlphaTextButtonPreview() {
   TvManiacTheme {
     Surface {
-      TvManiacTextButton(
+      FilledTextButton(
         onClick = {},
         enabled = false,
         buttonColors =
@@ -241,7 +407,7 @@ private fun TvManiacAlphaTextButtonPreview() {
 private fun TvManiacOutlinedButtonPreview() {
   TvManiacTheme {
     Surface {
-      TvManiacOutlinedButton(
+      OutlinedVerticalIconButton(
         onClick = {},
         enabled = true,
         leadingIcon = {
@@ -260,4 +426,12 @@ private fun TvManiacOutlinedButtonPreview() {
       )
     }
   }
+}
+
+
+private class ButtonPreviewParamProvider : PreviewParameterProvider<Boolean> {
+  override val values: Sequence<Boolean> = sequenceOf(
+    true,
+    false,
+  )
 }
