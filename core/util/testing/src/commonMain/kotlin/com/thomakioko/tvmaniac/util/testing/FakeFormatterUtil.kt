9c371165814569270b3e9d639da4cb9ed7722927
Thomas Kioko <kiokotomas@gmail.com>
2025-02-01 18:04:51 +0100

    Add formatter tests and ensure the implementation is the same on Android and iOS

--- core/util/testing/src/commonMain/kotlin/com/thomakioko/tvmaniac/util/testing/FakeFormatterUtil.kt
+++ core/util/testing/src/commonMain/kotlin/com/thomakioko/tvmaniac/util/testing/FakeFormatterUtil.kt
@@ -5,7 +5,9 @@ import com.thomaskioko.tvmaniac.util.FormatterUtil
 class FakeFormatterUtil : FormatterUtil {
   override fun formatTmdbPosterPath(imageUrl: String): String = ""
 
-  override fun formatDouble(number: Double?, scale: Int): Double = 0.0
+  override fun formatDouble(number: Double?, scale: Int): Double {
+    return number ?: 0.0
+  }
 
   override fun formatDuration(number: Int): String = ""
 }
