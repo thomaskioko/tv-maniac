81b7039c35268bbeb2e81afee043101dafe34b09
Thomas Kioko <kiokotomas@gmail.com>
2025-02-06 17:55:56 +0100

    Remove unused dependency.

--- android/ui/discover/build.gradle.kts
+++ android/ui/discover/build.gradle.kts
@@ -16,7 +16,6 @@ dependencies {
   implementation(libs.androidx.compose.foundation)
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.material)
-  implementation(libs.androidx.compose.ui.util)
   implementation(libs.androidx.compose.runtime)
   implementation(libs.snapper)
 
