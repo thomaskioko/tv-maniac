5a29655dc1d9bd394472eb4076f0e651cb1c1bba
Thomas Kioko <kiokotomas@gmail.com>
2025-02-07 19:59:55 +0100

    Add extension functions.

--- build-plugins/src/main/kotlin/com/thomaskioko/tvmaniac/extensions/VersionCatalog.kt
+++ build-plugins/src/main/kotlin/com/thomaskioko/tvmaniac/extensions/VersionCatalog.kt
@@ -7,3 +7,11 @@ import org.gradle.kotlin.dsl.getByType
 
 internal val Project.libs: VersionCatalog
   get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun Project.getVersion(name: String): String {
+  return getVersionOrNull(name) ?: throw NoSuchElementException("Could not find version $name")
+}
+
+internal fun Project.getVersionOrNull(name: String): String? {
+  return libs.findVersion(name).orElseGet { null }?.requiredVersion
+}
