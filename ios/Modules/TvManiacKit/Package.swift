d3f51ba8b527a5fff350333d94a4acb10b8c500b
Thomas Kioko <kiokotomas@gmail.com>
2025-02-22 20:04:17 +0100

    Update framework build configuration
    - Change KMP framework from static to dynamic linking
    - Remove unnecessary pre-compile script from TvManiacKit scheme

--- ios/Modules/TvManiacKit/Package.swift
+++ ios/Modules/TvManiacKit/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   products: [
     .library(
       name: "TvManiacKit",
-      targets: ["TvManiacKit", "TvManiac"]
+      targets: ["TvManiacKit"]
     ),
   ],
   dependencies: [
@@ -19,15 +19,10 @@ let package = Package(
       .package(name: "SnapshotTestingLib", path: "../SnapshotTestingLib"),
   ],
   targets: [
-    .binaryTarget(
-      name: "TvManiac",
-      path: "TvManiac.xcframework"
-    ),
     .target(
       name: "TvManiacKit",
       dependencies: [
-        "SwiftUIComponents",
-        "TvManiac",
+        "SwiftUIComponents"
       ]
     ),
     .testTarget(
