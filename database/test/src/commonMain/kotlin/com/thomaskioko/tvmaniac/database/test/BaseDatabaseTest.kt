4d79130c97b93c80fe1e213cbed26a483b5a59b2
Thomas Kioko <kiokotomas@gmail.com>
2025-03-02 15:19:37 +0100

    Move database creation instance out of class to make it reusable.

--- database/test/src/commonMain/kotlin/com/thomaskioko/tvmaniac/database/test/BaseDatabaseTest.kt
+++ database/test/src/commonMain/kotlin/com/thomaskioko/tvmaniac/database/test/BaseDatabaseTest.kt
@@ -31,108 +31,109 @@ expect fun inMemorySqlDriver(): SqlDriver
 
 abstract class BaseDatabaseTest {
   private val sqlDriver: SqlDriver = inMemorySqlDriver()
-  protected open val database: TvManiacDatabase =
-    TvManiacDatabase(
-      driver = sqlDriver,
-      last_requestsAdapter =
-        Last_requests.Adapter(
-          timestampAdapter = InstantColumnAdapter,
-        ),
-      episode_imageAdapter =
-        Episode_image.Adapter(
-          idAdapter = IdAdapter(),
-          tmdb_idAdapter = IdAdapter(),
-        ),
-      episodeAdapter =
-        Episode.Adapter(
-          idAdapter = IdAdapter(),
-          season_idAdapter = IdAdapter(),
-          show_idAdapter = IdAdapter(),
-        ),
-      seasonAdapter =
-        Season.Adapter(
-          idAdapter = IdAdapter(),
-          show_idAdapter = IdAdapter(),
-        ),
-      similar_showsAdapter =
-        Similar_shows.Adapter(
-          idAdapter = IdAdapter(),
-          similar_show_idAdapter = IdAdapter(),
-        ),
-      trailersAdapter =
-        Trailers.Adapter(
-          show_idAdapter = IdAdapter(),
-        ),
-      trending_showsAdapter =
-        Trending_shows.Adapter(
-          idAdapter = IdAdapter(),
-          pageAdapter = IdAdapter(),
-        ),
-      tvshowAdapter =
-        Tvshow.Adapter(
-          idAdapter = IdAdapter(),
-          genre_idsAdapter = intColumnAdapter,
-        ),
-      upcoming_showsAdapter =
-        Upcoming_shows.Adapter(
-          idAdapter = IdAdapter(),
-          pageAdapter = IdAdapter(),
-        ),
-      toprated_showsAdapter =
-        Toprated_shows.Adapter(
-          idAdapter = IdAdapter(),
-          pageAdapter = IdAdapter(),
-        ),
-      popular_showsAdapter =
-        Popular_shows.Adapter(
-          idAdapter = IdAdapter(),
-          pageAdapter = IdAdapter(),
-        ),
-      genresAdapter =
-        Genres.Adapter(
-          idAdapter = IdAdapter(),
-        ),
-      season_imagesAdapter =
-        Season_images.Adapter(
-          season_idAdapter = IdAdapter(),
-        ),
-      season_videosAdapter =
-        Season_videos.Adapter(
-          season_idAdapter = IdAdapter(),
-        ),
-      recommended_showsAdapter =
-        Recommended_shows.Adapter(
-          idAdapter = IdAdapter(),
-          recommended_show_idAdapter = IdAdapter(),
-        ),
-      castsAdapter =
-        Casts.Adapter(
-          idAdapter = IdAdapter(),
-          season_idAdapter = IdAdapter(),
-          tmdb_idAdapter = IdAdapter(),
-        ),
-      watch_providersAdapter =
-        Watch_providers.Adapter(
-          idAdapter = IdAdapter(),
-          tmdb_idAdapter = IdAdapter(),
-        ),
-      featured_showsAdapter =
-        Featured_shows.Adapter(
-          idAdapter = IdAdapter(),
-        ),
-      show_genresAdapter = Show_genres.Adapter(
-        show_idAdapter = IdAdapter(),
-        genre_idAdapter = IdAdapter(),
-      ),
-      show_metadataAdapter = Show_metadata.Adapter(
-        show_idAdapter = IdAdapter()
-      ),
-      watchlistAdapter = Watchlist.Adapter(
-        idAdapter = IdAdapter()
-      )
-    )
+  protected open val database: TvManiacDatabase = createDatabase(sqlDriver)
 
   fun closeDb() {
     sqlDriver.close()
   }
+}
+
+fun createDatabase(sqlDriver: SqlDriver): TvManiacDatabase {
+  return TvManiacDatabase(
+    driver = sqlDriver,
+    last_requestsAdapter =
+      Last_requests.Adapter(
+        timestampAdapter = InstantColumnAdapter,
+      ),
+    episode_imageAdapter =
+      Episode_image.Adapter(
+        idAdapter = IdAdapter(),
+        tmdb_idAdapter = IdAdapter(),
+      ),
+    episodeAdapter =
+      Episode.Adapter(
+        idAdapter = IdAdapter(),
+        season_idAdapter = IdAdapter(),
+        show_idAdapter = IdAdapter(),
+      ),
+    seasonAdapter =
+      Season.Adapter(
+        idAdapter = IdAdapter(),
+        show_idAdapter = IdAdapter(),
+      ),
+    similar_showsAdapter =
+      Similar_shows.Adapter(
+        idAdapter = IdAdapter(),
+        similar_show_idAdapter = IdAdapter(),
+      ),
+    trailersAdapter =
+      Trailers.Adapter(
+        show_idAdapter = IdAdapter(),
+      ),
+    trending_showsAdapter =
+      Trending_shows.Adapter(
+        idAdapter = IdAdapter(),
+        pageAdapter = IdAdapter(),
+      ),
+    tvshowAdapter =
+      Tvshow.Adapter(
+        idAdapter = IdAdapter(),
+        genre_idsAdapter = intColumnAdapter,
+      ),
+    upcoming_showsAdapter =
+      Upcoming_shows.Adapter(
+        idAdapter = IdAdapter(),
+        pageAdapter = IdAdapter(),
+      ),
+    toprated_showsAdapter =
+      Toprated_shows.Adapter(
+        idAdapter = IdAdapter(),
+        pageAdapter = IdAdapter(),
+      ),
+    popular_showsAdapter =
+      Popular_shows.Adapter(
+        idAdapter = IdAdapter(),
+        pageAdapter = IdAdapter(),
+      ),
+    genresAdapter =
+      Genres.Adapter(
+        idAdapter = IdAdapter(),
+      ),
+    season_imagesAdapter =
+      Season_images.Adapter(
+        season_idAdapter = IdAdapter(),
+      ),
+    season_videosAdapter =
+      Season_videos.Adapter(
+        season_idAdapter = IdAdapter(),
+      ),
+    recommended_showsAdapter =
+      Recommended_shows.Adapter(
+        idAdapter = IdAdapter(),
+        recommended_show_idAdapter = IdAdapter(),
+      ),
+    castsAdapter =
+      Casts.Adapter(
+        idAdapter = IdAdapter(),
+      ),
+    watch_providersAdapter =
+      Watch_providers.Adapter(
+        idAdapter = IdAdapter(),
+        tmdb_idAdapter = IdAdapter(),
+      ),
+    featured_showsAdapter =
+      Featured_shows.Adapter(
+        idAdapter = IdAdapter(),
+      ),
+    show_genresAdapter = Show_genres.Adapter(
+      show_idAdapter = IdAdapter(),
+      genre_idAdapter = IdAdapter(),
+    ),
+    show_metadataAdapter = Show_metadata.Adapter(
+      show_idAdapter = IdAdapter(),
+    ),
+    watchlistAdapter = Watchlist.Adapter(
+      idAdapter = IdAdapter(),
+    )
+  )
 }
