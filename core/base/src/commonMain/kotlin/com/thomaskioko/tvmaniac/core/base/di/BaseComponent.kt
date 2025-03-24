4c265d0ceeb77853f159d6a6b130a1184a6e30e0
Thomas Kioko <kiokotomas@gmail.com>
2025-03-24 16:18:15 +0100

    Add separate dispatchers for database operations.

--- core/base/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/base/di/BaseComponent.kt
+++ core/base/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/base/di/BaseComponent.kt
@@ -2,6 +2,7 @@ package com.thomaskioko.tvmaniac.core.base.di
 
 import com.thomaskioko.tvmaniac.core.base.model.AppCoroutineDispatchers
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import me.tatarka.inject.annotations.Provides
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
 import software.amazon.lastmile.kotlin.inject.anvil.ContributesTo
@@ -16,6 +17,8 @@ interface BaseComponent {
     AppCoroutineDispatchers(
       io = Dispatchers.Default,
       computation = Dispatchers.Default,
+      databaseWrite = Dispatchers.IO.limitedParallelism(1),
+      databaseRead = Dispatchers.IO.limitedParallelism(4),
       main = Dispatchers.Main,
     )
 }
