352233d1ad0a65e8b809e06fddaa04d758d3d4dd
Thomas Kioko <kiokotomas@gmail.com>
2025-04-02 20:38:40 +0200

    Create interactor classes.

---
+++ data/popularshows/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/data/popularshows/api/PopularShowsInteractor.kt
@@ -0,0 +1,19 @@
+package com.thomaskioko.tvmaniac.data.popularshows.api
+
+import com.thomaskioko.tvmaniac.core.base.interactor.Interactor
+import com.thomaskioko.tvmaniac.core.base.model.AppCoroutineDispatchers
+import kotlinx.coroutines.withContext
+import me.tatarka.inject.annotations.Inject
+
+@Inject
+class PopularShowsInteractor(
+  private val popularShowsRepository: PopularShowsRepository,
+  private val dispatchers: AppCoroutineDispatchers,
+) : Interactor<Boolean>() {
+
+  override suspend fun doWork(params: Boolean) {
+    withContext(dispatchers.io) {
+      popularShowsRepository.fetchPopularShows(params)
+    }
+  }
+}
