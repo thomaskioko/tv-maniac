89efe2c7dd602b17cb9d03bbd39853f4166f7337
Thomas Kioko <kiokotomas@gmail.com>
2025-04-02 20:09:43 +0200

    Create abstract interactor class.

---
+++ core/base/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/base/interactor/Interactor.kt
@@ -0,0 +1,63 @@
+package com.thomaskioko.tvmaniac.core.base.interactor
+
+import com.thomaskioko.tvmaniac.core.view.InvokeError
+import com.thomaskioko.tvmaniac.core.view.InvokeStarted
+import com.thomaskioko.tvmaniac.core.view.InvokeStatus
+import com.thomaskioko.tvmaniac.core.view.InvokeSuccess
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration.Companion.minutes
+
+abstract class Interactor<in P> {
+    operator fun invoke(
+        params: P,
+        timeoutMs: Long = defaultTimeoutMs,
+    ): Flow<InvokeStatus> = flow {
+        try {
+            withTimeout(timeoutMs) {
+                emit(InvokeStarted)
+                doWork(params)
+                emit(InvokeSuccess)
+            }
+        } catch (t: TimeoutCancellationException) {
+            emit(InvokeError(t))
+        }
+    }.catch { t -> emit(InvokeError(t)) }
+
+    protected abstract suspend fun doWork(params: P)
+
+    companion object {
+        private val defaultTimeoutMs = 5.minutes.inWholeMilliseconds
+    }
+}
+
+
+abstract class SubjectInteractor<P : Any, T> {
+    // Ideally this would be buffer = 0, since we use flatMapLatest below, BUT invoke is not
+    // suspending. This means that we can't suspend while flatMapLatest cancels any
+    // existing flows. The buffer of 1 means that we can use tryEmit() and buffer the value
+    // instead, resulting in mostly the same result.
+    private val paramState = MutableSharedFlow<P>(
+        replay = 1,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST,
+    )
+
+    val flow: Flow<T> = paramState
+        .distinctUntilChanged()
+        .flatMapLatest { createObservable(it) }
+        .distinctUntilChanged()
+
+    operator fun invoke(params: P) {
+        paramState.tryEmit(params)
+    }
+
+    protected abstract fun createObservable(params: P): Flow<T>
+}
