e4fe096b876b2501228b7c191f853e9fee3ef1f6
Thomas Kioko <kiokotomas@gmail.com>
2025-05-05 16:10:31 +0200

    Update Stringresource extension

--- i18n/src/androidMain/kotlin/com/thomaskioko/tvmaniac/i18n/TvManiacStringDesc.kt
+++ i18n/src/androidMain/kotlin/com/thomaskioko/tvmaniac/i18n/TvManiacStringDesc.kt
@@ -1,8 +1,9 @@
 package com.thomaskioko.tvmaniac.i18n
 
-import android.content.Context
+import androidx.compose.runtime.Composable
 import dev.icerock.moko.resources.StringResource
+import dev.icerock.moko.resources.compose.localized
 import dev.icerock.moko.resources.desc.desc
 
-fun StringResource.resolve(context: Context) =
-  desc().toString(context)
+@Composable
+fun StringResource.resolve(): String = desc().localized()
