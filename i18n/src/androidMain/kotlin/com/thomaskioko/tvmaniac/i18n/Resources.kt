4ef5e8f609a8979ee8b02f85588fac359472e45d
Thomas Kioko <kiokotomas@gmail.com>
2025-05-17 19:06:18 +0200

    Delete compose function.

--- i18n/src/androidMain/kotlin/com/thomaskioko/tvmaniac/i18n/Resources.kt
+++ i18n/src/androidMain/kotlin/com/thomaskioko/tvmaniac/i18n/Resources.kt
@@ -1,13 +1,7 @@
 package com.thomaskioko.tvmaniac.i18n
 
 import android.content.Context
-import androidx.compose.runtime.Composable
 import dev.icerock.moko.resources.StringResource
-import dev.icerock.moko.resources.compose.localized
 import dev.icerock.moko.resources.desc.desc
-
-@Composable
-fun StringResource.resolve(): String = desc().localized()
-
 
 fun StringResource.resolve(context: Context) = desc().toString(context)
