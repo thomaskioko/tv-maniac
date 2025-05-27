4429ced3e4b2fa37e4e741f04e8b4a12af613165
Thomas Kioko <kiokotomas@gmail.com>
2025-05-27 16:19:57 +0200

    Refactor GenreStore:
    - Introduce GenrePosterStore to separate poster URL handling.
    - Add coroutine dispatchers to ShowsByGenreIdStore for improved database operations.

--- data/genre/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/genre/GenrePosterStore.kt
+++ data/genre/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/genre/GenrePosterStore.kt
@@ -7,10 +7,10 @@ import com.thomaskioko.tvmaniac.core.store.storeBuilder
 import com.thomaskioko.tvmaniac.core.store.usingDispatchers
 import com.thomaskioko.tvmaniac.db.Genres
 import com.thomaskioko.tvmaniac.db.Id
-import com.thomaskioko.tvmaniac.db.Tvshow
 import com.thomaskioko.tvmaniac.tmdb.api.TmdbShowsNetworkDataSource
 import com.thomaskioko.tvmaniac.tmdb.api.model.TmdbShowResult
 import com.thomaskioko.tvmaniac.util.FormatterUtil
+import kotlinx.coroutines.withContext
 import me.tatarka.inject.annotations.Inject
 import org.mobilenativefoundation.store.store5.SourceOfTruth
 import org.mobilenativefoundation.store.store5.Store
@@ -19,25 +19,32 @@ import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 
 @Inject
 @SingleIn(AppScope::class)
-class ShowsByGenreIdStore(
+class GenrePosterStore(
     private val genreDao: GenreDao,
     private val tmdbRemoteDataSource: TmdbShowsNetworkDataSource,
     private val formatterUtil: FormatterUtil,
     private val scope: AppCoroutineScope,
     private val dispatchers: AppCoroutineDispatchers,
-) : Store<String, List<Tvshow>> by storeBuilder(
-    fetcher = apiFetcher { id: String ->
-        tmdbRemoteDataSource.discoverShows(genres = id)
+) : Store<Long, List<ShowGenresEntity>> by storeBuilder(
+    fetcher = apiFetcher { id ->
+        tmdbRemoteDataSource.discoverShows(
+            genres = id.toString(),
+            watchProviders = "8,15,283,318,337,350,1899",
+        )
     },
-    sourceOfTruth = SourceOfTruth.of(
-        reader = { id: String -> genreDao.observeShowsByGenreId(id) },
-        writer = { id: String, response: TmdbShowResult ->
-            response.results.forEach { genre ->
+    sourceOfTruth = SourceOfTruth.of<Long, TmdbShowResult, List<ShowGenresEntity>>(
+        reader = { genreId: Long -> genreDao.observeGenres() },
+        writer = { genreId, response ->
+            val posterPath = response.results.shuffled().firstOrNull()?.posterPath
+            val posterUrl = posterPath?.let { formatterUtil.formatTmdbPosterPath(it) }
+            val genre = genreDao.getGenre(genreId)
+
+            withContext(dispatchers.databaseWrite) {
                 genreDao.upsert(
                     Genres(
-                        id = Id(id.toLong()),
+                        id = Id(genreId),
                         name = genre.name,
-                        poster_url = genre.posterPath?.let { formatterUtil.formatTmdbPosterPath(it) },
+                        poster_url = posterUrl,
                     ),
                 )
             }
--- data/genre/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/genre/GenreStore.kt
+++ data/genre/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/genre/GenreStore.kt
@@ -8,7 +8,6 @@ import com.thomaskioko.tvmaniac.core.store.usingDispatchers
 import com.thomaskioko.tvmaniac.db.Genres
 import com.thomaskioko.tvmaniac.db.Id
 import com.thomaskioko.tvmaniac.tmdb.api.TmdbShowsNetworkDataSource
-import com.thomaskioko.tvmaniac.util.FormatterUtil
 import kotlinx.coroutines.withContext
 import me.tatarka.inject.annotations.Inject
 import org.mobilenativefoundation.store.store5.Fetcher
@@ -22,7 +21,6 @@ import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 class GenreStore(
     private val genreDao: GenreDao,
     private val tmdbRemoteDataSource: TmdbShowsNetworkDataSource,
-    private val formatterUtil: FormatterUtil,
     private val scope: AppCoroutineScope,
     private val dispatchers: AppCoroutineDispatchers,
 ) : Store<Unit, List<ShowGenresEntity>> by storeBuilder(
@@ -35,33 +33,21 @@ class GenreStore(
                 val results = mutableListOf<ShowGenresEntity>()
 
                 genres.forEach { genre ->
-                    val showsResponse = tmdbRemoteDataSource.discoverShows(
-                        genres = genre.id.toString(),
-                        watchProviders = "8,15,283,318,337,350,1899",
+                    val entity = ShowGenresEntity(
+                        id = genre.id.toLong(),
+                        name = genre.name,
+                        posterUrl = null, // Poster URL will be updated by GenrePosterStore
                     )
+                    results.add(entity)
 
-                    when (showsResponse) {
-                        is ApiResponse.Success -> {
-                            val entity = ShowGenresEntity(
-                                id = genre.id.toLong(),
-                                name = genre.name,
-                                posterUrl = showsResponse.body.results.shuffled().firstOrNull()?.posterPath,
-                            )
-                            results.add(entity)
-
-                            withContext(dispatchers.databaseWrite) {
-                                genreDao.upsert(
-                                    Genres(
-                                        id = Id(entity.id),
-                                        name = entity.name,
-                                        poster_url = entity.posterUrl?.let { formatterUtil.formatTmdbPosterPath(it) },
-                                    ),
-                                )
-                            }
-                        }
-                        else -> {
-                            // Skip failed genres
-                        }
+                    withContext(dispatchers.databaseWrite) {
+                        genreDao.upsert(
+                            Genres(
+                                id = Id(entity.id),
+                                name = entity.name,
+                                poster_url = null, // Poster URL will be updated by GenrePosterStore
+                            ),
+                        )
                     }
                 }
                 results
