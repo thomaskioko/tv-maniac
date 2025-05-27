7c94fe3c52dbdad49a5e3ee1fcec3b96da42a891
Thomas Kioko <kiokotomas@gmail.com>
2025-05-27 03:17:15 +0200

    Fix formatting and apply spotless plugin

--- tooling/tvmaniac-gradle-plugins/src/main/kotlin/com/thomaskioko/tvmaniac/gradle/plugin/BasePlugin.kt
+++ tooling/tvmaniac-gradle-plugins/src/main/kotlin/com/thomaskioko/tvmaniac/gradle/plugin/BasePlugin.kt
@@ -14,74 +14,74 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
  * Base class for all plugins in the project. This class provides common configurations that are shared across multiple plugins.
  */
 public abstract class BasePlugin : Plugin<Project> {
-  override fun apply(target: Project) {
-    target.plugins.apply("com.autonomousapps.dependency-analysis")
-      target.plugins.apply("com.thomaskioko.tvmaniac.gradle.spotless")
+    override fun apply(target: Project) {
+        target.plugins.apply("com.autonomousapps.dependency-analysis")
+        target.plugins.apply("com.thomaskioko.tvmaniac.gradle.spotless")
 
-    target.extensions.create("tvmaniac", BaseExtension::class.java)
+        target.extensions.create("tvmaniac", BaseExtension::class.java)
 
-    target.makeJarsReproducible()
-    target.configureJava()
-    target.configureKotlin()
-  }
-
-  private fun Project.makeJarsReproducible() {
-    tasks.withType(Jar::class.java).configureEach {
-      it.isReproducibleFileOrder = true
-      it.isPreserveFileTimestamps = false
+        target.makeJarsReproducible()
+        target.configureJava()
+        target.configureKotlin()
     }
-  }
 
-  internal fun Project.configureJava() {
-    java {
-      toolchain {
-        it.languageVersion.set(javaToolchainVersion)
-        it.vendor.set(JvmVendorSpec.AZUL)
-      }
-    }
-  }
-
-  private fun Project.configureKotlin() {
-    kotlin {
-      jvmToolchain { toolchain ->
-        toolchain.languageVersion.set(javaToolchainVersion)
-        toolchain.vendor.set(JvmVendorSpec.AZUL)
-      }
-
-      val isAndroid = this is KotlinAndroidProjectExtension
-
-      compilerOptions {
-        val version = getVersionOrNull("kotlin-language")
-          ?.let(KotlinVersion.Companion::fromVersion) ?: KotlinVersion.Companion.DEFAULT
-        languageVersion.set(version)
-
-        // In this mode, some deprecations and bug-fixes for unstable code take effect immediately.
-        progressiveMode.set(version >= KotlinVersion.Companion.DEFAULT)
-
-
-        // Enable new Kotlin 2.1
-        freeCompilerArgs.addAll(
-          "-Xnon-local-break-continue",
-          "-Xmulti-dollar-interpolation",
-          "-Xexpect-actual-classes",
-          "-Xwhen-guards",
-        )
-
-        if (this is KotlinJvmCompilerOptions) {
-          jvmTarget.set(project.jvmTarget)
-
-          freeCompilerArgs.addAll(
-            "-Xjvm-default=all",
-            "-Xassertions=jvm",
-            // https://kotlinlang.org/docs/whatsnew2020.html#data-class-copy-function-to-have-the-same-visibility-as-constructor
-            "-Xconsistent-data-class-copy-visibility",
-          )
-
-          if (!isAndroid) {
-            freeCompilerArgs.add("-Xjdk-release=${project.javaTarget}")
-          }
+    private fun Project.makeJarsReproducible() {
+        tasks.withType(Jar::class.java).configureEach {
+            it.isReproducibleFileOrder = true
+            it.isPreserveFileTimestamps = false
         }
-      }
     }
-  }
+
+    internal fun Project.configureJava() {
+        java {
+            toolchain {
+                it.languageVersion.set(javaToolchainVersion)
+                it.vendor.set(JvmVendorSpec.AZUL)
+            }
+        }
+    }
+
+    private fun Project.configureKotlin() {
+        kotlin {
+            jvmToolchain { toolchain ->
+                toolchain.languageVersion.set(javaToolchainVersion)
+                toolchain.vendor.set(JvmVendorSpec.AZUL)
+            }
+
+            val isAndroid = this is KotlinAndroidProjectExtension
+
+            compilerOptions {
+                val version = getVersionOrNull("kotlin-language")
+                    ?.let(KotlinVersion.Companion::fromVersion) ?: KotlinVersion.Companion.DEFAULT
+                languageVersion.set(version)
+
+                // In this mode, some deprecations and bug-fixes for unstable code take effect immediately.
+                progressiveMode.set(version >= KotlinVersion.Companion.DEFAULT)
+
+
+                // Enable new Kotlin 2.1
+                freeCompilerArgs.addAll(
+                    "-Xnon-local-break-continue",
+                    "-Xmulti-dollar-interpolation",
+                    "-Xexpect-actual-classes",
+                    "-Xwhen-guards",
+                )
+
+                if (this is KotlinJvmCompilerOptions) {
+                    jvmTarget.set(project.jvmTarget)
+
+                    freeCompilerArgs.addAll(
+                        "-Xjvm-default=all",
+                        "-Xassertions=jvm",
+                        // https://kotlinlang.org/docs/whatsnew2020.html#data-class-copy-function-to-have-the-same-visibility-as-constructor
+                        "-Xconsistent-data-class-copy-visibility",
+                    )
+
+                    if (!isAndroid) {
+                        freeCompilerArgs.add("-Xjdk-release=${project.javaTarget}")
+                    }
+                }
+            }
+        }
+    }
 }
