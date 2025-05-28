d092927e86b56af5f1282f55cde512726d764921
Thomas Kioko <kiokotomas@gmail.com>
2025-05-28 15:54:41 +0200

    Use AutoMirrored icon for ArrowBack in TopBar component

--- android-designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/components/TopBar.kt
+++ android-designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/components/TopBar.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -358,7 +358,7 @@ private fun TopBarScrimPreview() {
             },
             navigationIcon = {
                 Image(
-                    imageVector = Icons.Filled.ArrowBack,
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = null,
                     colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onSurface),
                     modifier = Modifier
