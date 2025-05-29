320d89b485622594674338ab16502d6c7c97dfd1
Thomas Kioko <kiokotomas@gmail.com>
2025-05-29 14:31:13 +0200

    Add configuration loading framework and environment-specific settings
    - Introduced `ConfigLoader` to load app configurations from `Info.plist` and JSON files.
    - Added OAuth configuration via `trakt_oauth_config.json`.
    - Added `config-dev.json` and `config-prod.json` for environment-specific settings.
    - Updated Xcode project to include `.xcconfig` files for development and production environments.

--- ios/ios/Configuration/Config.swift
+++ ios/ios/Configuration/Config.swift
@@ -11,59 +11,56 @@ import Foundation
 
 struct Config: Decodable {
 
-    let authorizeUrl: String
-    let accessTokenUrl: String
-    let callbackUrl: String
-    let clientId: String
-    let clientSecret: String
-    let responseType: String
-    
-    init() {
-        self.authorizeUrl = ""
-        self.accessTokenUrl = ""
-        self.callbackUrl = ""
-        self.clientId = ""
-        self.clientSecret = ""
-        self.responseType = ""
-    }
-    
-    func getAuthorizeUrl() throws -> URL {
-        
-        guard let url = URL(string: self.authorizeUrl) else {
+    var BUNDLE_ID: String
+    var APP_NAME: String
+    var APPICON_NAME: String
 
+    var authorizeUrl: String = ""
+    var accessTokenUrl: String = ""
+    var callbackUrl: String = ""
+    var clientId: String = ""
+    var clientSecret: String = ""
+    var responseType: String = ""
+
+    init() {
+        self.BUNDLE_ID = ""
+        self.APP_NAME = ""
+        self.APPICON_NAME = ""
+    }
+
+    init(bundleID: String, appName: String, appIconName: String) {
+        self.BUNDLE_ID = bundleID
+        self.APP_NAME = appName
+        self.APPICON_NAME = appIconName
+    }
+
+    func getAuthorizeUrl() throws -> URL {
+        guard let url = URL(string: self.authorizeUrl) else {
             throw ApplicationError(
                 title: "Invalid Configuration Error",
                 description: "The authorizeUrl could not be parsed"
             )
         }
-        
         return url
     }
 
     func getAccessTokenUrl() throws -> URL {
-        
         guard let url = URL(string: self.accessTokenUrl) else {
-
             throw ApplicationError(
                 title: "Invalid Configuration Error",
                 description: "The accessTokenUrl could not be parsed"
             )
         }
-        
         return url
     }
-    
-    func getCallbackURL() throws -> URL {
-        
-        guard let url = URL(string: self.callbackUrl) else {
 
+    func getCallbackURL() throws -> URL {
+        guard let url = URL(string: self.callbackUrl) else {
             throw ApplicationError(
                 title: "Invalid Configuration Error",
                 description: "The callbackURL could not be parsed"
             )
         }
-        
         return url
     }
-
 }
