b64a64eb32fc37b82b652a68b956606bfa8d12ee
Thomas Kioko <kiokotomas@gmail.com>
2025-06-07 19:02:05 +0200

    Add language persistence and observation to Datastore
    - Introduced `saveLanguage` and `observeLanguage` functions to DatastoreRepository.
    - Updated FakeDatastoreRepository with language flow implementation.

--- data/datastore/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/datastore/api/DatastoreRepository.kt
+++ data/datastore/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/datastore/api/DatastoreRepository.kt
@@ -3,16 +3,50 @@ package com.thomaskioko.tvmaniac.datastore.api
 import kotlinx.coroutines.flow.Flow
 
 interface DatastoreRepository {
+    /**
+     * Saves the application's theme preference.
+     *
+     * @param appTheme The [AppTheme] to be saved, which can be one of LIGHT_THEME, DARK_THEME, or SYSTEM_THEME.
+     */
     fun saveTheme(appTheme: AppTheme)
 
+    /**
+     * Observes changes to the application's theme preference stored in the datastore.
+     *
+     * @return A Flow emitting the current [AppTheme], which can be LIGHT_THEME, DARK_THEME, or SYSTEM_THEME.
+     */
     fun observeTheme(): Flow<AppTheme>
 
+    /**
+     * Clears the stored authentication state from the datastore.
+     *
+     * This method removes any saved authentication details, such as access tokens,
+     * refresh tokens, and the authorized state. It is typically used during logout
+     * or when resetting authentication data.
+     */
     fun clearAuthState()
 
+    /**
+     * Observes changes to the authentication state stored in the datastore.
+     *
+     * @return A Flow emitting the current [AuthState], which includes information such as
+     * access token, refresh token, and authorization status.
+     */
     fun observeAuthState(): Flow<AuthState>
 
+    /**
+     * Saves the provided authentication state to the datastore.
+     *
+     * @param authState The [AuthState] containing authentication details such as access token,
+     * refresh token, and authorization status to be saved.
+     */
     suspend fun saveAuthState(authState: AuthState)
 
+    /**
+     * Retrieves the current authentication state from the datastore.
+     *
+     * @return The current [AuthState], or null if no authentication state is stored.
+     */
     suspend fun getAuthState(): AuthState?
 
     /**
