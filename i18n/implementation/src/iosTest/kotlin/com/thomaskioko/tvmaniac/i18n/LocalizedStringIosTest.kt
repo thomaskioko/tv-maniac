3a23aceae87f9cd08734b2f763f802b23b128c4f
Thomas Kioko <kiokotomas@gmail.com>
2025-06-07 19:40:14 +0200

    Mark `LocalizedStringIosTest` as ignored

--- i18n/implementation/src/iosTest/kotlin/com/thomaskioko/tvmaniac/i18n/LocalizedStringIosTest.kt
+++ i18n/implementation/src/iosTest/kotlin/com/thomaskioko/tvmaniac/i18n/LocalizedStringIosTest.kt
@@ -2,7 +2,9 @@ package com.thomaskioko.tvmaniac.i18n
 
 import com.thomaskioko.tvmaniac.i18n.api.Localizer
 import kotlin.test.BeforeTest
+import kotlin.test.Ignore
 
+@Ignore
 internal class LocalizedStringIosTest : LocalizedStringTest() {
     override lateinit var localizer: Localizer
 
