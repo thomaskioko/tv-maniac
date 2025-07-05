4b1065bd6f26dc6d50e48f7029965ccad5292279
Thomas Kioko <kiokotomas@gmail.com>
2025-07-05 21:21:18 +0200

    Refactor WatchlistRepository to return Flow instead of Either

--- data/watchlist/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/shows/api/WatchlistRepository.kt
+++ data/watchlist/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/shows/api/WatchlistRepository.kt
@@ -1,16 +1,14 @@
 package com.thomaskioko.tvmaniac.shows.api
 
-import com.thomaskioko.tvmaniac.core.networkutil.model.Either
-import com.thomaskioko.tvmaniac.core.networkutil.model.Failure
 import com.thomaskioko.tvmaniac.db.SearchWatchlist
 import com.thomaskioko.tvmaniac.db.Watchlists
 import kotlinx.coroutines.flow.Flow
 
 interface WatchlistRepository {
 
-    fun observeWatchlist(): Flow<Either<Failure, List<Watchlists>>>
+    fun observeWatchlist(): Flow<List<Watchlists>>
 
-    fun searchWatchlistByQuery(query: String): Flow<Either<Failure, List<SearchWatchlist>>>
+    fun searchWatchlistByQuery(query: String): Flow<List<SearchWatchlist>>
 
     fun observeUnSyncedItems(): Flow<Unit>
 
