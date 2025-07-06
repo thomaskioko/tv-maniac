0ba808ae638b1cef3187c4d29318e6764895eb7c
Thomas Kioko <kiokotomas@gmail.com>
2025-07-06 21:29:12 +0200

    Create `domain:watchlist` module

---
+++ domain/watchlist/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    alias(libs.plugins.tvmaniac.kmp)
+}
+
+tvmaniac {
+    optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(projects.data.shows.api)
+
+                implementation(projects.core.base)
+                implementation(projects.core.util)
+                implementation(projects.core.networkUtil)
+                implementation(projects.data.watchlist.api)
+
+                implementation(libs.coroutines.core)
+                implementation(libs.kotlinInject.runtime)
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.bundles.unittest)
+                implementation(projects.data.watchlist.testing)
+            }
+        }
+    }
+}
