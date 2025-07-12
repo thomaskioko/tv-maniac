47c6e9c483886b5b8f40298b2c4f4234bc62ade6
Thomas Kioko <kiokotomas@gmail.com>
2025-07-12 15:40:04 +0200

    Specify return type

--- i18n/generator/src/androidMain/kotlin/com/thomaskioko/tvmaniac/i18n/Resources.kt
+++ i18n/generator/src/androidMain/kotlin/com/thomaskioko/tvmaniac/i18n/Resources.kt
@@ -4,4 +4,4 @@ import android.content.Context
 import dev.icerock.moko.resources.StringResource
 import dev.icerock.moko.resources.desc.desc
 
-public fun StringResource.resolve(context: Context) = desc().toString(context)
+public fun StringResource.resolve(context: Context): String = desc().toString(context)
