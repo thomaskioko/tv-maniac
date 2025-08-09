8de3ec5801fc3f5dd38fcb262ae758f6d0cb1058
Thomas Kioko <kiokotomas@gmail.com>
2025-08-09 15:45:56 +0200

    Remove unused dependencies.

--- core/imageloading/implementation/build.gradle.kts
+++ core/imageloading/implementation/build.gradle.kts
@@ -13,8 +13,6 @@ dependencies {
     api(projects.core.imageloading.api)
     api(projects.data.datastore.api)
     implementation(projects.core.base)
-    implementation(projects.core.networkUtil)
-    implementation(projects.core.util)
 
     implementation(libs.coil.compose)
     implementation(libs.coil.base)
