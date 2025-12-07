ed7fbc117bf5b42b4b3c6962fc163d3de2d0ca4e
Thomas Kioko <kiokotomas@gmail.com>
2025-12-07 16:56:41 +0100

    Configure WorkManager
    Disable automatic WorkManager initialization via AndroidX Startup and configure it manually.
    
    This involves:
    - Removing the `WorkManagerInitializer` from the `androidx.startup` provider in the `AndroidManifest.xml`.
    - Implementing `Configuration.Provider` in the `Application` class.
    - Providing a custom `WorkManager` configuration that uses a dependency-injected `WorkerFactory`.

--- app/src/main/kotlin/com/thomaskioko/tvmaniac/app/TvManicApplication.kt
+++ app/src/main/kotlin/com/thomaskioko/tvmaniac/app/TvManicApplication.kt
@@ -1,21 +1,31 @@
 package com.thomaskioko.tvmaniac.app
 
 import android.app.Application
+import androidx.work.Configuration
+import androidx.work.WorkerFactory
 import com.thomaskioko.tvmaniac.core.base.extensions.unsafeLazy
 import com.thomaskioko.tvmaniac.inject.ApplicationComponent
 import com.thomaskioko.tvmaniac.inject.create
 
-class TvManicApplication : Application() {
+class TvManicApplication : Application(), Configuration.Provider {
     private val component: ApplicationComponent by unsafeLazy {
         ApplicationComponent::class.create(
             this,
         )
     }
 
+    private lateinit var workerFactory: WorkerFactory
+
     override fun onCreate() {
         super.onCreate()
+        workerFactory = component.workerFactory
         component.initializers.initialize()
     }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 
     fun getApplicationComponent() = component
 }
