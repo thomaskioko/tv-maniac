f160569531812a5911dc7e358b9d16c791611e58
Thomas Kioko <kiokotomas@gmail.com>
2025-12-07 19:47:56 +0100

    Refactor FakeTraktRefreshTokenAction to use RefreshTokenResult

--- data/traktauth/testing/src/commonMain/kotlin/com/thomaskioko/tvmaniac/traktauth/testing/FakeTraktRefreshTokenAction.kt
+++ data/traktauth/testing/src/commonMain/kotlin/com/thomaskioko/tvmaniac/traktauth/testing/FakeTraktRefreshTokenAction.kt
@@ -1,14 +1,15 @@
 package com.thomaskioko.tvmaniac.traktauth.testing
 
 import com.thomaskioko.tvmaniac.traktauth.api.AuthState
+import com.thomaskioko.tvmaniac.traktauth.api.RefreshTokenResult
 import com.thomaskioko.tvmaniac.traktauth.api.TraktRefreshTokenAction
 
 class FakeTraktRefreshTokenAction : TraktRefreshTokenAction {
-    private var result: AuthState? = null
+    private var result: RefreshTokenResult = RefreshTokenResult.TokenExpired
 
-    fun setResult(authState: AuthState?) {
+    fun setResult(authState: RefreshTokenResult) {
         result = authState
     }
 
-    override suspend fun invoke(currentState: AuthState): AuthState? = result
+    override suspend fun invoke(currentState: AuthState): RefreshTokenResult = result
 }
