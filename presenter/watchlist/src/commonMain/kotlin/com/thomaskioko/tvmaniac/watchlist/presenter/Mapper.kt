f17681291f586b5cfbe098411c1b710ea446072f
Thomas Kioko <kiokotomas@gmail.com>
2025-12-13 01:51:14 +0100

    Refactor WatchlistItem model to include episodesWatched and totalEpisodesTracked

--- presenter/watchlist/src/commonMain/kotlin/com/thomaskioko/tvmaniac/watchlist/presenter/Mapper.kt
+++ presenter/watchlist/src/commonMain/kotlin/com/thomaskioko/tvmaniac/watchlist/presenter/Mapper.kt
@@ -9,19 +9,19 @@ import kotlinx.collections.immutable.toPersistentList
 
 fun List<Watchlists>.entityToWatchlistShowList(): PersistentList<WatchlistItem> {
     return this.map {
-        val watchedCount = it.watched_count
-        val totalCount = it.total_episode_count
-        val progress = if (totalCount > 0) watchedCount.toFloat() / totalCount else 0f
+        val watched = it.watched_count
+        val total = it.total_episode_count
+        val progress = if (total > 0) watched.toFloat() / total else 0f
         WatchlistItem(
             tmdbId = it.id.id,
             title = it.name,
             posterImageUrl = it.poster_path,
-            year = it.first_air_date,
             status = it.status,
+            year = it.first_air_date,
             seasonCount = it.season_count ?: 0,
             episodeCount = it.episode_count ?: 0,
-            watchedCount = watchedCount,
-            totalEpisodeCount = totalCount,
+            episodesWatched = watched,
+            totalEpisodesTracked = total,
             watchProgress = progress,
         )
     }
@@ -30,19 +30,19 @@ fun List<Watchlists>.entityToWatchlistShowList(): PersistentList<WatchlistItem> 
 
 fun List<SearchWatchlist>.entityToWatchlistShowList(): ImmutableList<WatchlistItem> {
     return this.map {
-        val watchedCount = it.watched_count
-        val totalCount = it.total_episode_count
-        val progress = if (totalCount > 0) watchedCount.toFloat() / totalCount else 0f
+        val watched = it.watched_count
+        val total = it.total_episode_count
+        val progress = if (total > 0) watched.toFloat() / total else 0f
         WatchlistItem(
             tmdbId = it.id.id,
             title = it.name,
             posterImageUrl = it.poster_path,
-            year = it.first_air_date,
             status = it.status,
+            year = it.first_air_date,
             seasonCount = it.season_count ?: 0,
             episodeCount = it.episode_count ?: 0,
-            watchedCount = watchedCount,
-            totalEpisodeCount = totalCount,
+            episodesWatched = watched,
+            totalEpisodesTracked = total,
             watchProgress = progress,
         )
     }
