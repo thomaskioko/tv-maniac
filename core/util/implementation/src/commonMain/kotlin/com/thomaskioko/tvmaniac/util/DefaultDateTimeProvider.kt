aa0e6bd031a8a6634fe029cee606086642a45a9a
Thomas Kioko <kiokotomas@gmail.com>
2025-12-31 16:19:16 +0100

    Delete old implementation and add new date and time utility functions to DefaultDateTimeProvider

--- core/util/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/DefaultDateTimeProvider.kt
+++ core/util/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/DefaultDateTimeProvider.kt
@@ -1,9 +1,12 @@
 package com.thomaskioko.tvmaniac.util
 
+import co.touchlab.kermit.Logger
 import com.thomaskioko.tvmaniac.util.api.DateTimeProvider
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.datetime.todayIn
 import me.tatarka.inject.annotations.Inject
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
@@ -32,5 +35,27 @@ public class DefaultDateTimeProvider : DateTimeProvider {
         }
         val days = today().daysUntil(airDate)
         return if (days > 0) days else null
+    }
+
+    override fun startOfDay(timeZone: TimeZone): Instant = now()
+        .toLocalDateTime(timeZone)
+        .date
+        .atStartOfDayIn(timeZone)
+
+    override fun formatDate(epochMillis: Long, timeZone: TimeZone): String {
+        val instant = Instant.fromEpochMilliseconds(epochMillis)
+        val localDate = instant.toLocalDateTime(timeZone).date
+        return localDate.toString()
+    }
+
+    override fun getYear(dateString: String): String {
+        if (dateString.isEmpty()) return "--"
+        return try {
+            val localDate = LocalDate.parse(dateString)
+            localDate.year.toString()
+        } catch (exception: Exception) {
+            Logger.e("getYear:: $dateString ${exception.message}", exception)
+            "TBA"
+        }
     }
 }
