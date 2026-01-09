ae2636ebc6fb8eca279e0e06ac43584498ee715d
Thomas Kioko <kiokotomas@gmail.com>
2026-01-09 16:11:56 +0100

    Use system default timezone

--- core/util/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/api/DateTimeProvider.kt
+++ core/util/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/util/api/DateTimeProvider.kt
@@ -9,8 +9,8 @@ public interface DateTimeProvider {
     public fun nowMillis(): Long = now().toEpochMilliseconds()
     public fun today(timeZone: TimeZone = TimeZone.currentSystemDefault()): LocalDate
     public fun calculateDaysUntilAir(airDateStr: String?, timeZone: TimeZone = TimeZone.currentSystemDefault()): Int?
-    public fun startOfDay(timeZone: TimeZone = TimeZone.UTC): Instant
-    public fun formatDate(epochMillis: Long, timeZone: TimeZone = TimeZone.UTC): String
+    public fun startOfDay(timeZone: TimeZone = TimeZone.currentSystemDefault()): Instant
+    public fun formatDate(epochMillis: Long, timeZone: TimeZone = TimeZone.currentSystemDefault()): String
     public fun formatDateTime(epochMillis: Long, timeZone: TimeZone = TimeZone.currentSystemDefault()): String
     public fun getYear(dateString: String): String
 }
