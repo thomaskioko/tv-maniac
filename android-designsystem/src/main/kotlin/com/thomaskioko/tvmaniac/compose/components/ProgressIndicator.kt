b3d06f809ac3923b59aa465ed70d4279f5d45f94
Thomas Kioko <kiokotomas@gmail.com>
2026-01-17 19:13:22 +0100

    Update ProgressIndicator to fill max size and center content

--- android-designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/components/ProgressIndicator.kt
+++ android-designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/components/ProgressIndicator.kt
@@ -1,10 +1,12 @@
 package com.thomaskioko.tvmaniac.compose.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.thomaskioko.tvmaniac.compose.theme.TvManiacTheme
@@ -15,7 +17,8 @@ public fun LoadingIndicator(
     color: Color = MaterialTheme.colorScheme.secondary,
 ) {
     Box(
-        modifier = modifier,
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
     ) {
         CircularProgressIndicator(
             color = color,
