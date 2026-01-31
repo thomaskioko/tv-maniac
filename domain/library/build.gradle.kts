d5e9252fc1fb713b6edf51eac13c4910623e23cc
Thomas Kioko <kiokotomas@gmail.com>
2026-01-31 14:14:47 +0100

    Add library synchronization interactor and observe functionality

---
+++ domain/library/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.app.kmp)
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(projects.core.base)
+                implementation(projects.core.logger.api)
+                implementation(projects.data.episode.api)
+                implementation(projects.data.followedshows.api)
+                implementation(projects.data.library.api)
+                implementation(projects.data.seasons.api)
+                implementation(projects.data.showdetails.api)
+                implementation(projects.data.watchlist.api)
+
+                implementation(libs.coroutines.core)
+                implementation(libs.kotlinInject.runtime)
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.bundles.unittest)
+                implementation(projects.data.library.testing)
+            }
+        }
+    }
+}
