ccf5a05129229c0c9679aa901cbf76636857e16e
Thomas Kioko <kiokotomas@gmail.com>
2026-02-02 21:13:43 +0100

    Remove show content sync

--- domain/watchlist/src/commonMain/kotlin/com/thomaskioko/tvmaniac/domain/watchlist/WatchlistSyncInteractor.kt
+++ domain/watchlist/src/commonMain/kotlin/com/thomaskioko/tvmaniac/domain/watchlist/WatchlistSyncInteractor.kt
@@ -1,26 +1,17 @@
 package com.thomaskioko.tvmaniac.domain.watchlist
 
-import com.thomaskioko.tvmaniac.core.base.extensions.DEFAULT_SYNC_CONCURRENCY
-import com.thomaskioko.tvmaniac.core.base.extensions.parallelForEach
 import com.thomaskioko.tvmaniac.core.base.interactor.Interactor
 import com.thomaskioko.tvmaniac.core.base.model.AppCoroutineDispatchers
-import com.thomaskioko.tvmaniac.core.logger.Logger
 import com.thomaskioko.tvmaniac.data.library.LibraryRepository
-import com.thomaskioko.tvmaniac.domain.showdetails.ShowContentSyncInteractor
-import com.thomaskioko.tvmaniac.followedshows.api.FollowedShowsRepository
 import com.thomaskioko.tvmaniac.syncactivity.api.TraktActivityRepository
-import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
 import me.tatarka.inject.annotations.Inject
 
 @Inject
 public class WatchlistSyncInteractor(
-    private val followedShowsRepository: FollowedShowsRepository,
     private val libraryRepository: LibraryRepository,
     private val traktActivityRepository: TraktActivityRepository,
-    private val showContentSyncInteractor: ShowContentSyncInteractor,
     private val dispatchers: AppCoroutineDispatchers,
-    private val logger: Logger,
 ) : Interactor<WatchlistSyncInteractor.Param>() {
 
     override suspend fun doWork(params: Param) {
@@ -28,23 +19,6 @@ public class WatchlistSyncInteractor(
             traktActivityRepository.fetchLatestActivities(params.forceRefresh)
 
             libraryRepository.syncLibrary(params.forceRefresh)
-
-            val followedShows = followedShowsRepository.getFollowedShows()
-            logger.debug(TAG, "Syncing content for ${followedShows.size} followed shows.")
-
-            followedShows.parallelForEach(concurrency = DEFAULT_SYNC_CONCURRENCY) { show ->
-                ensureActive()
-
-                showContentSyncInteractor.executeSync(
-                    ShowContentSyncInteractor.Param(
-                        traktId = show.traktId,
-                        forceRefresh = params.forceRefresh,
-                        isUserInitiated = false,
-                    ),
-                )
-            }
-
-            logger.debug(TAG, "Followed shows content sync complete")
         }
     }
 
