27a9b3864237e45a9126c4273bc8622925f1f20c
Thomas Kioko <kiokotomas@gmail.com>
2026-02-08 23:58:32 +0100

    Refactor background task implementation for Android and iOS
    - Introduce a platform-specific implementation for background tasks on Android and iOS.
    - On Android, `WorkManager` is now used to schedule and dispatch background workers.
    - On iOS, the implementation now uses `BGTaskScheduler` for managing background tasks.
    - Refactor dependencies in the `core-tasks-implementation` module, moving some to the `iosMain` source set.

---
+++ core/tasks/implementation/src/iosMain/kotlin/com/thomaskioko/tvmaniac/core/tasks/implementation/BGTaskSchedulerWrapper.kt
@@ -0,0 +1,56 @@
+package com.thomaskioko.tvmaniac.core.tasks.implementation
+
+import com.thomaskioko.tvmaniac.core.logger.Logger
+import com.thomaskioko.tvmaniac.core.tasks.implementation.TaskResult.Failure
+import com.thomaskioko.tvmaniac.core.tasks.implementation.TaskResult.Success
+import kotlinx.cinterop.ExperimentalForeignApi
+import me.tatarka.inject.annotations.Inject
+import platform.BackgroundTasks.BGAppRefreshTaskRequest
+import platform.BackgroundTasks.BGTask
+import platform.BackgroundTasks.BGTaskScheduler
+import platform.Foundation.NSDate
+import platform.Foundation.dateWithTimeIntervalSinceNow
+import software.amazon.lastmile.kotlin.inject.anvil.AppScope
+import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
+
+@Inject
+@SingleIn(AppScope::class)
+public class BGTaskSchedulerWrapper(
+    private val logger: Logger,
+) {
+    private val scheduler = BGTaskScheduler.sharedScheduler
+
+    public fun register(taskId: String, handler: (BGTask?) -> Unit) {
+        scheduler.registerForTaskWithIdentifier(
+            identifier = taskId,
+            usingQueue = null,
+            launchHandler = handler,
+        )
+        logger.debug(TAG, "Registered background task [$taskId]")
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    public fun submit(taskId: String, interval: Double): TaskResult {
+        val request = BGAppRefreshTaskRequest(identifier = taskId).apply {
+            earliestBeginDate = NSDate.dateWithTimeIntervalSinceNow(interval)
+        }
+
+        return try {
+            scheduler.submitTaskRequest(taskRequest = request, error = null)
+            logger.debug(TAG, "Scheduled task [$taskId] for ${request.earliestBeginDate}")
+            Success
+        } catch (t: Throwable) {
+            logger.error(TAG, "Error scheduling task [$taskId]: ${t.message}")
+            Failure(t.message ?: "Unknown error")
+        }
+    }
+
+    public fun cancel(taskId: String) {
+        scheduler.cancelTaskRequestWithIdentifier(taskId)
+        logger.debug(TAG, "Cancelled task [$taskId]")
+    }
+
+    private companion object {
+        private const val TAG = "BGTaskSchedulerWrapper"
+    }
+}
