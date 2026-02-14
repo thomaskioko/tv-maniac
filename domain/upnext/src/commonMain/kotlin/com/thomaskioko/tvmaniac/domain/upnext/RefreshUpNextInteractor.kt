ea2fdc63ffb8137b0f52baec1805aedf023ef9a3
Thomas Kioko <kiokotomas@gmail.com>
2026-02-14 16:21:48 +0100

    Add datastore integration for last up next sync timestamp

--- domain/upnext/src/commonMain/kotlin/com/thomaskioko/tvmaniac/domain/upnext/RefreshUpNextInteractor.kt
+++ domain/upnext/src/commonMain/kotlin/com/thomaskioko/tvmaniac/domain/upnext/RefreshUpNextInteractor.kt
@@ -1,15 +1,20 @@
 package com.thomaskioko.tvmaniac.domain.upnext
 
 import com.thomaskioko.tvmaniac.core.base.interactor.Interactor
+import com.thomaskioko.tvmaniac.datastore.api.DatastoreRepository
 import com.thomaskioko.tvmaniac.upnext.api.UpNextRepository
+import com.thomaskioko.tvmaniac.util.api.DateTimeProvider
 import me.tatarka.inject.annotations.Inject
 
 @Inject
 public class RefreshUpNextInteractor(
     private val upNextRepository: UpNextRepository,
+    private val datastoreRepository: DatastoreRepository,
+    private val dateTimeProvider: DateTimeProvider,
 ) : Interactor<Boolean>() {
 
     override suspend fun doWork(params: Boolean) {
         upNextRepository.fetchUpNextEpisodes(forceRefresh = params)
+        datastoreRepository.setLastUpNextSyncTimestamp(dateTimeProvider.nowMillis())
     }
 }
