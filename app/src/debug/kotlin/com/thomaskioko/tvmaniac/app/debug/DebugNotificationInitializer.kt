1313b1f8a580b6de2735ced1903609db16ae48c3
Thomas Kioko <kiokotomas@gmail.com>
2026-04-11 04:30:32 +0200

    refactor: rename DebugNotificationInitializer and update coroutine scope annotations

--- app/src/debug/kotlin/com/thomaskioko/tvmaniac/app/debug/DebugNotificationInitializer.kt
+++ app/src/debug/kotlin/com/thomaskioko/tvmaniac/app/debug/DebugNotificationInitializer.kt
@@ -1,24 +1,21 @@
-package com.thomaskioko.tvmaniac.inject
+package com.thomaskioko.tvmaniac.app.debug
 
-import com.thomaskioko.tvmaniac.core.base.AppInitializer
-import com.thomaskioko.tvmaniac.core.base.model.AppCoroutineScope
+import com.thomaskioko.tvmaniac.core.base.IoCoroutineScope
 import com.thomaskioko.tvmaniac.core.notifications.implementation.DebugNotificationManager
 import com.thomaskioko.tvmaniac.datastore.api.DatastoreRepository
+import dev.zacsweers.metro.Inject
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import me.tatarka.inject.annotations.Inject
-import software.amazon.lastmile.kotlin.inject.anvil.AppScope
-import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
 
 @Inject
-@ContributesBinding(AppScope::class, multibinding = true)
 public class DebugNotificationInitializer(
-    private val coroutineScope: AppCoroutineScope,
+    @IoCoroutineScope private val coroutineScope: CoroutineScope,
     private val datastoreRepository: Lazy<DatastoreRepository>,
     private val debugNotificationManager: Lazy<DebugNotificationManager>,
-) : AppInitializer {
+) {
 
-    override fun init() {
-        coroutineScope.io.launch {
+    public fun init() {
+        coroutineScope.launch {
             datastoreRepository.value.observeEpisodeNotificationsEnabled()
                 .collect { enabled ->
                     when {
