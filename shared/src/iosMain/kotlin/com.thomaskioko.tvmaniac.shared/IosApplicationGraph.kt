c4a97f1dfec09f64047a8d28fc327c414a7c2bc4
Thomas Kioko <kiokotomas@gmail.com>
2026-04-11 04:37:51 +0200

    refactor: migrate DI components to Metro Dependency Graphs

---
+++ shared/src/iosMain/kotlin/com.thomaskioko.tvmaniac.shared/IosApplicationGraph.kt
@@ -0,0 +1,24 @@
+package com.thomaskioko.tvmaniac.shared
+
+import com.thomaskioko.tvmaniac.core.base.AppInitializers
+import com.thomaskioko.tvmaniac.core.logger.Logger
+import com.thomaskioko.tvmaniac.core.tasks.api.BackgroundTaskScheduler
+import com.thomaskioko.tvmaniac.traktauth.api.TraktAuthManager
+import com.thomaskioko.tvmaniac.traktauth.api.TraktAuthRepository
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.createGraph
+
+@DependencyGraph(AppScope::class)
+public interface IosApplicationGraph {
+    public val initializers: AppInitializers
+    public val viewPresenterGraphFactory: IosViewPresenterGraph.Factory
+    public val traktAuthRepository: TraktAuthRepository
+    public val traktAuthManager: TraktAuthManager
+    public val backgroundTaskScheduler: BackgroundTaskScheduler
+    public val logger: Logger
+
+    public companion object {
+        public fun create(): IosApplicationGraph = createGraph<IosApplicationGraph>()
+    }
+}
