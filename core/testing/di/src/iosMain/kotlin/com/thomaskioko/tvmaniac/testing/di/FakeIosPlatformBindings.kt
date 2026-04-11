d4423e367017d09205179dfe2d37088f37f15e85
Thomas Kioko <kiokotomas@gmail.com>
2026-04-11 16:31:10 +0200

    remove blank line

--- core/testing/di/src/iosMain/kotlin/com/thomaskioko/tvmaniac/testing/di/FakeIosPlatformBindings.kt
+++ core/testing/di/src/iosMain/kotlin/com/thomaskioko/tvmaniac/testing/di/FakeIosPlatformBindings.kt
@@ -14,7 +14,6 @@ import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.BindingContainer
 import dev.zacsweers.metro.ContributesTo
 
-
 @BindingContainer
 @ContributesTo(
     AppScope::class,
