ea591303c401e9aff8a60ba255c944508b74a68b
Thomas Kioko <kiokotomas@gmail.com>
2026-04-11 04:32:59 +0200

    refactor: add multibinding definitions for initializers and async initializers

---
+++ core/base/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/base/di/InitializerMultibindings.kt
@@ -0,0 +1,18 @@
+package com.thomaskioko.tvmaniac.core.base.di
+
+import com.thomaskioko.tvmaniac.core.base.AsyncInitializers
+import com.thomaskioko.tvmaniac.core.base.Initializers
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Multibinds
+
+@ContributesTo(AppScope::class)
+public interface InitializerMultibindings {
+    @Initializers
+    @Multibinds
+    public fun initializers(): Set<() -> Unit>
+
+    @AsyncInitializers
+    @Multibinds
+    public fun asyncInitializers(): Set<() -> Unit>
+}
