a1a3bc1d69896ca060ca9b3e7eda97f7e9f1d036
Thomas Kioko <kiokotomas@gmail.com>
2026-04-26 18:21:44 +0200

    use domain object

--- app/src/debug/kotlin/com/thomaskioko/tvmaniac/app/debug/DebugNotificationInitializer.kt
+++ app/src/debug/kotlin/com/thomaskioko/tvmaniac/app/debug/DebugNotificationInitializer.kt
@@ -2,21 +2,24 @@ package com.thomaskioko.tvmaniac.app.debug
 
 import com.thomaskioko.tvmaniac.core.base.IoCoroutineScope
 import com.thomaskioko.tvmaniac.core.notifications.implementation.DebugNotificationManager
-import com.thomaskioko.tvmaniac.datastore.api.DatastoreRepository
+import com.thomaskioko.tvmaniac.domain.settings.ObserveSettingsPreferencesInteractor
 import dev.zacsweers.metro.Inject
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 @Inject
 public class DebugNotificationInitializer(
     @IoCoroutineScope private val coroutineScope: CoroutineScope,
-    private val datastoreRepository: Lazy<DatastoreRepository>,
+    private val observeSettingsPreferencesInteractor: Lazy<ObserveSettingsPreferencesInteractor>,
     private val debugNotificationManager: Lazy<DebugNotificationManager>,
 ) {
 
     public fun init() {
         coroutineScope.launch {
-            datastoreRepository.value.observeEpisodeNotificationsEnabled()
+            observeSettingsPreferencesInteractor.value(Unit)
+            observeSettingsPreferencesInteractor.value.flow
+                .map { it.episodeNotificationsEnabled }
                 .collect { enabled ->
                     when {
                         enabled -> debugNotificationManager.value.show()
