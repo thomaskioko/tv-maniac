0c2be6d0277b2b83b3170b564fcd6fb83362e3e7
Thomas Kioko <kiokotomas@gmail.com>
2026-04-28 17:43:10 +0200

    refactor: Update notification rationale scope from ActivityScope to AppScope

--- features/root/presenter/src/commonMain/kotlin/com/thomaskioko/tvmaniac/presenter/root/di/DefaultNotificationRationale.kt
+++ features/root/presenter/src/commonMain/kotlin/com/thomaskioko/tvmaniac/presenter/root/di/DefaultNotificationRationale.kt
@@ -1,16 +1,16 @@
 package com.thomaskioko.tvmaniac.presenter.root.di
 
 import com.thomaskioko.root.nav.NotificationRationale
-import com.thomaskioko.tvmaniac.core.base.ActivityScope
 import com.thomaskioko.tvmaniac.datastore.api.DatastoreRepository
+import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.SingleIn
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.take
 
-@SingleIn(ActivityScope::class)
-@ContributesBinding(ActivityScope::class)
+@SingleIn(AppScope::class)
+@ContributesBinding(AppScope::class)
 public class DefaultNotificationRationale(
     private val datastoreRepository: DatastoreRepository,
 ) : NotificationRationale {
