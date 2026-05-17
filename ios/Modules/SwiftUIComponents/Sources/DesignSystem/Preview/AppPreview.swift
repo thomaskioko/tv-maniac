b95d068a695670fa563ae30a7f3e3e0982c02d37
Thomas Kioko <kiokotomas@gmail.com>
2026-05-18 00:58:45 +0200

    refactor: Support automatic theme detection in SwiftUI previews

--- ios/Modules/SwiftUIComponents/Sources/DesignSystem/Preview/AppPreview.swift
+++ ios/Modules/SwiftUIComponents/Sources/DesignSystem/Preview/AppPreview.swift
@@ -1,8 +1,24 @@
 import SwiftUI
 
-public extension View {
-    func appPreview(_ theme: TvManiacTheme = LightTheme()) -> some View {
-        appTheme(theme)
+public struct AppPreviewModifier: ViewModifier {
+    @Environment(\.colorScheme) private var colorScheme
+
+    private let explicitTheme: TvManiacTheme?
+
+    public init(explicitTheme: TvManiacTheme?) {
+        self.explicitTheme = explicitTheme
+    }
+
+    public func body(content: Content) -> some View {
+        let theme: TvManiacTheme = explicitTheme ?? (colorScheme == .dark ? DarkTheme() : LightTheme())
+        content
+            .appTheme(theme)
             .background(theme.colors.background)
+    }
+}
+
+public extension View {
+    func appPreview(_ theme: TvManiacTheme? = nil) -> some View {
+        modifier(AppPreviewModifier(explicitTheme: theme))
     }
 }
