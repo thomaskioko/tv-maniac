6876e6e43771a37696eca5c4a376d1231b20a8a7
Thomas Kioko <kiokotomas@gmail.com>
2026-05-18 19:29:27 +0200

    feat: Implement TraktSyncRemoteDataSource with watched shows support

---
+++ api/trakt/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/trakt/api/model/TraktWatchedShowResponse.kt
@@ -0,0 +1,14 @@
+package com.thomaskioko.tvmaniac.trakt.api.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class TraktWatchedShowResponse(
+    @SerialName("plays") val plays: Long,
+    @SerialName("last_watched_at") val lastWatchedAt: String,
+    @SerialName("last_updated_at") val lastUpdatedAt: String,
+    @SerialName("reset_at") val resetAt: String? = null,
+    @SerialName("show") val show: TraktShowResponse,
+    @SerialName("seasons") val seasons: List<TraktWatchedSeasonResponse>? = null,
+)
