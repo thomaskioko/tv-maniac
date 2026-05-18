edc86ecd256a8e18e7800924d1cb336cfc14d551
Thomas Kioko <kiokotomas@gmail.com>
2026-05-18 23:22:51 +0200

    refactor: Clean up build configuration and remove unused repository bindings

--- data/watched-shows/testing/build.gradle.kts
+++ data/watched-shows/testing/build.gradle.kts
@@ -2,16 +2,11 @@ plugins {
     alias(libs.plugins.app.kmp)
 }
 
-scaffold {
-    useMetro()
-}
-
 kotlin {
     sourceSets {
         commonMain {
             dependencies {
                 api(projects.data.watchedShows.api)
-                api(projects.data.watchedShows.implementation)
                 api(libs.coroutines.core)
             }
         }
--- data/watched-shows/testing/src/commonMain/kotlin/com/thomaskioko/tvmaniac/watchedshows/testing/FakeWatchedShowsRepository.kt
+++ data/watched-shows/testing/src/commonMain/kotlin/com/thomaskioko/tvmaniac/watchedshows/testing/FakeWatchedShowsRepository.kt
@@ -1,13 +1,7 @@
 package com.thomaskioko.tvmaniac.watchedshows.testing
 
 import com.thomaskioko.tvmaniac.watchedshows.api.WatchedShowsRepository
-import com.thomaskioko.tvmaniac.watchedshows.implementation.DefaultWatchedShowsRepository
-import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.ContributesBinding
-import dev.zacsweers.metro.SingleIn
 
-@SingleIn(AppScope::class)
-@ContributesBinding(AppScope::class, replaces = [DefaultWatchedShowsRepository::class])
 public class FakeWatchedShowsRepository : WatchedShowsRepository {
 
     private val syncInvocations = mutableListOf<Boolean>()
