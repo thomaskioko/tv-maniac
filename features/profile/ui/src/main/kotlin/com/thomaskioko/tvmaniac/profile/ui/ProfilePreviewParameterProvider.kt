f966acf54ed121b7a6d9885f97b028399dc8b074
Thomas Kioko <kiokotomas@gmail.com>
2026-05-29 20:10:52 +0200

    Update ProfilePreviewParameterProvider with mock data for new profile sections

--- features/profile/ui/src/main/kotlin/com/thomaskioko/tvmaniac/profile/ui/ProfilePreviewParameterProvider.kt
+++ features/profile/ui/src/main/kotlin/com/thomaskioko/tvmaniac/profile/ui/ProfilePreviewParameterProvider.kt
@@ -2,8 +2,19 @@ package com.thomaskioko.tvmaniac.profile.ui
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.thomaskioko.tvmaniac.profile.presenter.model.ProfileInfo
+import com.thomaskioko.tvmaniac.profile.presenter.model.ProfileListItem
+import com.thomaskioko.tvmaniac.profile.presenter.model.ProfileRecentItem
+import com.thomaskioko.tvmaniac.profile.presenter.model.ProfileShowItem
 import com.thomaskioko.tvmaniac.profile.presenter.model.ProfileState
 import com.thomaskioko.tvmaniac.profile.presenter.model.ProfileStats
+import com.thomaskioko.tvmaniac.profile.presenter.model.SectionState
+import kotlinx.collections.immutable.persistentListOf
+
+private val sampleShows = persistentListOf(
+    ProfileShowItem(traktId = 1, tmdbId = 1396, title = "Breaking Bad", posterUrl = null),
+    ProfileShowItem(traktId = 2, tmdbId = 1399, title = "Game of Thrones", posterUrl = null),
+    ProfileShowItem(traktId = 3, tmdbId = 66732, title = "Stranger Things", posterUrl = null),
+)
 
 internal val unauthenticatedState = ProfileState(
     isLoading = false,
@@ -32,6 +43,32 @@ internal val authenticatedState = ProfileState(
     ),
     errorMessage = null,
     authenticated = true,
+    userLists = SectionState.Content(
+        persistentListOf(
+            ProfileListItem(
+                id = 1,
+                name = "Watchlist",
+                itemCount = 24,
+                posterUrls = persistentListOf("/poster1.jpg", "/poster2.jpg", "/poster3.jpg", "/poster4.jpg"),
+            ),
+            ProfileListItem(
+                id = 2,
+                name = "Favorites",
+                itemCount = 12,
+                posterUrls = persistentListOf("/poster5.jpg", "/poster6.jpg"),
+            ),
+        ),
+    ),
+    inProgress = SectionState.Content(sampleShows),
+    recentlyWatched = SectionState.Content(
+        persistentListOf(
+            ProfileRecentItem(traktId = 1, tmdbId = 1396, title = "Breaking Bad", posterUrl = null, episodeLabel = "S5E14"),
+            ProfileRecentItem(traktId = 2, tmdbId = 1399, title = "Game of Thrones", posterUrl = null, episodeLabel = "S8E3"),
+        ),
+    ),
+    library = SectionState.Content(sampleShows),
+    watchlist = SectionState.Content(sampleShows),
+    favorites = SectionState.Content(sampleShows),
 )
 
 internal class ProfilePreviewParameterProvider : PreviewParameterProvider<ProfileState> {
