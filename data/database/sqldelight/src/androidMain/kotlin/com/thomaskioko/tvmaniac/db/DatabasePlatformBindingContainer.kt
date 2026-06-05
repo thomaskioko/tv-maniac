e8713566397968194fb3c0f6b5f4ca94d952beda
Thomas Kioko <kiokotomas@gmail.com>
2026-06-05 18:40:32 +0200

    refactor: Move SqlDriver provision to platform-specific containers
    * Move the `provideSqlDriver` function from the common `DatabaseBindingContainer` to platform-specific `DatabasePlatformBindingContainer` implementations
    * Create new `DatabasePlatformBindingContainer` objects in `androidMain` and `iosMain` to handle platform-dependent `SqlDriver` creation via `MigrationDriverFactory`
    * Maintain `@SingleIn(AppScope::class)` scoping for `SqlDriver` across both Android and iOS platforms

--- data/database/sqldelight/src/androidMain/kotlin/com/thomaskioko/tvmaniac/db/DatabasePlatformBindingContainer.kt
+++ data/database/sqldelight/src/androidMain/kotlin/com/thomaskioko/tvmaniac/db/DatabasePlatformBindingContainer.kt
@@ -1,16 +1,17 @@
 package com.thomaskioko.tvmaniac.db
 
+import app.cash.sqldelight.db.SqlDriver
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.BindingContainer
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Provides
+import dev.zacsweers.metro.SingleIn
 
 @BindingContainer
 @ContributesTo(AppScope::class)
-public object DatabaseBindingContainer {
+public object DatabasePlatformBindingContainer {
 
     @Provides
-    public fun provideTvManiacDatabase(
-        factory: DatabaseFactory,
-    ): TvManiacDatabase = factory.createDatabase()
+    @SingleIn(AppScope::class)
+    public fun provideSqlDriver(factory: MigrationDriverFactory): SqlDriver = factory.create()
 }
