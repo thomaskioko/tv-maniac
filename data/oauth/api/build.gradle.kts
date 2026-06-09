c0d4166b82341730e70222d1a3967c6d18a06516
Thomas Kioko <kiokotomas@gmail.com>
2026-06-09 17:42:38 +0200

    Add missing dependency.

--- data/oauth/api/build.gradle.kts
+++ data/oauth/api/build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
 
 kotlin {
     sourceSets {
+        commonMain {
+            dependencies {
+                api(projects.data.accountManager.api)
+                api(libs.coroutines.core)
+            }
+        }
+
         androidMain {
             dependencies {
                 runtimeOnly(libs.coroutines.android)
