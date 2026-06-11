1d342a68dd2973e100c407a07c7a77f4af4ac3b3
Thomas Kioko <kiokotomas@gmail.com>
2026-06-11 21:55:32 +0200

    feat: Define user data models and remote data source interface
    * feat: Add `RemoteUserProfile` and `RemoteUserStats` data models to the user API module
    * feat: Define `UserRemoteDataSource` interface with methods for fetching user profile and statistics
    * chore: Update `data/user/api` build configuration with `networkUtil` and `accountManager` dependencies

---
+++ data/user/api/src/commonMain/kotlin/com/thomaskioko/tvmaniac/data/user/api/model/RemoteUserStats.kt
@@ -0,0 +1,7 @@
+package com.thomaskioko.tvmaniac.data.user.api.model
+
+public data class RemoteUserStats(
+    val showsWatched: Long,
+    val episodesWatched: Long,
+    val minutesWatched: Long,
+)
