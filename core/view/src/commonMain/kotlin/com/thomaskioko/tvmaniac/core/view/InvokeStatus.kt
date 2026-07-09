e7370fb90e0beccf7bfa16b79c7428426535e413
Thomas Kioko <kiokotomas@gmail.com>
2026-07-09 15:26:38 +0200

    Improve error logging

--- core/view/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/view/InvokeStatus.kt
+++ core/view/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/view/InvokeStatus.kt
@@ -3,4 +3,4 @@ package com.thomaskioko.tvmaniac.core.view
 public sealed class InvokeStatus
 public object InvokeStarted : InvokeStatus()
 public object InvokeSuccess : InvokeStatus()
-public data class InvokeError(val throwable: Throwable) : InvokeStatus()
+public data class InvokeError(val throwable: Throwable, val source: String? = null) : InvokeStatus()
