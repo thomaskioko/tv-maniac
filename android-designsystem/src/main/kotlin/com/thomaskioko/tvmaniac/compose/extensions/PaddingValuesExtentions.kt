4d08ee9547c68c44d7ae84a203d14ca192a78538
Thomas Kioko <kiokotomas@gmail.com>
2026-07-24 22:24:56 +0200

    Simplify reinvented Kotlin helpers

--- android-designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/extensions/PaddingValuesExtentions.kt
+++ android-designsystem/src/main/kotlin/com/thomaskioko/tvmaniac/compose/extensions/PaddingValuesExtentions.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.LayoutDirection
 import com.thomaskioko.tvmaniac.compose.theme.TvManiacSpacing
 
@@ -15,16 +13,9 @@ public fun PaddingValues.copy(
     copyTop: Boolean = true,
     copyEnd: Boolean = true,
     copyBottom: Boolean = true,
-): PaddingValues {
-    return remember(this) {
-        derivedStateOf {
-            PaddingValues(
-                start = if (copyStart) calculateStartPadding(LayoutDirection.Ltr) else TvManiacSpacing.none,
-                top = if (copyTop) calculateTopPadding() else TvManiacSpacing.none,
-                end = if (copyEnd) calculateEndPadding(LayoutDirection.Ltr) else TvManiacSpacing.none,
-                bottom = if (copyBottom) calculateBottomPadding() else TvManiacSpacing.none,
-            )
-        }
-    }
-        .value
-}
+): PaddingValues = PaddingValues(
+    start = if (copyStart) calculateStartPadding(LayoutDirection.Ltr) else TvManiacSpacing.none,
+    top = if (copyTop) calculateTopPadding() else TvManiacSpacing.none,
+    end = if (copyEnd) calculateEndPadding(LayoutDirection.Ltr) else TvManiacSpacing.none,
+    bottom = if (copyBottom) calculateBottomPadding() else TvManiacSpacing.none,
+)
--- core/base/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/base/extensions/Lazy.kt
+++
@@ -1,5 +0,0 @@
-package com.thomaskioko.tvmaniac.core.base.extensions
-
-@Suppress("NOTHING_TO_INLINE")
-public inline fun <T> unsafeLazy(noinline initializer: () -> T): Lazy<T> =
-    lazy(LazyThreadSafetyMode.NONE, initializer)
