3ab50a83323a9fc3f88283ddcaf7f2cfbb3da9bf
Thomas Kioko <kiokotomas@gmail.com>
2026-07-25 12:13:14 +0200

    Remove the unused library tab test tag

--- core/test-tags/src/commonMain/kotlin/com/thomaskioko/tvmaniac/testtags/home/HomeTestTags.kt
+++ core/test-tags/src/commonMain/kotlin/com/thomaskioko/tvmaniac/testtags/home/HomeTestTags.kt
@@ -4,7 +4,6 @@ public object HomeTestTags {
     public const val NAVIGATION_BAR: String = "home_navigation_bar"
     public const val DISCOVER_TAB: String = "home_tab_discover"
     public const val PROGRESS_TAB: String = "home_tab_progress"
-    public const val LIBRARY_TAB: String = "home_tab_library"
     public const val MY_SHOWS_TAB: String = "home_tab_my_shows"
     public const val PROFILE_TAB: String = "home_tab_profile"
 }
