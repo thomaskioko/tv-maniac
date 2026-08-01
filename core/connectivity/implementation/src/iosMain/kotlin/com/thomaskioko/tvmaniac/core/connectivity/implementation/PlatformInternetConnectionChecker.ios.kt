6ed1ab93ecabf995cf93501f3a622d099aedf954
Thomas Kioko <kiokotomas@gmail.com>
2026-08-01 18:08:05 +0200

    Expose connectivity changes as a flow

--- core/connectivity/implementation/src/iosMain/kotlin/com/thomaskioko/tvmaniac/core/connectivity/implementation/PlatformInternetConnectionChecker.ios.kt
+++ core/connectivity/implementation/src/iosMain/kotlin/com/thomaskioko/tvmaniac/core/connectivity/implementation/PlatformInternetConnectionChecker.ios.kt
@@ -4,6 +4,8 @@ import com.thomaskioko.tvmaniac.core.connectivity.api.InternetConnectionChecker
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.SingleIn
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import platform.Network.nw_path_get_status
 import platform.Network.nw_path_monitor_create
 import platform.Network.nw_path_monitor_set_queue
@@ -11,7 +13,6 @@ import platform.Network.nw_path_monitor_set_update_handler
 import platform.Network.nw_path_monitor_start
 import platform.Network.nw_path_status_satisfied
 import platform.darwin.dispatch_queue_create
-import kotlin.concurrent.Volatile
 
 @SingleIn(AppScope::class)
 @ContributesBinding(AppScope::class)
@@ -23,16 +24,17 @@ public actual class PlatformInternetConnectionChecker : InternetConnectionChecke
     )
     private val monitor = nw_path_monitor_create()
 
-    @Volatile
-    private var connected: Boolean = true
+    private val connectionState = MutableStateFlow(true)
 
     init {
         nw_path_monitor_set_queue(monitor, monitorQueue)
         nw_path_monitor_set_update_handler(monitor) { path ->
-            connected = path != null && nw_path_get_status(path) == nw_path_status_satisfied
+            connectionState.value = path != null && nw_path_get_status(path) == nw_path_status_satisfied
         }
         nw_path_monitor_start(monitor)
     }
 
-    public actual override fun isConnected(): Boolean = connected
+    public actual override fun isConnected(): Boolean = connectionState.value
+
+    public actual override fun observeConnection(): Flow<Boolean> = connectionState
 }
