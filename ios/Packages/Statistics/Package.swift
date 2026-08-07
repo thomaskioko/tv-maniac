1812e52b2b12c605c0cea26be423ad23234da8d0
Thomas Kioko <kiokotomas@gmail.com>
2026-08-07 18:07:47 +0200

    Drop the unused Models dependency from the statistics target

--- ios/Packages/Statistics/Package.swift
+++ ios/Packages/Statistics/Package.swift
@@ -28,7 +28,6 @@ let package = Package(
             dependencies: [
                 "DesignSystem",
                 "Components",
-                "Models",
                 "TvManiacKit",
                 .product(name: "TvManiac", package: "TvManiacFramework"),
             ]
