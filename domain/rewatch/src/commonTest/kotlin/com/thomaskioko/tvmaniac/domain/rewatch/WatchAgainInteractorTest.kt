a48f27944be3e9b03cc7507a5566f11601d33f1a
Thomas Kioko <kiokotomas@gmail.com>
2026-08-13 20:10:57 +0200

    Let the watch again interactor own the clock

--- domain/rewatch/src/commonTest/kotlin/com/thomaskioko/tvmaniac/domain/rewatch/WatchAgainInteractorTest.kt
+++ domain/rewatch/src/commonTest/kotlin/com/thomaskioko/tvmaniac/domain/rewatch/WatchAgainInteractorTest.kt
@@ -4,6 +4,7 @@ import app.cash.turbine.test
 import com.thomaskioko.tvmaniac.core.view.InvokeStarted
 import com.thomaskioko.tvmaniac.core.view.InvokeSuccess
 import com.thomaskioko.tvmaniac.data.rewatch.testing.FakeRewatchRepository
+import com.thomaskioko.tvmaniac.util.testing.FakeDateTimeProvider
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
@@ -11,9 +12,11 @@ import kotlin.test.Test
 internal class WatchAgainInteractorTest {
 
     private val rewatchRepository = FakeRewatchRepository()
+    private val dateTimeProvider = FakeDateTimeProvider()
 
     private val interactor = WatchAgainInteractor(
         rewatchRepository = rewatchRepository,
+        dateTimeProvider = dateTimeProvider,
     )
 
     @Test
@@ -59,13 +62,15 @@ internal class WatchAgainInteractorTest {
         rewatchRepository.lastAddEpisodeSessionId shouldBe firstSessionId
     }
 
-    private fun episodeWatch(watchedAt: Long): WatchAgainInteractor.Param = WatchAgainInteractor.Param(
-        showId = SHOW_ID,
-        episodeId = EPISODE_ID,
-        seasonNumber = 1L,
-        episodeNumber = 3L,
-        watchedAt = watchedAt,
-    )
+    private fun episodeWatch(watchedAt: Long): WatchAgainInteractor.Param {
+        dateTimeProvider.setCurrentTimeMillis(watchedAt)
+        return WatchAgainInteractor.Param(
+            showId = SHOW_ID,
+            episodeId = EPISODE_ID,
+            seasonNumber = 1L,
+            episodeNumber = 3L,
+        )
+    }
 
     private companion object {
         private const val SHOW_ID = 84958L
