a3351354c4b69b7b2e85a464c1e464421659dd52
Thomas Kioko <kiokotomas@gmail.com>
2026-08-22 20:54:57 +0200

    Delete the flaky test annotation and its rule
    Retry now happens once at the build level for the whole app flow package, so a list of individual tests marked as expected to fail is no longer needed.

--- app/src/sharedTest/kotlin/com/thomaskioko/tvmaniac/app/test/compose/flows/discover/DiscoverToShowDetailsFollowFlowTest.kt
+++ app/src/sharedTest/kotlin/com/thomaskioko/tvmaniac/app/test/compose/flows/discover/DiscoverToShowDetailsFollowFlowTest.kt
@@ -3,7 +3,6 @@ package com.thomaskioko.tvmaniac.app.test.compose.flows.discover
 import com.thomaskioko.tvmaniac.app.test.BaseAppFlowTest
 import com.thomaskioko.tvmaniac.testtags.home.HomeTestTags
 import com.thomaskioko.tvmaniac.testtags.showdetails.ShowDetailsTestTags
-import com.thomaskioko.tvmaniac.util.testing.FlakyTests
 import org.junit.Test
 
 internal class DiscoverToShowDetailsFollowFlowTest : BaseAppFlowTest() {
@@ -22,7 +21,6 @@ internal class DiscoverToShowDetailsFollowFlowTest : BaseAppFlowTest() {
     }
 
     @Test
-    @FlakyTests(count = 2)
     fun givenShowDetails_whenBackIsPressed_thenRestoresDiscover() = runAppFlowTest {
         scenarios.discover.stubBrowseGraph()
 
