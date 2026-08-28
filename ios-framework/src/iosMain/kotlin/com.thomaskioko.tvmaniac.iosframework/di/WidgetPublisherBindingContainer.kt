90ad4c97f54d12ba942ba95a0160e0375acf65c2
Thomas Kioko <kiokotomas@gmail.com>
2026-08-28 20:35:07 +0200

    Download posters into the shared container on ios

--- ios-framework/src/iosMain/kotlin/com.thomaskioko.tvmaniac.iosframework/di/WidgetPublisherBindingContainer.kt
+++ ios-framework/src/iosMain/kotlin/com.thomaskioko.tvmaniac.iosframework/di/WidgetPublisherBindingContainer.kt
@@ -1,9 +1,12 @@
 package com.thomaskioko.tvmaniac.iosframework.di
 
 import com.thomaskioko.tvmaniac.core.files.api.JsonFileManager
+import com.thomaskioko.tvmaniac.core.logger.Logger
+import com.thomaskioko.tvmaniac.domain.widget.PosterDownloader
 import com.thomaskioko.tvmaniac.domain.widget.SnapshotWidgetPublisher
 import com.thomaskioko.tvmaniac.domain.widget.WidgetManager
 import com.thomaskioko.tvmaniac.domain.widget.WidgetPublisher
+import com.thomaskioko.tvmaniac.iosframework.widget.IosPosterDownloader
 import com.thomaskioko.tvmaniac.util.api.DateTimeProvider
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.BindingContainer
@@ -17,15 +20,21 @@ import dev.zacsweers.metro.SingleIn
 public object WidgetPublisherBindingContainer {
 
     @Provides
+    @SingleIn(AppScope::class)
+    public fun providePosterDownloader(logger: Logger): PosterDownloader = IosPosterDownloader(logger)
+
+    @Provides
     @IntoSet
     @SingleIn(AppScope::class)
     public fun provideWidgetPublisher(
         widgetManager: WidgetManager,
         jsonFileManager: JsonFileManager,
+        posterDownloader: PosterDownloader,
         dateTimeProvider: DateTimeProvider,
     ): WidgetPublisher = SnapshotWidgetPublisher(
         widgetManager = widgetManager,
         jsonFileManager = jsonFileManager,
+        posterDownloader = posterDownloader,
         dateTimeProvider = dateTimeProvider,
     )
 }
