f379c371b466775d98a381ceb2e1a9bbbde3c5af
Thomas Kioko <kiokotomas@gmail.com>
2026-09-12 16:03:42 +0200

    Add Liquid Glass to the rating and watch date sheets

--- ios/Packages/WatchDateSelection/Tests/WatchDateSelectionTests/WatchDateSelectionContentTest.swift
+++ ios/Packages/WatchDateSelection/Tests/WatchDateSelectionTests/WatchDateSelectionContentTest.swift
@@ -6,23 +6,27 @@ import XCTest
 
 class WatchDateSelectionContentTest: SnapshotTestCase {
     func test_WatchDateSelectionContent_Default() {
-        makeSheet()
-            .assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_Default")
+        let view = makeSheet()
+        view.assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_Default")
+        view.assertSnapshot(layout: .defaultDevice, liquidGlass: true, testName: "WatchDateSelectionContent_Default")
     }
 
     func test_WatchDateSelectionContent_ReleaseDateDisabled() {
-        makeSheet(isReleaseDateEnabled: false)
-            .assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_ReleaseDateDisabled")
+        let view = makeSheet(isReleaseDateEnabled: false)
+        view.assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_ReleaseDateDisabled")
+        view.assertSnapshot(layout: .defaultDevice, liquidGlass: true, testName: "WatchDateSelectionContent_ReleaseDateDisabled")
     }
 
     func test_WatchDateSelectionContent_EditWithDate() {
-        makeSheet(title: "Change watched date", currentWatchedAtLabel: "12 Jan 2026 20:30")
-            .assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_EditWithDate")
+        let view = makeSheet(title: "Change watched date", currentWatchedAtLabel: "12 Jan 2026 20:30")
+        view.assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_EditWithDate")
+        view.assertSnapshot(layout: .defaultDevice, liquidGlass: true, testName: "WatchDateSelectionContent_EditWithDate")
     }
 
     func test_WatchDateSelectionContent_EditWithUnknownDate() {
-        makeSheet(title: "Change watched date", currentWatchedAtLabel: "A long time ago")
-            .assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_EditWithUnknownDate")
+        let view = makeSheet(title: "Change watched date", currentWatchedAtLabel: "A long time ago")
+        view.assertSnapshot(layout: .defaultDevice, testName: "WatchDateSelectionContent_EditWithUnknownDate")
+        view.assertSnapshot(layout: .defaultDevice, liquidGlass: true, testName: "WatchDateSelectionContent_EditWithUnknownDate")
     }
 
     private func makeSheet(
