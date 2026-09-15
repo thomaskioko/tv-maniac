b007fa6fc4df4c8b2da43efc64c27bfa1b506cea
Thomas Kioko <kiokotomas@gmail.com>
2026-09-15 13:37:22 +0200

    Use the default prefetch distance

--- core/paging/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/paging/CommonPagingConfig.kt
+++ core/paging/src/commonMain/kotlin/com/thomaskioko/tvmaniac/core/paging/CommonPagingConfig.kt
@@ -8,11 +8,9 @@ import kotlin.time.Duration.Companion.hours
 public object CommonPagingConfig {
 
     public val CACHE_EXPIRE_TIME: Duration = 6.hours
-    private const val PREFETCH_DISTANCE = 1
 
     public val pagingConfig: PagingConfig = PagingConfig(
         pageSize = DEFAULT_PAGE_SIZE,
         initialLoadSize = DEFAULT_PAGE_SIZE,
-        prefetchDistance = PREFETCH_DISTANCE,
     )
 }
