d30fe068c5a8af86caaded812c02bf7ce3e3240e
Thomas Kioko <kiokotomas@gmail.com>
2026-09-15 13:24:49 +0200

    Read the first page constant from the data layer

--- data/similar/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/similar/implementation/SimilarParams.kt
+++ data/similar/implementation/src/commonMain/kotlin/com/thomaskioko/tvmaniac/similar/implementation/SimilarParams.kt
@@ -1,6 +1,6 @@
 package com.thomaskioko.tvmaniac.similar.implementation
 
-import com.thomaskioko.tvmaniac.tmdb.api.DEFAULT_API_PAGE
+import com.thomaskioko.tvmaniac.shows.api.model.DEFAULT_API_PAGE
 
 public data class SimilarParams(
     val page: Long = DEFAULT_API_PAGE,
